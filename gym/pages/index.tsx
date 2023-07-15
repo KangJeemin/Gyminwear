b400cb181d1d634b93e8d8e59fe5c3f4863e3574
jiminKang <wlals1234560@gmail.com>
2023-07-15 18:16:27 +0900

    프로젝트 정리 및 next로 진행하기로 결정.

---
+++ gym/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+const index = ()=>{
+    return(
+        <div>
+            <Link href={"/main"}>
+                <a>강지민 페이지</a>  
+            </Link>
+            
+        </div>
+    )
+}
+
+export default index;
---
+++ gym/pages/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'next/link'
+const main = ()=>{
+    return(
+        <div>
+            <Link href={"/"}>
+                <a>메인 페이지</a>  
+            </Link>
+            
+        </div>
+    )
+}
+
+export default main;

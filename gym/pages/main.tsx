7cff8b1edb4f205d7b84a6426a5f7a9e38879157
jiminKang <wlals1234560@gmail.com>
2023-07-15 18:46:07 +0900

    header 만들어서 메인페이지에 연결

--- gym/pages/main.tsx
+++ gym/pages/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
+import Header from './src/component/header'
 const main = ()=>{
     return(
         <div>
+            <Header></Header>
             <Link href={"/"}>
                 <a>메인 페이지</a>  
             </Link>
---
+++ gym/pages/src/component/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+const header = ()=>{
+    return(
+        <div className='Header' style={
+            {
+                width:'100vw',
+                height:'10vh',
+                backgroundColor:'white'
+
+            }
+        }>
+            
+        </div>
+    )
+}
+
+export default header;

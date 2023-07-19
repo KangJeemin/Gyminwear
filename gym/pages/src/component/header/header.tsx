cc6d01308aef83b69372b415fd44373b67966e28
jiminKang <wlals1234560@gmail.com>
2023-07-19 16:09:42 +0900

    이미지 넣는거 성공

--- gym/pages/src/component/header/header.tsx
+++ gym/pages/src/component/header/header.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import styles from './header.module.css'
 import Image from 'next/image'
+import a from '../../../../public/image/search.png'
 const header = ()=>{
     return(
         <div id='header' className={`${styles.header} ${styles.flexColumn}`}>
             <div id='categorybox'className={`${styles.category} ${styles.flexRow}`}>
-                <div id="logoBox" className={styles.logoBox}></div>
+                <div id="logoBox" className={styles.logoBox}>
+                    <Image
+                        src={a}
+                        alt= "이미지를 표시 할 수 없습니다."
+                        width={100}
+                        height={100}
+
+                    />
+                </div>
                 <div id="centerBox" className={styles.centerBox}></div>
                 <div id="menuBox" className={styles.menuBox}>
                 </div>

085a8c7febf71511f3230bd0877ac77a8d8160a8
jiminKang <wlals1234560@gmail.com>
2023-07-19 10:01:52 +0900

    스타일 지정시 id 형식으로 지정 되는 것을 확인

--- gym/pages/src/component/middle/middle.tsx
+++ gym/pages/src/component/middle/middle.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
+import styles from './middle.module.css'
 const middle = ()=>{
     return(
-        <div className='Header' style={
-            {
-                width:'100vw',
-                height:'80vh',
-                backgroundColor:'red'
-
-            }
-        }>
+        <div id='middle' className={styles.middle}> 
+            <div id={styles.middle_topText} ></div>
+            <div id='middle_middletopComponent'  className={styles.middle_middletopComponent}></div>
             
         </div>
     )

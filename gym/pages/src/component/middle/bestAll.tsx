1c2d6fbc729d18dd4e91669c32ff1a871a46aec9
jiminKang <wlals1234560@gmail.com>
2023-07-20 23:29:41 +0900

    가로 스크롤 완성. 이제 반복문으로 아이템만 떄려 넣으면 돼

--- gym/pages/src/component/middle/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll.tsx
@@ -7,7 +7,7 @@ const bestAll = ()=>{
                 <h1 id={styles.bestAll_text}>이번 주 인기 상품</h1>
             </div>
             <div id={styles.flex_nowrap}>
-                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2}`}>
+                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
                     <span className={`${styles.padding_1} ${styles.flex_column}`}>
                         <span id={styles.item_imageSize}></span>
                         <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
@@ -17,7 +17,7 @@ const bestAll = ()=>{
                         </span>
                     </span>
                 </div>
-                <div id={styles.bestAll_topComponent} className={styles.grid_2x2}>
+                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
                     <span className={`${styles.padding_1} ${styles.flex_column}`}>
                         <span id={styles.item_imageSize}></span>
                         <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
@@ -27,7 +27,7 @@ const bestAll = ()=>{
                         </span>
                     </span>
                 </div>
-                <div id={styles.bestAll_topComponent} className={styles.grid_2x2}>
+                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
                     <span className={`${styles.padding_1} ${styles.flex_column}`}>
                         <span id={styles.item_imageSize}></span>
                         <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>

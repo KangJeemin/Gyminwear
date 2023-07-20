94a9978bd58c0f0b78dd85567a85b6c976434831
jiminKang <wlals1234560@gmail.com>
2023-07-20 23:27:39 +0900

    가로 스크롤이 안되고 있는 형태

--- gym/pages/src/component/middle/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll.tsx
@@ -6,15 +6,37 @@ const bestAll = ()=>{
             <div id={styles.bestAll_topText}>
                 <h1 id={styles.bestAll_text}>이번 주 인기 상품</h1>
             </div>
-            <div id={styles.bestAll_topComponent} className={styles.grid_2x2}>
-                <span className={`${styles.padding_1} ${styles.flex_column}`}>
-                    <span id={styles.item_imageSize}></span>
-                    <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
-                        <span id={styles.item_itemBrandName}>borntowin</span>
-                        <span id={styles.item_itemName}>헤리코든 오버핏</span>
-                        <span id={styles.item_itemPrice}>44,000원</span>
+            <div id={styles.flex_nowrap}>
+                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2}`}>
+                    <span className={`${styles.padding_1} ${styles.flex_column}`}>
+                        <span id={styles.item_imageSize}></span>
+                        <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.item_itemBrandName}>borntowin</span>
+                            <span id={styles.item_itemName}>헤리코든 오버핏</span>
+                            <span id={styles.item_itemPrice}>44,000원</span>
+                        </span>
                     </span>
-                </span>
+                </div>
+                <div id={styles.bestAll_topComponent} className={styles.grid_2x2}>
+                    <span className={`${styles.padding_1} ${styles.flex_column}`}>
+                        <span id={styles.item_imageSize}></span>
+                        <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.item_itemBrandName}>borntowin</span>
+                            <span id={styles.item_itemName}>헤리코든 오버핏</span>
+                            <span id={styles.item_itemPrice}>44,000원</span>
+                        </span>
+                    </span>
+                </div>
+                <div id={styles.bestAll_topComponent} className={styles.grid_2x2}>
+                    <span className={`${styles.padding_1} ${styles.flex_column}`}>
+                        <span id={styles.item_imageSize}></span>
+                        <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.item_itemBrandName}>borntowin</span>
+                            <span id={styles.item_itemName}>헤리코든 오버핏</span>
+                            <span id={styles.item_itemPrice}>44,000원</span>
+                        </span>
+                    </span>
+                </div>
             </div>
             
         </div>

fb98a316fad8de3ad0e4508976833085e9440567
jiminKang <wlals1234560@gmail.com>
2023-07-21 10:54:00 +0900

    jsx 문법에서 반복문을 사용하여 직접 jsx문법을 내 보낼 수 없고 배열을 거처서 내 보내야 한다네

--- gym/pages/src/component/middle/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll.tsx
@@ -4,9 +4,9 @@ import styles from './bestAll.module.css'
 
 
 const BestAll:React.FC = () =>{
- const [itemfor, setItemfor] = useState<number>(3)
-
-    
+ const [itemfor, setItemfor] = useState<string[]>([])
+ 
+ 
     return(
         <div id={styles.bestAll}> 
             <div id={styles.bestAll_topText}>
@@ -14,18 +14,19 @@ const BestAll:React.FC = () =>{
             </div>
             <div id={styles.flex_nowrap}>
                 <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    
-                    {itemfor.map(function(){
-
-                    })}
-                    <span className={`${styles.padding_1} ${styles.flex_column}`}>
+                    for (let index = 0; index< 4; index++) {
+                        <span className={`${styles.padding_1} ${styles.flex_column}`}>
                         <span id={styles.item_imageSize}></span>
                         <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
                             <span id={styles.item_itemBrandName}><h1>borntowin</h1></span>
                             <span id={styles.item_itemName}><h2>헤리코든 오버핏</h2></span>
                             <span id={styles.item_itemPrice}><h3>44,000원</h3></span>
                         </span>
-                    </span>
+                    </span>    
+                        
+                    } 
+                   
+                    
 
                 </div>
             </div>

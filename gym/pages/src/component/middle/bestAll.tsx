9d64977a97f017b0ddc77b07c00ed3b1eeb9e6d5
jiminKang <wlals1234560@gmail.com>
2023-07-22 13:10:30 +0900

    왜 아이템이 겹쳐 보이는걸까?

--- gym/pages/src/component/middle/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll.tsx
@@ -46,26 +46,25 @@ const BestAll:React.FC = () =>{
                 <h1 id={styles.bestAll_text}>이번 주 인기 상품</h1>
             </div>
             <div id={styles.flex_nowrap}>
-                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                <div id={styles.bestAll_topComponent} >
                 {items.map((item, index) => {
-    return (
-        <div key={index}>
-            {item.itemFrom4.map((innerItem, innerIndex) => {
-                return (
-                    <span key={innerIndex} className={`${styles.padding_1} ${styles.flex_column}`}>
-                        <span id={styles.item_imageSize}></span>
-                        <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.item_itemBrandName}><h1>{innerItem.brandName}</h1></span>
-                            <span id={styles.item_itemName}><h2>{innerItem.itemName}</h2></span>
-                            <span id={styles.item_itemPrice}><h3>{innerItem.itemPrice}</h3></span>
-                        </span>
-                    </span>
-                );
-            })}
-        </div>
-    );
-})}
-
+                     return (
+                          <div key={index} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                            {item.itemFrom4.map((innerItem, innerIndex) => {
+                                return (
+                                    <span key={innerIndex} className={`${styles.padding_1} ${styles.flex_column}`}>
+                                        <span id={styles.item_imageSize}></span>
+                                        <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
+                                            <span id={styles.item_itemBrandName}><h1>{innerItem.brandName}</h1></span>
+                                            <span id={styles.item_itemName}><h2>{innerItem.itemName}</h2></span>
+                                            <span id={styles.item_itemPrice}><h3>{innerItem.itemPrice}</h3></span>
+                                        </span>
+                                    </span>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
                 </div>
             </div>
         </div>

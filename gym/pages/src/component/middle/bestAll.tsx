b98fbd528e502f90d4ac641f8a2fa358567bdea1
jiminKang <wlals1234560@gmail.com>
2023-07-24 13:22:22 +0900

    flex-basis, flex-wrap, 속성에 대해 지금 내가 잘 못 알고 있는 것 같다.

--- gym/pages/src/component/middle/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll.tsx
@@ -1,110 +1,58 @@
 import React, { useState } from 'react'
 import styles from './bestAll.module.css'
-import getArrayLength from '../../module/arrayLength'
 
 interface Item {
-    brandName:string;
-    itemName:string;
-    itemPrice:number|string;
-}
-
-interface ItemFrom{
-    itemFrom4?:Item[]
-    itemFrom5?:Item[]
+    brandName:string
+    itemName:string
+    itemPrice:number|string
 }
 
 const BestAll:React.FC = () =>{
- const items:ItemFrom[] =[
+ const items:Item[] =[
     {
-        itemFrom4:[
-        {
-            brandName:"brontowin",
-            itemName:"헤리코든 오버핏",
-            itemPrice:44000+'원'
-        },
-        {
-            brandName:"brontowin",
-            itemName:"헤리코든 오버핏",
-            itemPrice:44000+'원'
-        },
-        {
-            brandName:"brontowin",
-            itemName:"헤리코든 오버핏",
-            itemPrice:44000+'원'
-        },
-        {
-            brandName:"brontowin",
-            itemName:"헤리코든 오버핏",
-            itemPrice:44000+'원'
-        },
-    ]
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'원'
     },
     {
-        itemFrom5:[
-            {
-                brandName:"brontowin",
-                itemName:"헤리코든 오버핏",
-                itemPrice:44000+'원이'
-            },
-            {
-                brandName:"brontowin",
-                itemName:"헤리코든 오버핏",
-                itemPrice:44000+'원이'
-            },
-            {
-                brandName:"brontowin",
-                itemName:"헤리코든 오버핏",
-                itemPrice:44000+'원이'
-            },
-            {
-                brandName:"brontowin",
-                itemName:"헤리코든 오버핏",
-                itemPrice:44000+'원'
-            },
-        ]
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'원'
+    },
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'원'
+    },
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'원'
     }
-
  ]
+ 
+ 
     return(
         <div id={styles.bestAll}> 
             <div id={styles.bestAll_topText}>
                 <h1 id={styles.bestAll_text}>이번 주 인기 상품</h1>
             </div>
             <div id={styles.flex_nowrap}>
-                {items.map((item, index) => {
-                     return (
-                        <div key={index} id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`} >
-                            {item.itemFrom4 && item.itemFrom4.map((innerItem, innerIndex) => {
-                                return (
-                                    <span id={styles.bestAll_topComponent_1} key={innerIndex} className={`${styles.padding_1} ${styles.flex_column}`}>
-                                        <span id={styles.item_imageSize}></span>
-                                        <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
-                                            <span id={styles.item_itemBrandName}><h1>{innerItem.brandName}</h1></span>
-                                            <span id={styles.item_itemName}><h2>{innerItem.itemName}</h2></span>
-                                            <span id={styles.item_itemPrice}><h3>{innerItem.itemPrice}</h3></span>
-                                        </span>
-                                    </span>
-                                );
-                            })}
-                            {/* {item.itemFrom5 && item.itemFrom5.map((innerItem, innerIndex) => {
-                                return (
-                                    <span id={styles.bestAll_topComponent_1} key={innerIndex} className={`${styles.padding_1} ${styles.flex_column}`}>
-                                        <span id={styles.item_imageSize}></span>
-                                        <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
-                                            <span id={styles.item_itemBrandName}><h1>{innerItem.brandName}</h1></span>
-                                            <span id={styles.item_itemName}><h2>{innerItem.itemName}</h2></span>
-                                            <span id={styles.item_itemPrice}><h3>{innerItem.itemPrice}</h3></span>
-                                        </span>
-                                    </span>
-                                );
-                            })} */}
-                            
-                        </div>
-                    );
-                })}
+                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                    {items.map((item, index) => (
+                        <span key={index} className={`${styles.padding_1} ${styles.flex_column}`}>
+                          <span id={styles.item_imageSize}></span>
+                          <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.item_itemBrandName}><h1>{item.brandName}</h1></span>
+                            <span id={styles.item_itemName}><h2>{item.itemName}</h2></span>
+                            <span id={styles.item_itemPrice}><h3>{item.itemPrice}</h3></span>
+                          </span>
+                        </span>
+                    ))}
                 </div>
+                
             </div>
-        
+        </div>
     )
 }
 

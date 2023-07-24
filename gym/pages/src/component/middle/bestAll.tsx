64833738a76a76cf020ee20cc79d4c6546d20352
jiminKang <wlals1234560@gmail.com>
2023-07-24 17:05:54 +0900

    여기서 아이템들을 더 추가하면 왜 아이템 컨포넌트의 크기가 줄어들지?

--- gym/pages/src/component/middle/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll.tsx
@@ -40,7 +40,7 @@ const BestAll:React.FC = () =>{
             <div id={styles.flex_nowrap}>
                 <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
                     {items.map((item, index) => (
-                        <span key={index} className={`${styles.padding_1} ${styles.flex_column}`}>
+                        <span key={index} id={styles.item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                           <span id={styles.item_imageSize}></span>
                           <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
                             <span id={styles.item_itemBrandName}><h1>{item.brandName}</h1></span>
@@ -50,18 +50,7 @@ const BestAll:React.FC = () =>{
                         </span>
                     ))}
                 </div>
-                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    {items.map((item, index) => (
-                        <span key={index} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.item_imageSize}></span>
-                          <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.item_itemBrandName}><h1>{item.brandName}</h1></span>
-                            <span id={styles.item_itemName}><h2>{item.itemName}</h2></span>
-                            <span id={styles.item_itemPrice}><h3>{item.itemPrice}</h3></span>
-                          </span>
-                        </span>
-                    ))}
-                </div>
+              
             </div>
         </div>
     )

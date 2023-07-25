9060bcab176f20edcf5d73620d8e8a827265048e
jiminKang <wlals1234560@gmail.com>
2023-07-25 16:42:05 +0900

    gpt가 알려준대로 따라하고나서 1차 시도.useRef<HTMLDivElement | null>은 타입스크립트가 요소에 접근할 때 scrollHeight같은 DOM 속성을 찾을 수 있게 해주는 문법

--- gym/pages/src/component/middle/topComponent/topComponent.tsx
+++ gym/pages/src/component/middle/topComponent/topComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState ,useRef, useEffect} from 'react'
 import styles from './topComponent.module.css'
 
 interface Item {
@@ -31,26 +31,55 @@ const TopPage:React.FC = () =>{
     }
     
  ]
+ const scrollableRef = useRef<HTMLDivElement | null>(null);
+ const [showNewDiv, setShowNewDiv] = useState(false);
+
+ const handleScroll = () => {
+   const element = scrollableRef.current;
+   if (element) {
+     const isAtBottom = element.scrollHeight - element.scrollTop === element.clientHeight;
+     setShowNewDiv(isAtBottom); // 스크롤이 최하단에 도달하면 showNewDiv 값을 true로 변경
+   }
+ };
+
+ useEffect(() => {
+   const element = scrollableRef.current;
+   if (element) {
+     element.addEventListener('scroll', handleScroll);
+   }
+   return () => {
+     if (element) {
+       element.removeEventListener('scroll', handleScroll);
+     }
+   };
+ }, []);
  
- 
+  {/* {items.map((item, index) => (
+                    <span key={index} id={styles.topComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                        <span id={styles.topComponent_item_imageSize}></span>
+                        <span id={styles.topComponent_item_textBoxSize} className={`${styles.flex_column}`}>
+                        <span id={styles.topComponent_item_itemBrandName}><h1>{item.brandName}</h1></span>
+                        <span id={styles.topComponent_item_itemName}><h2>{item.itemName}</h2></span>
+                        <span id={styles.topComponent_item_itemPrice}><h3>{item.itemPrice}</h3></span>
+                        </span>
+                    </span>
+                ))} */}
     return(
         <div id={styles.topComponent}> 
             <div id={styles.topComponent_topText}>
                 <h1 id={styles.topComponent_text}>상의</h1>
             </div>
-            <div id={styles.topComponent_itemContainer_flexNowrap}>
-                <div id={styles.topComponent_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    {items.map((item, index) => (
-                        <span key={index} id={styles.topComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.topComponent_item_imageSize}></span>
-                          <span id={styles.topComponent_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.topComponent_item_itemBrandName}><h1>{item.brandName}</h1></span>
-                            <span id={styles.topComponent_item_itemName}><h2>{item.itemName}</h2></span>
-                            <span id={styles.topComponent_item_itemPrice}><h3>{item.itemPrice}</h3></span>
-                          </span>
-                        </span>
-                    ))}
+            <div ref ={scrollableRef} id={styles.topComponent_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+               <span>a</span>
+               <span>a</span>
+               <span>a</span>
+               <span>a</span>
+               {showNewDiv && (
+                <div style={{ backgroundColor: 'lightblue', padding: '10px', margin: '10px' }}>
+                    새로운 div 컴포넌트가 생성되었습니다!
                 </div>
+      )}
+
             </div>
         </div>
     )

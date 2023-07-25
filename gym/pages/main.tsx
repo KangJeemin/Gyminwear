29fbcf6ff74b1768192d393b7e5289da71847957
jiminKang <wlals1234560@gmail.com>
2023-07-25 10:40:55 +0900

    메인 페이지 레이아웃 완성. 이제 스타일링 필요

--- gym/pages/main.tsx
+++ gym/pages/main.tsx
@@ -4,12 +4,14 @@ import Header from './src/component/header/header'
 import BestAll from './src/component/middle/bestAll'
 import Footer from './src/component/footer/footer'
 import BestTop from './src/component/middle/bestTop'
+import BestBottom from './src/component/middle/bestBottom'
 const main = ()=>{
     return(
         <div>
             <Header/>
             <BestAll/>
             <BestTop/>
+            <BestBottom/>
         </div>
     )
 }

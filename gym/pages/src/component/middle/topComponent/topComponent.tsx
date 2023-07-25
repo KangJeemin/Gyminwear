a79c738c5290971b76865cbda31241816631663b
jiminKang <wlals1234560@gmail.com>
2023-07-25 14:23:17 +0900

    topPage 테스트 할 수 있게 경로 및 파일 내용 수정

---
+++ gym/pages/src/component/middle/topComponent/topComponent.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import styles from './topComponent.module.css'
+
+interface Item {
+    brandName:string
+    itemName:string
+    itemPrice:number|string
+}
+
+const TopPage:React.FC = () =>{
+ const items:Item[] =[
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'원'
+    },
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'원'
+    },
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'원'
+    },
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'원'
+    }
+    
+ ]
+ 
+ 
+    return(
+        <div id={styles.topComponent}> 
+            <div id={styles.topComponent_topText}>
+                <h1 id={styles.topComponent_text}>상의</h1>
+            </div>
+            <div id={styles.topComponent_itemContainer_flexNowrap}>
+                <div id={styles.topComponent_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                    {items.map((item, index) => (
+                        <span key={index} id={styles.topComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                          <span id={styles.topComponent_item_imageSize}></span>
+                          <span id={styles.topComponent_item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.topComponent_item_itemBrandName}><h1>{item.brandName}</h1></span>
+                            <span id={styles.topComponent_item_itemName}><h2>{item.itemName}</h2></span>
+                            <span id={styles.topComponent_item_itemPrice}><h3>{item.itemPrice}</h3></span>
+                          </span>
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TopPage;
--- gym/pages/topPage.tsx
+++ gym/pages/topPage.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
 import Header from './src/component/header/header'
+import TopComponent from './src/component/middle/topComponent/topComponent'
 
-const TopPage= () => {
+const topPage = () => {
     return (
+        <div>
         <Header/>
-        
+        <TopComponent/>
+        </div>
     )
 
 }
+
+export default topPage

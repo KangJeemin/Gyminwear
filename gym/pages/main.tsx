d34a99f8326e218654d1945074c0d4fe1f052329
jiminKang <wlals1234560@gmail.com>
2023-07-25 11:22:34 +0900

    파일 구조 일부분 수정. (급격하게 바뀐건 없음)

--- gym/pages/main.tsx
+++ gym/pages/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
 import Header from './src/component/header/header'
-import BestAll from './src/component/middle/bestAll'
+import BestAll from './src/component/middle/bestAll/bestAll'
 import Footer from './src/component/footer/footer'
-import BestTop from './src/component/middle/bestTop'
-import BestBottom from './src/component/middle/bestBottom'
+import BestTop from './src/component/middle/bestTop/bestTop'
+import BestBottom from './src/component/middle/bestBottom/bestBottom'
 const main = ()=>{
     return(
         <div>

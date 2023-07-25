127f02848c99158ddfdc6b6899d577ebd84dc431
jiminKang <wlals1234560@gmail.com>
2023-07-25 14:17:12 +0900

    상의 페이지 레이아웃 생성하기 시작

---
+++ gym/pages/topPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+import Header from './src/component/header/header'
+
+const TopPage= () => {
+    return (
+        <Header/>
+        
+    )
+
+}

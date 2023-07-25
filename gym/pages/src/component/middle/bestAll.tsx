5a2479a927a04768fc61dea59d56b29426f29c07
jiminKang <wlals1234560@gmail.com>
2023-07-25 10:27:25 +0900

    bestall 컨포넌트 이름 수정

--- gym/pages/src/component/middle/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll.tsx
@@ -38,44 +38,43 @@ const BestAll:React.FC = () =>{
             <div id={styles.bestAll_topText}>
                 <h1 id={styles.bestAll_text}>이번 주 인기 상품</h1>
             </div>
-            <div id={styles.flex_nowrap}>
-                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+            <div id={styles.bestAll_itemContainer_flexNowrap}>
+                <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
                     {items.map((item, index) => (
-                        <span key={index} id={styles.item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.item_imageSize}></span>
-                          <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.item_itemBrandName}><h1>{item.brandName}</h1></span>
-                            <span id={styles.item_itemName}><h2>{item.itemName}</h2></span>
-                            <span id={styles.item_itemPrice}><h3>{item.itemPrice}</h3></span>
+                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                          <span id={styles.bestAll_item_imageSize}></span>
+                          <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.bestAll_item_itemBrandName}><h1>{item.brandName}</h1></span>
+                            <span id={styles.bestAll_item_itemName}><h2>{item.itemName}</h2></span>
+                            <span id={styles.bestAll_item_itemPrice}><h3>{item.itemPrice}</h3></span>
                           </span>
                         </span>
                     ))}
                 </div>
-                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
                     {items.map((item, index) => (
-                        <span key={index} id={styles.item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.item_imageSize}></span>
-                          <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.item_itemBrandName}><h1>{item.brandName}</h1></span>
-                            <span id={styles.item_itemName}><h2>{item.itemName}</h2></span>
-                            <span id={styles.item_itemPrice}><h3>{item.itemPrice}</h3></span>
+                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                          <span id={styles.bestAll_item_imageSize}></span>
+                          <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.bestAll_item_itemBrandName}><h1>{item.brandName}</h1></span>
+                            <span id={styles.bestAll_item_itemName}><h2>{item.itemName}</h2></span>
+                            <span id={styles.bestAll_item_itemPrice}><h3>{item.itemPrice}</h3></span>
                           </span>
                         </span>
                     ))}
                 </div>
-                <div id={styles.bestAll_topComponent} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
                     {items.map((item, index) => (
-                        <span key={index} id={styles.item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.item_imageSize}></span>
-                          <span id={styles.item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.item_itemBrandName}><h1>{item.brandName}</h1></span>
-                            <span id={styles.item_itemName}><h2>{item.itemName}</h2></span>
-                            <span id={styles.item_itemPrice}><h3>{item.itemPrice}</h3></span>
+                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                          <span id={styles.bestAll_item_imageSize}></span>
+                          <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.bestAll_item_itemBrandName}><h1>{item.brandName}</h1></span>
+                            <span id={styles.bestAll_item_itemName}><h2>{item.itemName}</h2></span>
+                            <span id={styles.bestAll_item_itemPrice}><h3>{item.itemPrice}</h3></span>
                           </span>
                         </span>
                     ))}
                 </div>
-                
             </div>
         </div>
     )

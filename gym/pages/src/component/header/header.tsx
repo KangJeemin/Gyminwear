f8f2b6e3edcb7f2131bdd0006797f87f27017730
jiminKang <wlals1234560@gmail.com>
2023-07-27 21:55:29 +0900

    header 컨포넌트에 drawer 적용 시키려고 했더니, react.useState 선언하는 부분에서 에러 발생

--- gym/pages/src/component/header/header.tsx
+++ gym/pages/src/component/header/header.tsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import * as React from 'react';
 import styles from './header.module.css'
 import Image from 'next/image'
 import searchIcon from '../../../../public/image/search.png'
 import shoppingIcon from '../../../../public/image/shopingBag.png'
 import hambergerIcon from '../../../../public/image/hamberger.png'
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+
 const header = ()=>{
+
+type Anchor = 'top'
+
+const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
+
+  const list = (anchor: Anchor) => (
+    
+    <Box
+      sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      
+    </Box>
+  );
     return(
+        
         <div id={styles.header} className={`${styles.flexColumn}`}>
             <div id={styles.categoryBox} className={`${styles.flexRow}`}>
                 <div id={styles.logoBox}></div>

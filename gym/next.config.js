0c9481361822672b61c93e6f98fb5e584ca94390
jiminKang <wlals1234560@gmail.com>
2023-07-30 19:59:37 +0900

    proxy 문제 해결 및 naver API 요청 응답 성공

--- gym/next.config.js
+++ gym/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+		return [
+			{
+				source: "/:path*",
+				destination: "http://localhost:3000/:path*",
+			},
+		];
+	},
 }
 
 module.exports = nextConfig
--- gym/pages/api/naver.ts
+++ gym/pages/api/naver.ts
@@ -2,8 +2,8 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse)=>{
     const encoder = new TextEncoder();
-    let url ='https://openapi.naver.com/v1/search/blog?/query=rbw'
-    // let url ='https://openapi.naver.com/v1/search/shop?query='
+    // let url ='https://openapi.naver.com/v1/search/blog?/query=rbw'
+    let url ='https://openapi.naver.com/v1/search/shop?query='
     let ClientID='vqhMSE8joE5v7eeLQaHt'
     let ClientSecret='EMjd5sI8k9'
 
@@ -20,5 +20,5 @@ export default (req: NextApiRequest, res: NextApiResponse)=>{
     
       }
     ).then((response) => response.json()).then(data=>{console.log(data)})
-      return console.log("heel")
+      return res.send("heoolo")
 };

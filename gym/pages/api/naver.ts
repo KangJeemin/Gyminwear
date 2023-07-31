07df4cc3dda80894d58afa804cf2282c3c907997
jiminKang <wlals1234560@gmail.com>
2023-07-31 11:35:13 +0900

    환경변수 인식하지 못하는 문제 발생

--- gym/pages/api/naver.ts
+++ gym/pages/api/naver.ts
@@ -2,9 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse)=>{
     const encoder = new TextEncoder();
+    let ClientID=process.env.NEXT_NAVER_ClientId
     // let url ='https://openapi.naver.com/v1/search/blog?/query=rbw'
     let url ='https://openapi.naver.com/v1/search/shop?query='
-    let ClientID='vqhMSE8joE5v7eeLQaHt'
+    // let ClientID='vqhMSE8joE5v7eeLQaHt'
     let ClientSecret='EMjd5sI8k9'
 
     let AppName=encodeURIComponent('rbw')

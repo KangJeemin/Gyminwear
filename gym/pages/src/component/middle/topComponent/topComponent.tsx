7d4e6d713b1a7394bc875357393440dea76bab6f
jiminKang <wlals1234560@gmail.com>
2023-07-31 15:10:04 +0900

    8x2 로 변경했음.

--- gym/pages/src/component/middle/topComponent/topComponent.tsx
+++ gym/pages/src/component/middle/topComponent/topComponent.tsx
@@ -16,19 +16,21 @@ const TopComponent: React.FC = () => {
   
   return (
     <div id={styles.topComponent}>
-      {showComponent === 1 ? (
-      <>
         <div id={styles.topComponent_topText}>
           <h1 id={styles.topComponent_text}>상의</h1>
         </div>
-        <div id={styles.topComponent_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-          <span>a</span>
-          <span>a</span>
-          <span>a</span>
-          <span>a</span>
+        {showComponent === 0 ? (
+        <div id={styles.topComponent_itemContainer} className={`${styles.grid_8x2} ${styles.flex_scrollSet}`}>
+          <span id={styles.topComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+            <span id={styles.topComponent_item_imageSize}></span>
+            <span id={styles.topComponent_item_textBoxSize} className={`${styles.flex_column}`}>
+              <span id={styles.topComponent_item_itemBrandName}><h1>aa</h1></span>
+              <span id={styles.topComponent_item_itemName}><h2>bb</h2></span>
+              <span id={styles.topComponent_item_itemPrice}><h3>cc</h3></span>
+            </span>
+          </span>
         </div>
-      </>
-): null}
+        ): null}
     </div>
   );
 };

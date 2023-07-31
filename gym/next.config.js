4b2e31445ce37bb096b8352a1f39f76124646efa
jiminKang <wlals1234560@gmail.com>
2023-07-31 11:39:33 +0900

    next.config에 환경변수 기입

--- gym/next.config.js
+++ gym/next.config.js
@@ -9,6 +9,11 @@ const nextConfig = {
 			},
 		];
 	},
+  env:{
+    NEXT_PUBLIC_NAVER_ClientId:'vqhMSE8joE5v7eeLQaHt',
+    NEXT_PUBLIC_NAVER_ClientSecret:'EMjd5sI8k9'
+
+  }
 }
 
 module.exports = nextConfig
--- gym/pages/api/naver.ts
+++ gym/pages/api/naver.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse)=>{
     const encoder = new TextEncoder();
-    let ClientID=process.env.NEXT_NAVER_ClientId
+    const ClientID=process.env.NEXT_PUBLIC_NAVER_ClientId
     // let url ='https://openapi.naver.com/v1/search/blog?/query=rbw'
     let url ='https://openapi.naver.com/v1/search/shop?query='
     // let ClientID='vqhMSE8joE5v7eeLQaHt'

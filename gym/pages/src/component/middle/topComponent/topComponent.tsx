c02f7891529d0b3e800c55362a3b6c91a320dc18
jiminKang <wlals1234560@gmail.com>
2023-07-31 14:38:36 +0900

    버튼 누르면 페이지 이동으로 바꾸자

--- gym/pages/src/component/middle/topComponent/topComponent.tsx
+++ gym/pages/src/component/middle/topComponent/topComponent.tsx
@@ -8,47 +8,11 @@ interface Item {
 }
 
 const TopComponent: React.FC = () => {
-  const target = useRef<HTMLDivElement | null>(null);
-  const target2 = useRef<HTMLDivElement | null>(null);
-  const [showNewDiv, setShowNewDiv] = useState(false);
-  const [showNewDiv2, setShowNewDiv2] = useState(false);
+;
+  const [showComponent, setshowComponent] = useState(0);
+  
 
-  const options = {
-    threshold: 1.0,
-  };
 
-  const callback1 = () => {
-    if (target.current) {
-      setShowNewDiv(true)
-    }
-  };
-  const callback2 = () => {
-    if (target2.current) {
-      setShowNewDiv2(true)
-    }
-  };
-
-  useEffect(() => {
-    const observer1 = new IntersectionObserver(callback1, options);
-
-    if (target.current) {
-      observer1.observe(target.current); //관찰 대상 등록
-    }
-
-    // Clean up the observer when the component unmounts
-    
-    const observer2 = new IntersectionObserver(callback2, options);
-
-    if (target2.current) {
-      observer2.observe(target2.current); //관찰 대상 등록
-    }
-
-    // Clean up the observer when the component unmounts
-    return () => {
-      observer1.disconnect();
-      observer2.disconnect();
-    };
-  }, [options.threshold]);
   
   return (
     <div id={styles.topComponent}>
@@ -61,16 +25,6 @@ const TopComponent: React.FC = () => {
         <span>a</span>
         <span>a</span>
       </div>
-      {showNewDiv && (
-          <div style={{ backgroundColor: 'blue', padding: '10px', margin: '10px' }} ref={target2}>
-            새로운 div 컴포넌트가 생성되었습니다!2
-          </div>
-        )}
-        {showNewDiv2 && (
-          <div style={{ backgroundColor: 'green', padding: '10px', margin: '10px',height: 60 }}>
-            새로운 div 컴포넌트가 생성되었습니다!2
-          </div>
-        )}
     </div>
   );
 };

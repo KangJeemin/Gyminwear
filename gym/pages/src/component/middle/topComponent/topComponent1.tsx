f451ef24dab545eda80194baed4771b121d60632
jiminKang <wlals1234560@gmail.com>
2023-07-31 16:16:39 +0900

    글씨 가운데 정렬 완료

--- gym/pages/src/component/middle/topComponent/topComponent1.tsx
+++ gym/pages/src/component/middle/topComponent/topComponent1.tsx
@@ -113,11 +113,11 @@ const TopComponent1: React.FC = () => {
                 ))}
         </div>
         <div id ={styles.topComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`}>
-                <span className={`${styles.width_15per}`}>a</span>
-                <span className={`${styles.width_15per}`}>b</span>
-                <span className={`${styles.width_15per}`}>c</span>
-                <span className={`${styles.width_15per}`}>d</span>
-                <span className={`${styles.width_15per}`}>e</span>
+                <span className={`${styles.width_15per} ${styles.flex_row} ${styles.justify_content_center}`}>a</span>
+                <span className={`${styles.width_15per} ${styles.flex_row} ${styles.justify_content_center}`}>b</span>
+                <span className={`${styles.width_15per} ${styles.flex_row} ${styles.justify_content_center}`}>c</span>
+                <span className={`${styles.width_15per} ${styles.flex_row} ${styles.justify_content_center}`}>d</span>
+                <span className={`${styles.width_15per} ${styles.flex_row} ${styles.justify_content_center}`}>e</span>
         </div>
     </div>
   );

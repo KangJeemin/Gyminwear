1f8e6bfa35a9b0f3c1982f7ffbfd0f87f0ccbfb1
jiminKang <wlals1234560@gmail.com>
2023-08-01 17:17:26 +0900

    모달창에서 테스트. 근데 애니메이션이 2번 실행되내

--- gym/pages/src/component/header/modal.tsx
+++ gym/pages/src/component/header/modal.tsx
@@ -18,6 +18,58 @@ const Modal = () => {
       {/* 모달 내용 */}
       {/* 모달 내에 표시할 내용이나 컴포넌트를 추가할 수 있습니다 */}
       <p>이것은 모달입니다!</p>
+      <div
+    style={{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center',
+        width:"100px",
+        height:"100px",
+        position: 'relative'
+        
+    }}>
+    <motion.div
+        style={{
+            width:"30px",
+            height:"5px",
+            backgroundColor:"blue",
+            position: 'absolute'
+        }}
+        animate={{
+            scale:[1,1],
+            rotate:[0,45]
+        }}
+        transition={{
+            duration: 1,
+            ease: "liner",
+            times: [0, 0.5],
+            repeat: 1,
+            
+          }}
+    />
+    <motion.div
+        style={{
+            width:"30px",
+            height:"5px",
+            backgroundColor:"blue",
+            position: 'absolute'
+        }}
+        animate={{
+            scale:[1,1],
+            rotate:[0,-45]
+        }}
+        transition={{
+            duration: 1,
+            ease: "liner",
+            times: [0, 0.5],
+            repeat: 1,
+
+          }}
+    />
+    </div>
+      
+
     </div>
   );
 };

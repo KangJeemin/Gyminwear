a5d963c151e6ae2b939aa0c274b74536697801d2
jiminKang <wlals1234560@gmail.com>
2023-08-01 11:29:09 +0900

    fetch 대신 axios로 환경변수 적용

--- gym/pages/api/naver.ts
+++ gym/pages/api/naver.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
+import axios from "axios"
 export default (req: NextApiRequest, res: NextApiResponse)=>{
     const encoder = new TextEncoder();
     
@@ -8,19 +8,39 @@ export default (req: NextApiRequest, res: NextApiResponse)=>{
     let ClientID='vqhMSE8joE5v7eeLQaHt'
     let ClientSecret='EMjd5sI8k9'
 
-    let AppName=encodeURIComponent('rbw')
-    console.log(AppName)
-    let URL =url + AppName
-    console.log(URL)
-    let returnData=[{}]
-    console.log(process.env.NEXT_PUBLIC_API_URL, "서버")
+    // let AppName=encodeURIComponent('rbw')
+    // console.log(AppName)
+    // let URL =url + AppName
+    // console.log(URL)
+    // let returnData=[{}]
+    // console.log(process.env.NEXT_PUBLIC_API_URL, "서버")
     
-    fetch(URL,{
-      method:'GET',
-      headers:
-        {'X-Naver-Client-Id':ClientID, 'X-Naver-Client-Secret': ClientSecret}
+    // fetch(URL,{
+    //   method:'GET',
+    //   headers:
+    //     {'X-Naver-Client-Id':ClientID, 'X-Naver-Client-Secret': ClientSecret}
     
-      }
-    ).then((response) => response.json()).then(data=>{console.log(data)})
+    //   }
+    // ).then((response) => response.json()).then(data=>{console.log(data)})
+
+const AppName = encodeURIComponent('rbw');
+console.log(AppName);
+
+const URL = url + AppName; // Assuming you have defined 'url' somewhere in your code
+console.log(URL);
+
+const returnData = [{}];
+console.log(process.env.NEXT_PUBLIC_API_URL, "서버");
+
+axios.get(URL, {
+  headers: {
+    'X-Naver-Client-Id': process.env.NEXT_PUBLIC_API_URL,
+    'X-Naver-Client-Secret': ClientSecret
+  }
+}).then((response) => {
+  console.log(response.data);
+}).catch((error) => {
+  console.error(error);
+});
       return res.send("heoolo")
 };

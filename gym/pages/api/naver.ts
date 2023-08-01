4e22d46b6bfc08f438d2f11e7a20d04330e5db26
jiminKang <wlals1234560@gmail.com>
2023-08-01 11:43:28 +0900

    env ignore 테스트

--- gym/pages/api/naver.ts
+++ gym/pages/api/naver.ts
@@ -1,20 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios"
 export default (req: NextApiRequest, res: NextApiResponse)=>{
-    
-    // let url ='https://openapi.naver.com/v1/search/blog?/query=rbw'
-    let url ='https://openapi.naver.com/v1/search/shop?query='
-    let ClientID='vqhMSE8joE5v7eeLQaHt'
-    let ClientSecret='EMjd5sI8k9'
 
-const AppName = encodeURIComponent('rbw');
-console.log(AppName);
+  const AppName = encodeURIComponent('rbw');    
+let url ='https://openapi.naver.com/v1/search/shop?query='
 
 const URL = url + AppName; // Assuming you have defined 'url' somewhere in your code
-console.log(URL);
-
-const returnData = [{}];
-console.log(process.env.NEXT_PUBLIC_API_URL, "서버");
 
 axios.get(URL, {
   headers: {

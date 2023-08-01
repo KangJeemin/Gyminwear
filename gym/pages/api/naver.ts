f506c21f82071279c3d13036171d4f1b3d4d8f72
jiminKang <wlals1234560@gmail.com>
2023-08-01 11:38:43 +0900

    env파일 형태를 변경하여 ClientSecret까지 환경변수 적용함

--- gym/pages/api/naver.ts
+++ gym/pages/api/naver.ts
@@ -1,27 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios"
 export default (req: NextApiRequest, res: NextApiResponse)=>{
-    const encoder = new TextEncoder();
     
     // let url ='https://openapi.naver.com/v1/search/blog?/query=rbw'
     let url ='https://openapi.naver.com/v1/search/shop?query='
     let ClientID='vqhMSE8joE5v7eeLQaHt'
     let ClientSecret='EMjd5sI8k9'
-
-    // let AppName=encodeURIComponent('rbw')
-    // console.log(AppName)
-    // let URL =url + AppName
-    // console.log(URL)
-    // let returnData=[{}]
-    // console.log(process.env.NEXT_PUBLIC_API_URL, "서버")
-    
-    // fetch(URL,{
-    //   method:'GET',
-    //   headers:
-    //     {'X-Naver-Client-Id':ClientID, 'X-Naver-Client-Secret': ClientSecret}
-    
-    //   }
-    // ).then((response) => response.json()).then(data=>{console.log(data)})
 
 const AppName = encodeURIComponent('rbw');
 console.log(AppName);
@@ -34,8 +18,8 @@ console.log(process.env.NEXT_PUBLIC_API_URL, "서버");
 
 axios.get(URL, {
   headers: {
-    'X-Naver-Client-Id': process.env.NEXT_PUBLIC_API_URL,
-    'X-Naver-Client-Secret': ClientSecret
+    'X-Naver-Client-Id': process.env.NEXT_PUBLIC_NAVER_CLIENTID,
+    'X-Naver-Client-Secret': process.env.NEXT_PUBLIC_NAVER_CLIENTSECRET
   }
 }).then((response) => {
   console.log(response.data);

0eceb65023508b5c55e33094a0c737f726846097
jiminKang <wlals1234560@gmail.com>
2023-08-03 14:44:31 +0900

    검색 상태 관찰하기 위한 State 도 하나 만듬

--- gym/public/context/authcontext.tsx
+++ gym/public/context/authcontext.tsx
@@ -1,26 +1,32 @@
 import React, { useState, createContext } from "react";
 
 interface AuthContextProps {
-  state: number;
-  setState: (state:number) => void;
-  
+  hambergerState: number;
+  setHambergerState: (state:number) => void;
+  searchState: number;
+  setSearchState: (state:number) => void;
 }
 
 export const AuthContext = createContext<AuthContextProps>({
-  state: 0,
-  setState: () => {},
+  hambergerState: 0,
+  setHambergerState: () => {},
+  searchState:0,
+  setSearchState: () => {},
+
 });
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
-  const [state, setState] = useState(0);
-
+  const [hambergerState, setHambergerState] = useState(0);
+  const [searchState, setSearchState] = useState(0);
   return (
     <AuthContext.Provider
       value={{
-        state,
-        setState
+        hambergerState,
+        setHambergerState,
+        searchState,
+        setSearchState
       }}
     >
       {children}

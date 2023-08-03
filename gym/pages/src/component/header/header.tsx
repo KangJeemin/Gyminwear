c526a1636f27590a5dead0216bb95e63a8d19fdd
jiminKang <wlals1234560@gmail.com>
2023-08-03 17:07:17 +0900

    이 코드 실행하면,state 값이 계속 변하면서 무한루프에 빠지겠지?

--- gym/pages/src/component/header/header.tsx
+++ gym/pages/src/component/header/header.tsx
@@ -13,15 +13,21 @@ import { AuthContext } from '../../../../public/context/authcontext';
 
 
 const Header = () => {
-const [searchState, setSearchState] = React.useState(false);
-const {state,setState, setHambergerState}= React.useContext(AuthContext)
+
+const {state,setState, searchState, setSearchState, setHambergerState}= React.useContext(AuthContext)
 
 const searchBoxRef = useRef<HTMLDivElement | null>(null);
 const shoppingBoxRef = useRef<HTMLDivElement | null>(null);
-const handle = ()=> {
-    if(state ===0){
+const clickHamberger = ()=> {
+    setState(1)
+    setHambergerState(1)
+}
+const clickSearch = () => {
+    setState(1)
+    setSearchState(1)
+}
+ if(state ===0){
         setState(1)
-        setHambergerState(1)
         if(searchBoxRef.current){
             searchBoxRef.current.style.visibility = "hidden"
         }
@@ -31,7 +37,6 @@ const handle = ()=> {
     }
     else if(state===1){
         setState(2)
-        setHambergerState(2)
         if(searchBoxRef.current){
             searchBoxRef.current.style.visibility = "visible"
         }
@@ -41,7 +46,6 @@ const handle = ()=> {
     }
     else if (state ===2){
         setState(1)
-        setHambergerState(1)
         if(searchBoxRef.current){
             searchBoxRef.current.style.visibility = "hidden"
         }
@@ -51,7 +55,6 @@ const handle = ()=> {
     }
     else{
         setState(2)
-        setHambergerState(2)
         if(searchBoxRef.current){
             searchBoxRef.current.style.visibility = "visible"
         }
@@ -59,14 +62,13 @@ const handle = ()=> {
             shoppingBoxRef.current.style.visibility = "visible"
         }
     }
-}
     return (
         <div id={styles.header} className={`${styles.flexColumn}`}>
             <div id={styles.categoryBox} className={`${styles.flexRow}`}>
                 <div id={styles.logoBox}></div>
                 <div id={styles.centerBox}></div>
                 <div id={styles.menuBox}>
-                    <div id ={styles.searchBox} className={styles.menuBoxMargin} ref={searchBoxRef}>
+                    <div id ={styles.searchBox} className={styles.menuBoxMargin} ref={searchBoxRef} onClick={clickSearch}>
                         <Image
                             src={searchIcon}
                             alt="검색아이콘"
@@ -80,7 +82,7 @@ const handle = ()=> {
                             layout='fill'
                         />
                     </div>
-                    <div id ={styles.hambergerBox} className={styles.menuBoxMargin} onClick={handle}>
+                    <div id ={styles.hambergerBox} className={styles.menuBoxMargin} onClick={clickHamberger}>
                         <div
                         style={{
                             display:'flex',
--- gym/pages/src/component/header/searchModal.tsx
+++ gym/pages/src/component/header/searchModal.tsx
@@ -1,4 +1,3 @@
-import { style } from '@mui/system';
 import { motion } from 'framer-motion';
 import styles from './searchModal.module.css'
 const SearchModal = () => {

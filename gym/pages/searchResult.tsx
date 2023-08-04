afd8f078271a3a27632fac6d479dc7cea4e18deb
jiminKang <wlals1234560@gmail.com>
2023-08-04 15:00:56 +0900

    글씨 크기가 내가 원하는 크기로 변하지 않는데 ?

--- gym/pages/searchResult.tsx
+++ gym/pages/searchResult.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './src/component/header/header'
-import SearchResult from './src/component/middle/searchResult/searchResult'
+import SearchResult from './src/component/middle/searchResultComponent/searchResultComponent'
 const searchResult = () => {
     return (
         <div>

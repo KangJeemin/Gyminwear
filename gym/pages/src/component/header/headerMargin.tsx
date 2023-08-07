e07a93b36f192cbb1919fc26e69d502c498033ae
jiminKang <wlals1234560@gmail.com>
2023-08-07 17:45:10 +0900

    main 페이지에 적용 해 놓고 다른 페이지 가서 결과 확인하고 있었네

--- gym/pages/src/component/header/headerMargin.tsx
+++ gym/pages/src/component/header/headerMargin.tsx
@@ -4,7 +4,8 @@ import * as React from 'react';
 const HeaderMargin = () => {
 
     return (
-       <div style={{width:"100vw",height:"10vh"}}></div>
+       <div style={{width:"100vw",height:"10vh", backgroundColor:"black"}}>
+       </div>
    )
 }
 

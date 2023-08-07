36aa5086e51198067f8e24c3930a8f124dbdde0e
jiminKang <wlals1234560@gmail.com>
2023-08-07 17:32:55 +0900

    header가 fixed 로 지정된 경우 header의 크키랑 동일한 컨포넌트를 만들어 header 밑에 추가하면 컨포넌트가 안 밀리고 보여질 것이라고 생각했음

--- gym/pages/main.tsx
+++ gym/pages/main.tsx
@@ -5,10 +5,12 @@ import BestAll from './src/component/middle/bestAll/bestAll'
 import Footer from './src/component/footer/footer'
 import BestTop from './src/component/middle/bestTop/bestTop'
 import BestBottom from './src/component/middle/bestBottom/bestBottom'
+import HeaderMargin from './src/component/header/headerMargin'
 const main = ()=>{
     return(
         <div>
             <Header/>
+            <HeaderMargin/>
             <BestAll/>
             <BestTop/>
             <BestBottom/>
---
+++ gym/pages/src/component/header/headerMargin.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+
+const HeaderMargin = () => {
+
+    return (
+       <div style={{width:"100vw",height:"10vh"}}></div>
+   )
+}
+
+export default HeaderMargin

c9bf19f1cdb6a30d6cd85834d62096238612a347
jiminKang <wlals1234560@gmail.com>
2023-08-08 16:46:33 +0900

    main page에 BestAll을 타겟으로 설정하고 Header랑 겹칠 때 공지사항 없애려고 시도 했는데 안되네

--- gym/pages/main.tsx
+++ gym/pages/main.tsx
@@ -11,11 +11,34 @@ import { useRef } from 'react'
 const Main = ()=>{
 
     const target = useRef<HTMLDivElement | null>(null);
+    React.useEffect(()=>{
+        const root = document.createElement('div');
+        root.style.width = '100vw';
+        root.style.height = '5vh';
+        // const headerElement = document.getElementById(styles.header);
+        //     if (headerElement) {
+        //         headerElement.appendChild(root);
+        //     }
+        const observerOptions = {
+            root: root,
+            rootMargin: '0px',
+            threshold: 1 // Adjust this threshold as needed
+          };
+        const intersectionCallback = ()=>{
+            console.log("a")
+        }
+      
+          const observer = new IntersectionObserver(intersectionCallback, observerOptions);
+          if(target.current){
+            observer.observe(target.current);
+          }
+          
+    })
     return(
         <div>
             <Header/>
             <HeaderMargin/>
-            <BestAll/>
+            <BestAll ref={target} />
             <BestTop/>
             <BestBottom/>
         </div>

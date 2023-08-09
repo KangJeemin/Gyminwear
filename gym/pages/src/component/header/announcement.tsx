848c181c1950f4067ff6bdcaa68fe0c44481706b
jiminKang <wlals1234560@gmail.com>
2023-08-09 11:31:38 +0900

    공지사항 컨포넌트 스크롤시 크기 조절 이벤트 추가

--- gym/pages/src/component/header/announcement.tsx
+++ gym/pages/src/component/header/announcement.tsx
@@ -2,34 +2,54 @@ import { motion } from 'framer-motion';
 import styles from './hambergerModal.module.css'
 import { useRouter } from 'next/router';
 import { Router } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../../../../public/context/authcontext';
+import { useScrollPosition, useScrollXPosition, useScrollYPosition } from 'react-use-scroll-position';
+
 
 const Announcement = () => {
+  const scrollX = useScrollXPosition();
+  const scrollY = useScrollYPosition();
+  const [announcementOpacity, setAnnouncementOpacity] = useState(1);
+  const [headerHeight, setHeaderHeight] = useState(5);
   const {announceState,setAnnounceState} = useContext(AuthContext)
   const router = useRouter();
+
+  useEffect(()=>{
+    const handleScroll = () => {
+        const scrollPosition = scrollY; // Get the current scroll position
+        const maxScroll = 50; // You can adjust this value
+  
+        // Calculate the opacity based on scroll position
+        const opacity = 1 - Math.min(scrollPosition / maxScroll, 1);
+        setAnnouncementOpacity(opacity);
+        const calculatedHeight = Math.max(0, 5 - (scrollPosition / maxScroll) * 5); // Limit between 0 and 5
+        setHeaderHeight(calculatedHeight);
+      };
+  
+      window.addEventListener('scroll', handleScroll);
+  
+      // Clean up the event listener on unmount
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
+      
+})
   return (
     <motion.div
     style={{
         width: '100vw',
-        height: '5vh',
+        height: `${headerHeight}vh`, // Apply the calculated header height
         backgroundColor: 'white',
         display : 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        
+        opacity: announcementOpacity, // Apply the calculated opacity
+        transition: 'opacity 0.5s,height 0.5s',
+               
       }}
-    //   animate={{
-    //     opacity:===0 ? [0,0] : === 1 ? [0,1] : [1,0],
-    //     height: ===0 ? ['0vh','0vh'] : === 1 ? ['0vh','95vh'] : ['95vh','0vh']
-    //   }}
-    //   transition={{
-    //     duration: ===0 ? 0 : ===1 ? 0.5 : 0.5, 
-    //     times:===0 ? [0,0] : ===1 ? [0,1] : [0,1],
-    //     ease: 'easeOut',
-    //   }}
     >
-      
+      <h1>하이</h1>
     </motion.div>
   );
 };

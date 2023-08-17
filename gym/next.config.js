6e34fc3552fc0e0b41a3e5b22b940a88e2391c89
jiminKang <wlals1234560@gmail.com>
2023-08-17 19:48:24 +0900

    웹팩 과정중에 net, tls 등등 문제가 생겨서 웹팩 설정 해줌

--- gym/next.config.js
+++ gym/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack5 : true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs : false, net : false, tls : false, cardinal : false };
+    return config;
+  },
   async rewrites() {
 		return [
 			{
--- gym/pages/api/test.tsx
+++ gym/pages/api/test.tsx
@@ -2,7 +2,13 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const db = require('../src/db/db');
 
 export default function test(req : NextApiRequest, res : NextApiResponse) {
-
+    db.connect((err:any)=>{
+        if(err){
+          console.log("DB와 연결중에 에러가 발생했습니다.");
+        }
+        else
+        console.log("DB와 연결 완료");
+      });
     db.query("SELECT * FROM brand",
     function (err: any, result: any) {
         if(err) {

53b5bf9656f02b31d1ca2be220dbc6e66df35437
jiminKang <wlals1234560@gmail.com>
2023-08-17 19:52:09 +0900

    데이터 베이스 코드를 이렇게 바꿔봄. 근데도 안되네

--- gym/pages/src/db/db.tsx
+++ gym/pages/src/db/db.tsx
@@ -1,12 +1,16 @@
-export {};
-const mysql = require("mysql2/promise"); // Use the promise version of mysql2
-const pool = mysql.createPool({
-    user: process.env.NEXT_PUBLIC_DATABASE_USER,
-    password: process.env.NEXT_PUBLIC_DATABASE_PASSWARD, // Fix typo here
-    host: 'localhost',
-    port: process.env.NEXT_PUBLIC_DATABASE_PORT,
-    database: process.env.NEXT_PUBLIC_DATABASE_DATABASE,
-    connectionLimit: 10, // Adjust this value based on your needs
-});
+const mysql =() => {
 
-module.exports = pool;
+    const mysql = require("mysql2"); // Use the promise version of mysql2
+    const db = mysql.createConnection({
+        user: process.env.NEXT_PUBLIC_DATABASE_USER,
+        password: process.env.NEXT_PUBLIC_DATABASE_PASSWARD, // Fix typo here
+        host: 'localhost',
+        port: process.env.NEXT_PUBLIC_DATABASE_PORT,
+        database: process.env.NEXT_PUBLIC_DATABASE_DATABASE,
+        connectionLimit: 10, // Adjust this value based on your needs
+    });
+
+    return(db)
+}
+
+export default mysql

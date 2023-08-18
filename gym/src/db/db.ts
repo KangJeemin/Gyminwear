e1fa18bd53a422ca8832785afe4fb6f6cc4b290f
jiminKang <wlals1234560@gmail.com>
2023-08-18 11:23:51 +0900

    내 방식에 맞게 db 코드 수정 중. 이렇게 하면 될 줄 알았는데 안되네

--- gym/src/db/db.ts
+++ gym/src/db/db.ts
@@ -1,20 +1,24 @@
-    export{}
-    const mysql = require("mysql2"); // Use the promise version of mysql2
-    let db
-    try{
-         db = mysql.createConnection({
-            user: process.env.NEXT_PUBLIC_DATABASE_USERNAME,
-            password: process.env.NEXT_PUBLIC_DATABASE_PASSWAOD,
-            host: 'localhost',
-            port: process.env.NEXT_PUBLIC_DATABASE_PORT,
-            database: process.env.NEXT_PUBLIC_DATABASE_DATABASE,
-            connectionLimit: 10, // Adjust this value based on your needs
-        });
-    }
-    catch (err){
-        console.log(err)
-    }
-    
+export{}
+const mysql = require('mysql2')
 
-    module.exports = db
+const mysqlModule =() => {
+    let db
+        try{
+             db = mysql.createConnection({
+             user: process.env.NEXT_PUBLIC_DATABASE_USERNAME,
+             password: process.env.NEXT_PUBLIC_DATABASE_PASSWAOD,
+             host: 'localhost',
+             port: process.env.NEXT_PUBLIC_DATABASE_PORT,
+             database: process.env.NEXT_PUBLIC_DATABASE_DATABASE,
+             connectionLimit: 10, // Adjust this value based on your needs
+             });
+        }
+        catch (err){
+            console.log(err)
+        }
+    return db
+    }
+module.exports=mysqlModule
+    
+    
 

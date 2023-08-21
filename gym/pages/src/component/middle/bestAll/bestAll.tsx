5aa010f7fda4fd569a6b15a55627719523063034
jiminKang <wlals1234560@gmail.com>
2023-08-21 10:11:06 +0900

    client-side error 발생

--- gym/pages/src/component/middle/bestAll/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll/bestAll.tsx
@@ -1,51 +1,100 @@
-import React, { useState, useEffect } from 'react';
-import styles from './bestAll.module.css';
-import axios from 'axios';
+import React, { useState , useEffect} from 'react'
+import styles from './bestAll.module.css'
+import axios from 'axios'
 
 interface Item {
-    brandName: string;
-    itemName: string;
-    itemPrice: number | string;
+    brandName:string
+    itemName:string
+    itemPrice:number|string
 }
 
-const BestAll: React.FC = () => {
-    const [items, setItems] = useState<Item[]>([]);
+const BestAll:React.FC = () =>{
+    const [item,setItem] = useState([])
+    useEffect(()=>{
+        getDatabaseData()
 
-    useEffect(() => {
-        getDatabaseData();
-    }, []);
-
+    },)
+    let array
     const getDatabaseData = async () => {
-        try {
-            const response = await axios.get("/api/test");
-            const data = response.data; // 데이터 추출
-
-            const updatedItems: Item[] = data.map((itemData: any) => ({
-                brandName: itemData.brandName,
-                itemName: "헤리코든 오버핏", // 여기서 원하는 이름을 할당하거나 itemData에서 추출해 사용
-                itemPrice: `${itemData.itemPrice}₩`,
-            }));
-
-            setItems(updatedItems);
-        } catch (e) {
-            console.log(e);
+        try{
+                const res = await axios.get("/api/test");
+                setItem(res.data)
         }
-    };
+        catch(e){
+            console.log(e)
+        }
+     }
 
-    return (
-        <div id={styles.bestAll}>
-            {/* ... */}
-            {items.map((item, index) => (
-                <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                    {/* ... */}
-                    <span id={styles.bestAll_item_itemBrandName}><h4>{item.brandName}</h4></span>
-                    <span id={styles.bestAll_item_itemName}><h5>{item.itemName}</h5></span>
-                    <span id={styles.bestAll_item_itemPrice}><h5>{item.itemPrice}</h5></span>
-                </span>
-            ))}
-            {/* ... */}
+ const items:Item[] =[
+    {
+        brandName:item[0],
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'₩'
+    },
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'₩'
+    },
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'₩'
+    },
+    {
+        brandName:"brontowin",
+        itemName:"헤리코든 오버핏",
+        itemPrice:44000+'₩'
+    }
+    
+ ]
+ 
+ 
+    return(
+        <div id={styles.bestAll}> 
+            <div id={styles.bestAll_topText}>
+                <h3 id={styles.bestAll_text}>이번 주 인기 상품</h3>
+            </div>
+            <div id={styles.bestAll_itemContainer_flexNowrap}>
+                <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                    {items.map((item, index) => (
+                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                          <span id={styles.bestAll_item_imageSize}></span>
+                          <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.bestAll_item_itemBrandName}><h4>{item.brandName}</h4></span>
+                            <span id={styles.bestAll_item_itemName}><h5>{item.itemName}</h5></span>
+                            <span id={styles.bestAll_item_itemPrice}><h5>{item.itemPrice}</h5></span>
+                          </span>
+                        </span>
+                    ))}
+                </div>
+                <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                    {items.map((item, index) => (
+                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                          <span id={styles.bestAll_item_imageSize}></span>
+                          <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.bestAll_item_itemBrandName}><h5>{item.brandName}</h5></span>
+                            <span id={styles.bestAll_item_itemName}><h5>{item.itemName}</h5></span>
+                            <span id={styles.bestAll_item_itemPrice}><h5>{item.itemPrice}</h5></span>
+                          </span>
+                        </span>
+                    ))}
+                </div>
+                <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
+                    {items.map((item, index) => (
+                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                          <span id={styles.bestAll_item_imageSize}></span>
+                          <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
+                            <span id={styles.bestAll_item_itemBrandName}><h3>{item.brandName}</h3></span>
+                            <span id={styles.bestAll_item_itemName}><h4>{item.itemName}</h4></span>
+                            <span id={styles.bestAll_item_itemPrice}><h4>{item.itemPrice}</h4></span>
+                          </span>
+                        </span>
+                    ))}
+                </div>
+            </div>
         </div>
-    );
-};
+    )
+}
 
 export default BestAll;

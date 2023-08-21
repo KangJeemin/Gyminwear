b482e979e2c3beaebe95fdca5861818ce5ea5022
jiminKang <wlals1234560@gmail.com>
2023-08-21 11:43:40 +0900

    getitem을 콘솔로 찍어보니까 undefined 가 나온다. 해결해보자

--- gym/pages/src/component/middle/bestAll/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll/bestAll.tsx
@@ -3,13 +3,13 @@ import styles from './bestAll.module.css'
 import axios from 'axios'
 
 interface Item {
-    brandName:string
+    brandName:any
     itemName:string
     itemPrice:number|string
 }
 
 const BestAll:React.FC = () =>{
-    const [item,setItem] = useState<Item[]>([]);
+    const [getitem,setItem] = useState<Item[]>([]);
     useEffect(()=>{
         getDatabaseData()
 
@@ -19,6 +19,7 @@ const BestAll:React.FC = () =>{
                 const res = await axios.get("/api/test");
                 console.log(res.data)
                 setItem(res.data)
+                console.log(getitem[0])
         }
         catch(e){
             console.log(e)
@@ -26,22 +27,22 @@ const BestAll:React.FC = () =>{
      }
  const items:Item[] =[
     {
-        brandName:item[0].brandName,
+        brandName:'본투윈',
         itemName:"헤리코든 오버핏",
         itemPrice:44000+'₩'
     },
     {
-        brandName:"brontowin",
+        brandName:'본투윈',
         itemName:"헤리코든 오버핏",
         itemPrice:44000+'₩'
     },
     {
-        brandName:"brontowin",
+        brandName:'본투윈',
         itemName:"헤리코든 오버핏",
         itemPrice:44000+'₩'
     },
     {
-        brandName:"brontowin",
+        brandName:'본투윈',
         itemName:"헤리코든 오버핏",
         itemPrice:44000+'₩'
     }
@@ -60,7 +61,7 @@ const BestAll:React.FC = () =>{
                         <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                           <span id={styles.bestAll_item_imageSize}></span>
                           <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.bestAll_item_itemBrandName}><h4>{item.brandName}</h4></span>
+                            <span id={styles.bestAll_item_itemBrandName}><h4>{item.brandName.toString()}</h4></span>
                             <span id={styles.bestAll_item_itemName}><h5>{item.itemName}</h5></span>
                             <span id={styles.bestAll_item_itemPrice}><h5>{item.itemPrice}</h5></span>
                           </span>
@@ -72,7 +73,7 @@ const BestAll:React.FC = () =>{
                         <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                           <span id={styles.bestAll_item_imageSize}></span>
                           <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.bestAll_item_itemBrandName}><h5>{item.brandName}</h5></span>
+                            <span id={styles.bestAll_item_itemBrandName}><h5>{item.brandName.toString()}</h5></span>
                             <span id={styles.bestAll_item_itemName}><h5>{item.itemName}</h5></span>
                             <span id={styles.bestAll_item_itemPrice}><h5>{item.itemPrice}</h5></span>
                           </span>
@@ -84,7 +85,7 @@ const BestAll:React.FC = () =>{
                         <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                           <span id={styles.bestAll_item_imageSize}></span>
                           <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.bestAll_item_itemBrandName}><h3>{item.brandName}</h3></span>
+                            <span id={styles.bestAll_item_itemBrandName}><h3>{item.brandName.toString()}</h3></span>
                             <span id={styles.bestAll_item_itemName}><h4>{item.itemName}</h4></span>
                             <span id={styles.bestAll_item_itemPrice}><h4>{item.itemPrice}</h4></span>
                           </span>

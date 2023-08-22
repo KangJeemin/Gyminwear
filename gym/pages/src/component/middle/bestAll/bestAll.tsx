d57f912e0d3605f957f781ccc76b84ce60c83034
jiminKang <wlals1234560@gmail.com>
2023-08-22 10:47:37 +0900

    가로 스크롤 없애기로 헀으니까

--- gym/pages/src/component/middle/bestAll/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll/bestAll.tsx
@@ -72,30 +72,6 @@ const BestAll:React.FC = () =>{
                           </span>
                         </span>
                 </div>
-                <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    {items.map((item, index) => (
-                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.bestAll_item_imageSize}></span>
-                          <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.bestAll_item_itemBrandName}><h5>{item.brandName.toString()}</h5></span>
-                            <span id={styles.bestAll_item_itemName}><h5>{item.itemName}</h5></span>
-                            <span id={styles.bestAll_item_itemPrice}><h5>{item.itemPrice}</h5></span>
-                          </span>
-                        </span>
-                    ))}
-                </div>
-                <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    {items.map((item, index) => (
-                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.bestAll_item_imageSize}></span>
-                          <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.bestAll_item_itemBrandName}><h3>{item.brandName.toString()}</h3></span>
-                            <span id={styles.bestAll_item_itemName}><h4>{item.itemName}</h4></span>
-                            <span id={styles.bestAll_item_itemPrice}><h4>{item.itemPrice}</h4></span>
-                          </span>
-                        </span>
-                    ))}
-                </div>
             </div>
         </div>
     )

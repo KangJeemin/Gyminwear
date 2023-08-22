679637e74a4324447f7bdbbf39812fe26662e875
jiminKang <wlals1234560@gmail.com>
2023-08-22 15:15:40 +0900

    이미지 파일이 한글로 되어 있어도 되는 것을 확인

--- gym/pages/src/component/middle/bestAll/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll/bestAll.tsx
@@ -41,7 +41,7 @@ const BestAll: React.FC = () => {
                         <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                             <span id={styles.bestAll_item_imageSize}>
                                 <Image
-                                    src={require('public/image/arder.png')}
+                                    src={require('public/image/머슬암드 레드.png')}
                                     alt='이미지 표시 불가'
                                     layout='fixed'
                                     width={80}

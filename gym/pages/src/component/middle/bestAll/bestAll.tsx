44995977d461de5742284604e5ee8d2c6446ddae
jiminKang <wlals1234560@gmail.com>
2023-08-22 12:44:58 +0900

    성공했다... 퍼킹 오예 git add .git add .git add .git add .git add . 지피티가 알려줬음.  interface 부분에 key값인 brandname을 지정해주고, 객체 값이 담길 useState의 속성 값으로 부여 .

--- gym/pages/src/component/middle/bestAll/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll/bestAll.tsx
@@ -1,45 +1,50 @@
-import React, { useState , useEffect} from 'react'
-import styles from './bestAll.module.css'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import styles from './bestAll.module.css';
+import axios from 'axios';
 
-const BestAll:React.FC = () =>{
-    const [getDatabase,setGetDatabase] = useState([]);
+interface Item {
+    brandname: string;
+    // Add other properties here if needed
+}
+
+const BestAll: React.FC = () => {
+    const [getDatabase, setGetDatabase] = useState<Item[]>([]);
+
     async function fetchData() {
         try {
             const response = await axios.get("/api/test");
-            const data = response.data; // Extract the data property from the response
-            setGetDatabase(JSON.parse(data)); // Update the state with the fetched data
-            
+            const data: Item[] = response.data; // Assuming response.data is an array of items
+            setGetDatabase(data);
         } catch (error) {
             console.error(error);
         }
     }
+
     useEffect(() => {
         fetchData();
-    }, ); 
-    useEffect(() => {
+    }, []);
 
-    }, [getDatabase]);
- 
-    return(
-        <div id={styles.bestAll}> 
+    return (
+        <div id={styles.bestAll}>
             <div id={styles.bestAll_topText}>
                 <h3 id={styles.bestAll_text}>이번 주 인기 상품</h3>
             </div>
             <div id={styles.bestAll_itemContainer_flexNowrap}>
                 <div id={styles.bestAll_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    <span  id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                        <span id={styles.bestAll_item_imageSize}></span>
-                        <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
-                        <span id={styles.bestAll_item_itemBrandName}><h4>{getDatabase}</h4></span>
-                        <span id={styles.bestAll_item_itemName}><h5>itemName</h5></span>
-                        <span id={styles.bestAll_item_itemPrice}><h5>itemPrice</h5></span>
+                    {getDatabase.map((object, index) => (
+                        <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                            <span id={styles.bestAll_item_imageSize}></span>
+                            <span id={styles.bestAll_item_textBoxSize} className={`${styles.flex_column}`}>
+                                <span id={styles.bestAll_item_itemBrandName}><h4>{object.brandname}</h4></span>
+                                <span id={styles.bestAll_item_itemName}><h5>itemName</h5></span>
+                                <span id={styles.bestAll_item_itemPrice}><h5>itemPrice</h5></span>
+                            </span>
                         </span>
-                    </span>
+                    ))}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default BestAll;

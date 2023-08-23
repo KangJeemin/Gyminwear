8d963aefbe84e4e444acf7000ae2e64a167292c6
jiminKang <wlals1234560@gmail.com>
2023-08-23 14:33:57 +0900

    데이터베이스 안의 이미지의 경로가 public/image 로 시작 되었었는데, public을 지움. 그러니 이미지 나오네

--- gym/pages/src/component/middle/bestAll/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll/bestAll.tsx
@@ -49,7 +49,7 @@ const BestAll: React.FC = () => {
                         <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                             <span id={styles.bestAll_item_imageSize}>
                                 <Image
-                                    src='/image/physicalcrown/매쉬_럭비져지.jpeg'
+                                    src={object.image}
                                     alt='이미지 표시 불가'
                                     layout='fixed'
                                     width={80}

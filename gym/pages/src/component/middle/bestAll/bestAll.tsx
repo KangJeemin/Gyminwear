9edc3a6d16d86d1706966d0d0619cc7d29264401
jiminKang <wlals1234560@gmail.com>
2023-08-23 14:31:40 +0900

    파일 경로로 이미지 뛰우기 성공. next/Image 컨포넌트는 경로 시작이 public 폴더임 .

--- gym/pages/src/component/middle/bestAll/bestAll.tsx
+++ gym/pages/src/component/middle/bestAll/bestAll.tsx
@@ -49,7 +49,7 @@ const BestAll: React.FC = () => {
                         <span key={index} id={styles.bestAll_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                             <span id={styles.bestAll_item_imageSize}>
                                 <Image
-                                    src={object.image}
+                                    src='/image/physicalcrown/매쉬_럭비져지.jpeg'
                                     alt='이미지 표시 불가'
                                     layout='fixed'
                                     width={80}

2e778b4112a05304c40395e41da12ef5df44de75
jiminKang <wlals1234560@gmail.com>
2023-08-24 11:08:04 +0900

    useSearchParams 사용법 숙지 중. 근데 안되네

--- gym/pages/api/search.ts
+++ gym/pages/api/search.ts
@@ -1,8 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { useSearchParams } from 'react-router-dom';
 const db = require('../../src/db/db')
 
-export default function test(req : NextApiRequest, res : NextApiResponse) {
-    console.log(req)
+
+export default function Search(req : NextApiRequest, res : NextApiResponse) {
+    const [searchParams,setSearchParams] = useSearchParams()
+    const a = searchParams.get('sort')
+    console.log(a)
     let searchText = req.body
     db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchText}%' OR productname LIKE '%${searchText}%'`,
     function (err: any, result: any) {

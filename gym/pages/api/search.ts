2ed51b0b9fd33cbd235864efac3b8e2d9845cc02
jiminKang <wlals1234560@gmail.com>
2023-08-24 13:37:10 +0900

    데이터 베이스 검색 목록과, 검색 결과 갯수가 찍히는 것을 확인

--- gym/pages/api/search.ts
+++ gym/pages/api/search.ts
@@ -6,28 +6,29 @@ const db = require('../../src/db/db')
 export default function Search(req : NextApiRequest, res : NextApiResponse) {
     
     const searchstring = req.query.result
-    // db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-    // function (err: any, result: any) {
-    //     if(err) {
-    //         res.status(500).json({ error: 'An error occurred in err' });
-    //     } else {
-    //         db.query(`SELECT COUNT(*) FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-    //         function (counterr: any, countresult: any) {
-    //         if(counterr) {
-    //            res.status(500).json({ error: 'An error occurred in counterr' });
-    //         } else {
-    //             res.json({ result, countresult });
-    //         }
-    //         });                
-    //     }
-    // });
     db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
     function (err: any, result: any) {
         if(err) {
-            console.log(err)
+            res.status(500).json({ error: 'An error occurred in err' });
         } else {
-            res.json(result);
-            console.log(result)
+            db.query(`SELECT COUNT(*) FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+            function (counterr: any, countresult: any) {
+            if(counterr) {
+               res.status(500).json({ error: 'An error occurred in counterr' });
+            } else {
+                console.log('result1=',result)
+                console.log('result2=',countresult)
+            }
+            });                
         }
     });
+    // db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+    // function (err: any, result: any) {
+    //     if(err) {
+    //         console.log(err)
+    //     } else {
+    //         res.json(result);
+    //         console.log(result)
+    //     }
+    // });
 }

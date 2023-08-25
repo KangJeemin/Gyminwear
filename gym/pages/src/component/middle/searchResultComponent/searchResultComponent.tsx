9d2430f77e915656166fa60473fbc74cd3320c96
jiminKang <wlals1234560@gmail.com>
2023-08-25 17:18:28 +0900

    검색 결과 갯수가 20보다 작으면, 숫자 네비게이션 바 렌더링 안함.

--- gym/pages/src/component/middle/searchResultComponent/searchResultComponent.tsx
+++ gym/pages/src/component/middle/searchResultComponent/searchResultComponent.tsx
@@ -5,11 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass,faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 import { AuthContext } from '@/public/context/authcontext';
 import Image from 'next/image'
-interface Item {
-  brandName: string;
-  itemName: string;
-  itemPrice: number | string;
-}
 
 const SearchResult: React.FC = () => { 
     const target = useRef<HTMLDivElement | null>(null);
@@ -69,95 +64,15 @@ const SearchResult: React.FC = () => {
                 .then(res=> res.json())
                 .then(data=>{
                     setSearchResultData(data.result)
-                    setSearchResultCount(data.countresult[0].C)
+                    setSearchResultCount(data.result.length)
+                    console.log(data.result.length)
                 })
             }
-        const items:Item[] =[
-          {
-              brandName:"brontowin",
-                
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin1",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin2",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin3",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin4",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin5",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin6",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin7",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin8",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin9",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin10",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin11",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin12",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin13",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin14",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin15",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },    
- ]
+
     useEffect(()=>{
         searchDataAPI()
     },[searchWord])
+
   return (
     <div id={styles.searchResultComponent}>
         <div id={styles.searchResultComponent_searchContainer}
@@ -198,26 +113,18 @@ const SearchResult: React.FC = () => {
                             </span>
                         </span>
                     ))}
-            {/* {items.map((item, index) => (
-                <span key={index} id={styles.searchResultComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}` }>
-                <span id={styles.searchResultComponent_item_imageSize}></span>
-                <span id={styles.searchResultComponent_item_textBoxSize} className={`${styles.flex_column}`}>
-                    <span id={styles.searchResultComponent_item_itemBrandName}><h1>{item.brandName}</h1></span>
-                    <span id={styles.searchResultComponent_item_itemName}><h2>{item.itemName}</h2></span>
-                    <span id={styles.searchResultComponent_item_itemPrice}><h3>{item.itemPrice}</h3></span>
-                </span>
-                </span>
-                ))} */}
         </div>
-        <div id ={styles.searchResultComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`} ref={target}>
-                <span className={`${styles.width_15per} ${styles.text_set_center}`}></span>
-                <span className={`${styles.width_15per} ${styles.text_set_center}`}></span>
-                <span className={`${styles.width_15per} ${styles.text_set_center}`}>1/3</span>
-                        
-                <span className={`${styles.width_15per} ${styles.text_set_center}`} onClick={()=>{
-                }}>&#62;</span>
-                <span className={`${styles.width_15per} ${styles.text_set_center}`}>&#187;</span>
-        </div>
+        {searchResultCount > 20 ? (
+            <div id ={styles.searchResultComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`} ref={target}>
+            <span className={`${styles.width_15per} ${styles.text_set_center}`}></span>
+            <span className={`${styles.width_15per} ${styles.text_set_center}`}></span>
+            <span className={`${styles.width_15per} ${styles.text_set_center}`}>1/3</span>
+                    
+            <span className={`${styles.width_15per} ${styles.text_set_center}`} onClick={()=>{
+            }}>&#62;</span>
+            <span className={`${styles.width_15per} ${styles.text_set_center}`}>&#187;</span>
+    </div>
+        ): null}
     </div>
   );
 };

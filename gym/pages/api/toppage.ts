4739341e42688758cd5b144a2314fa7119f7dc03
jiminKang <wlals1234560@gmail.com>
2023-08-31 12:49:06 +0900

    페이지의 번호에 따라 api 제어 시도

--- gym/pages/api/toppage.ts
+++ gym/pages/api/toppage.ts
@@ -2,14 +2,19 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 const db = require('../../src/db/db')
 
 export default function test(req : NextApiRequest, res : NextApiResponse) {
-    const pageNumber = req.query.result
-    console.log(pageNumber)
-    // db.query("SELECT * FROM top WHERE topid < 6",
-    // function (err: any, result: any) {
-    //     if(err) {
-    //         console.log(err)
-    //     } else {
-    //         res.json(result);
-    //     }
-    // });
+    const pageNumber= req.query.result
+    if(pageNumber==='1'){
+        db.query("SELECT * FROM top WHERE topid < 6",
+        function (err: any, result: any) {
+        if(err) {
+            console.log(err)
+        } else {
+            res.json(result);
+        }
+        });
+    }
+    else{
+        console.log("페이지 1의 아이템 정보 요청 중 에러 발생")
+    }
+    
 }

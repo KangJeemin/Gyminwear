800932fb9bdd82132d8f0e06f59512a5b4c9d224
jiminKang <wlals1234560@gmail.com>
2023-08-31 16:01:15 +0900

    toppage에 headermargin 적용

--- gym/pages/topPage.tsx
+++ gym/pages/topPage.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 import Header from './src/component/header/header'
+import HeaderMargin from './src/component/header/headerMargin'
 import TopComponent1 from './src/component/middle/topComponent/topComponent1'
-import SwipeableTemporaryDrawer from './src/component/header/drawer'
 const topPage = () => {
     return (
         
         <div>
         <Header/>
+        <HeaderMargin/>
         <TopComponent1/>
         </div>
     )

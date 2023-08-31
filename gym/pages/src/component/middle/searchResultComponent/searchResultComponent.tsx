c27cd9a197f99949f5e70b9ed5666d76f171d5c3
jiminKang <wlals1234560@gmail.com>
2023-08-31 16:27:46 +0900

    header의 z-index를 변화하니까 전에 검색 결과 컨포넌트에서도 아이템이 겹치던 문제 해결되네

--- gym/pages/src/component/middle/searchResultComponent/searchResultComponent.tsx
+++ gym/pages/src/component/middle/searchResultComponent/searchResultComponent.tsx
@@ -121,9 +121,6 @@ const SearchResult: React.FC = () => {
                         <span key={index} id={styles.searchResultComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                             <span id={styles.searchResultComponent_item_imageSize}>
                                 <Image
-                                    style={{
-                                        display:hambergerState !=1 && searchState !=1 ? '' : 'none'
-                                    }}
                                     src={object.image}
                                     alt='이미지 표시 불가'
                                     layout='fill'

7ef8a5c4510e63d775e16e342791faf70d73c777
jiminKang <wlals1234560@gmail.com>
2023-08-31 16:36:37 +0900

    top 화면 텍스트 배경화면 및 크기 조절. 배경색 한번 더 체크해봐야할듯? 조금 칙칙한거같기도?

--- gym/pages/src/component/middle/topComponent/topComponent1.tsx
+++ gym/pages/src/component/middle/topComponent/topComponent1.tsx
@@ -19,21 +19,9 @@ const TopComponent1: React.FC = () => {
  
   return (
     <div id={styles.topComponent}>
-        <div id={styles.topComponent_topText}>
-          <h1 id={styles.topComponent_text}>상의</h1>
+        <div id={styles.topComponent_topText}  className={styles.text_set_center}>
+          <h3 id={styles.topComponent_text}>Top</h3>
         </div>
-            {/* <div id={styles.topComponent_itemContainer} className={`${styles.grid_8x2} ${styles.flex_scrollSet}`}>
-            {items.map((item, index) => (
-                <span key={index} id={styles.topComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                <span id={styles.topComponent_item_imageSize}></span>
-                <span id={styles.topComponent_item_textBoxSize} className={`${styles.flex_column}`}>
-                    <span id={styles.topComponent_item_itemBrandName}><h1>{item.brandName}</h1></span>
-                    <span id={styles.topComponent_item_itemName}><h2>{item.itemName}</h2></span>
-                    <span id={styles.topComponent_item_itemPrice}><h3>{item.itemPrice}</h3></span>
-                </span>
-                </span>
-                ))}
-            </div> */}
              <div id={styles.topComponent_itemContainer} className={`${styles.grid_1x2} ${styles.flex_scrollSet}`}>
                      {topData.map((object, index) => (
                         <span key={index} id={styles.topComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>

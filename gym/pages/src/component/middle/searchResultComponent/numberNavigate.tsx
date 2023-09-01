37cc1a2a357c9cf4661eadf62f51812d4c75fd9d
jiminKang <wlals1234560@gmail.com>
2023-09-01 18:47:18 +0900

    NumberNavigate는 jsx 구성요소로 쓸 수 없다는 오류

--- gym/pages/src/component/middle/searchResultComponent/numberNavigate.tsx
+++ gym/pages/src/component/middle/searchResultComponent/numberNavigate.tsx
@@ -2,12 +2,15 @@
 import { PropaneSharp } from "@mui/icons-material";
 import React from "react";
 import styles from './topComponent.module.css';
+interface NumberNavigateProps {
+    number: number; // 숫자 타입으로 정의
+}
 
 
-function NumberNavigate(props:number) {
-    if(props>20){
+function NumberNavigate(props:NumberNavigateProps) {
+    if(props.number>20){
         return(
-            <div id ={styles.searchResultComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`} ref={target}>
+            <div id ={styles.searchResultComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`}>
                <span className={`${styles.width_15per} ${styles.text_set_center}`}>&#60;</span>
                <span className={`${styles.width_15per} ${styles.text_set_center}`}>1</span>
                <span className={`${styles.width_15per} ${styles.text_set_center}`} onClick={()=>{}}>&#62;</span>

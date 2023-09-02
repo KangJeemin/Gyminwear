6a60e4d62e8bf2cad34fd39af3590647f12b4858
jiminKang <wlals1234560@gmail.com>
2023-09-02 09:23:44 +0900

    숫자범위렌더링 문법 문제 해결

--- gym/pages/src/component/middle/searchResultComponent/numberNavigate.tsx
+++ gym/pages/src/component/middle/searchResultComponent/numberNavigate.tsx
@@ -8,11 +8,27 @@ interface NumberNavigateProps {
 
 
 function NumberNavigate(props:NumberNavigateProps) {
-    if(props.number>20){
+    if(props.number<20){
+        return(
+            null     
+        )
+    }
+    else if(20<props.number &&props.number <40){
         return(
             <div id ={styles.searchResultComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`}>
                <span className={`${styles.width_15per} ${styles.text_set_center}`}>&#60;</span>
                <span className={`${styles.width_15per} ${styles.text_set_center}`}>1</span>
+               <span className={`${styles.width_15per} ${styles.text_set_center}`}>/2</span>
+               <span className={`${styles.width_15per} ${styles.text_set_center}`} onClick={()=>{}}>&#62;</span>
+            </div>
+        )
+    }
+    else if(40<props.number && props.number <60){
+        return(
+             <div id ={styles.searchResultComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`}>
+               <span className={`${styles.width_15per} ${styles.text_set_center}`}>&#60;</span>
+               <span className={`${styles.width_15per} ${styles.text_set_center}`}>1</span>
+               <span className={`${styles.width_15per} ${styles.text_set_center}`}>/2</span>
                <span className={`${styles.width_15per} ${styles.text_set_center}`} onClick={()=>{}}>&#62;</span>
             </div>
         )

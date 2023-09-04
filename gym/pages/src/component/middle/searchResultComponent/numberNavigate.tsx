a92a704c34bbf9c6bf1d90e39ea933b2fa482de1
jiminKang <wlals1234560@gmail.com>
2023-09-04 11:22:01 +0900

    검색 결과 컨포넌트에 일단 20~40 개 일때 렌더링 할 숫자 네비게이트 작성

--- gym/pages/src/component/middle/searchResultComponent/numberNavigate.tsx
+++ gym/pages/src/component/middle/searchResultComponent/numberNavigate.tsx
@@ -1,5 +1,4 @@
-
-import React from "react";
+import React,{useState} from "react";
 import styles from './searchResultComponent.module.css';
 
 interface NumberNavigateProps {
@@ -8,6 +7,7 @@ interface NumberNavigateProps {
 
 
 function NumberNavigate(props:NumberNavigateProps) {
+    const [pageState,setPageState]=useState<number>(1)
     if(props.number<20){
         return(
             null     
@@ -15,12 +15,18 @@ function NumberNavigate(props:NumberNavigateProps) {
     }
     else if(20<props.number &&props.number <40){
         return(
+            pageState==0? (
             <div id ={styles.searchResultComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`}>
-               <span className={`${styles.width_15per} ${styles.text_set_center}`}>&#60;</span>
-               <span className={`${styles.width_15per} ${styles.text_set_center}`}>1</span>
-               <span className={`${styles.width_15per} ${styles.text_set_center}`}>/2</span>
-               <span className={`${styles.width_15per} ${styles.text_set_center}`} onClick={()=>{}}>&#62;</span>
-            </div>
+                <span className={`${styles.width_15per} ${styles.text_set_center}`}></span>
+                <span className={`${styles.width_15per} ${styles.text_set_center}`}>&#60;</span>
+                <span className={`${styles.width_15per} ${styles.text_set_center}`}>
+                    <span className={styles.color_blue}>1</span>
+                    <span className={styles.color_black}>/2</span>
+                </span>
+                <span className={`${styles.width_15per} ${styles.text_set_center}`} onClick={()=>{}}>&#62;</span>
+                <span className={`${styles.width_15per} ${styles.text_set_center}`}></span>
+            </div>) :null 
+            
         )
     }
     else if(40<props.number && props.number <60){
@@ -28,9 +34,9 @@ function NumberNavigate(props:NumberNavigateProps) {
              <div id ={styles.searchResultComponent_navigateContainer} className={`${styles.flex_row} ${styles.justify_content_center}`}>
                <span className={`${styles.width_15per} ${styles.text_set_center}`}>&#60;</span>
                <span className={`${styles.width_15per} ${styles.text_set_center}`}>1</span>
-               <span className={`${styles.width_15per} ${styles.text_set_center}`}>/2</span>
+               <span className={`${styles.width_15per} ${styles.text_set_center}`}>/3</span>
                <span className={`${styles.width_15per} ${styles.text_set_center}`} onClick={()=>{}}>&#62;</span>
-            </div>
+            </div>  
         )
     }
     else{

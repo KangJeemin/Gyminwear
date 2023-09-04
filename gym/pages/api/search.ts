ccc2fe4ba808106f519b9eb07db9d3a7f520d2a0
jiminKang <wlals1234560@gmail.com>
2023-09-04 15:37:20 +0900

    clientSideexepction 발생

--- gym/pages/api/search.ts
+++ gym/pages/api/search.ts
@@ -4,23 +4,60 @@ const db = require('../../src/db/db')
 
 
 export default function Search(req : NextApiRequest, res : NextApiResponse) {
-    console.log(req.query)
-    const searchstring = req.query.result
-    db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-    function (err: any, result: any) {
-        if(err) {
-            res.status(500).json({ error: 'An error occurred in err' });
-        } else {
-            db.query(`SELECT COUNT(*) AS C FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-            function (counterr: any, countresult: any) {
-            if(counterr) {
-               res.status(500).json({ error: 'An error occurred in counterr' });
+    const searchstring = req.query.search;
+    const pageNumber = req.query.page;
+    if(pageNumber==='1'){
+        db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 0,20`,
+        function (err: any, result: any) {
+            if(err) {
+                res.status(500).json({ error: 'An error occurred in err' });
             } else {
-                    res.json({result,countresult})
-                // console.log('result1=',result)
-                // console.log('result2=',countresult)
+                db.query(`SELECT COUNT(*) AS C FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+                function (counterr: any, countresult: any) {
+                if(counterr) {
+                   res.status(500).json({ error: 'An error occurred in counterr' });
+                } else {
+                        res.json({result,countresult})
+                }
+                });                
             }
-            });                
-        }
-    });
+        });
+    }
+    else if(pageNumber==='2'){
+        db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 20,20`,
+        function (err: any, result: any) {
+            if(err) {
+                res.status(500).json({ error: 'An error occurred in err' });
+            } else {
+                db.query(`SELECT COUNT(*) AS C FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+                function (counterr: any, countresult: any) {
+                if(counterr) {
+                   res.status(500).json({ error: 'An error occurred in counterr' });
+                } else {
+                        res.json({result,countresult})
+                }
+                });                
+            }
+        });
+    }
+    else if(pageNumber==='3'){
+        db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 40,20`,
+        function (err: any, result: any) {
+            if(err) {
+                res.status(500).json({ error: 'An error occurred in err' });
+            } else {
+                db.query(`SELECT COUNT(*) AS C FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+                function (counterr: any, countresult: any) {
+                if(counterr) {
+                   res.status(500).json({ error: 'An error occurred in counterr' });
+                } else {
+                        res.json({result,countresult})
+                }
+                });                
+            }
+        });
+    }
+    else{
+        null
+    }
 }
--- gym/pages/src/component/middle/searchResultComponent/searchResultComponent.tsx
+++ gym/pages/src/component/middle/searchResultComponent/searchResultComponent.tsx
@@ -77,16 +77,19 @@ const SearchResult: React.FC = () => {
         // const response = axios.get(`/api/search?result=${searchWord}`)
         // console.log(response)
         
-         await fetch(`/api/search?result=${searchWord}&page=${page}`)
+         await fetch(`/api/search?search=${searchWord}&page=${page}`)
                 .then(res=> res.json())
                 .then(data=>{
                     setSearchResultDataSort20(data)
+                    setSearchResultCount(data.countresult.c)
+                    console.log(data)
                 })
             }
 
     useEffect(()=>{
         searchDataAPI(1)
     },[searchWord])
+    
 
     useEffect(()=>{
     },[searchResultDataSort20])
@@ -108,7 +111,7 @@ const SearchResult: React.FC = () => {
           <h1 id={styles.searchResultComponent_text} ref={target1}>	&#39;{searchWord}&#39;에 대한 {searchResultCount}개의 검색 결과를 발견했습니다.</h1>
         </div>
             <div id={styles.searchResultComponent_itemContainer} className={`${styles.grid_1x2} ${styles.flex_scrollSet}`} ref={target2}>
-                     {searchResultData.map((object, index) => (
+                     {searchResultDataSort20.map((object, index) => (
                         <span key={index} id={styles.searchResultComponent_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
                             <span id={styles.searchResultComponent_item_imageSize}>
                                 <Image

11fe8f94ae5ecff967a8b01df3f2591d4cfa4e3c
jiminKang <wlals1234560@gmail.com>
2023-09-05 11:19:56 +0900

    쿼리문 syntex 수정

--- gym/pages/api/bestall.ts
+++ gym/pages/api/bestall.ts
@@ -3,7 +3,7 @@ const db = require('../../src/db/db')
 
 export default function test(req : NextApiRequest, res : NextApiResponse) {
 
-    db.query("SELECT * FROM top WHERE ORDER BY date LIMIT 0,4",
+    db.query("SELECT * FROM top ORDER BY date LIMIT 0,4",
     function (err: any, result: any) {
         if(err) {
             console.log(err)

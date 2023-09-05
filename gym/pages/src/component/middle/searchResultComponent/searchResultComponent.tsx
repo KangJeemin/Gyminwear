6d9fcc0aa26141d569e81029d55bb7e001845c09
jiminKang <wlals1234560@gmail.com>
2023-09-05 13:10:30 +0900

    쿼리스트리의 key값 이름 문제도 아닌데

--- gym/pages/src/component/middle/searchResultComponent/searchResultComponent.tsx
+++ gym/pages/src/component/middle/searchResultComponent/searchResultComponent.tsx
@@ -73,7 +73,7 @@ const SearchResult: React.FC = () => {
         }
     }
     const searchDataAPI = async (page:number) =>{
-         await fetch(`/api/search?search=${searchWord}&page=${page}`)
+         await fetch(`/api/search?result=${searchWord}&page=${page}`)
                 .then(res=> res.json())
                 .then(data=>{
                     setSearchResultDataSort20(data.result)

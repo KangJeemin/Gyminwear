b78eceae81dd7742bbe32ba4e2a51d6d0f7098c2
jiminKang <wlals1234560@gmail.com>
2023-09-06 15:34:02 +0900

    bestall 쿼리문 좋아요 상위4개 출력 되도록 수정

--- gym/pages/api/bestall.ts
+++ gym/pages/api/bestall.ts
@@ -3,7 +3,7 @@ const db = require('../../src/db/db')
 
 export default function bestall(req : NextApiRequest, res : NextApiResponse) {
 
-    db.query("SELECT * FROM top ORDER BY date DESC LIMIT 0,4",
+    db.query("SELECT * FROM gym.bottom UNION SELECT * FROM gym.top ORDER BY likecount DESC LIMIT 0,4",
     function (err: any, result: any) {
         if(err) {
             console.log(err)

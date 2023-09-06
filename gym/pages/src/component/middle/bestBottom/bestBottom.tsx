11affdb1803cb2cad27315871245bcb8e533803b
jiminKang <wlals1234560@gmail.com>
2023-09-06 15:03:50 +0900

    bestbottom 레이아웃 제작 시작

--- gym/pages/src/component/middle/bestBottom/bestBottom.tsx
+++ gym/pages/src/component/middle/bestBottom/bestBottom.tsx
@@ -1,83 +1,72 @@
-import React, { useState } from 'react'
-import styles from './bestBottom.module.css'
+import React, { useState, useEffect, useContext } from 'react';
+import styles from './bestBottom.module.css';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import axios from 'axios';
+import { AuthContext } from '@/public/context/authcontext';
+import convertWon from '@/pages/src/module/convertWon';
 
 interface Item {
-    brandName:string
-    itemName:string
-    itemPrice:number|string
+    brandname: string;
+    image: string;
+    productname: string;
+    price: number;
+    url: string;
+    likecount: number;
+
+    // Add other properties here if needed
 }
 
-const BestBottom:React.FC = () =>{
- const items:Item[] =[
-    {
-        brandName:"brontowin",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
-    },
-    {
-        brandName:"brontowin",
-        itemName:"헤리코든 오버핏",
-        itemPrice:44000+'원'
+const BestBottom: React.FC = () => {
+    const {hambergerState,searchState} = useContext(AuthContext)
+    const [getDatabase, setGetDatabase] = useState<Item[]>([]);
+    const router = useRouter();
+    async function fetchData() {
+        try {
+            const response = await axios.get("/api/bestbottom");
+            const data: Item[] = response.data; // Assuming response.data is an array of items
+            setGetDatabase(data);
+        } catch (error) {
+            console.error(error);
+        }
     }
-    
- ]
- 
- 
-    return(
-        <div id={styles.bestBottom}> 
-            <div id={styles.bestBottom_BottomText} className={styles.setTextCenter}>
-                <h3 id={styles.bestBottom_text} >이번 주 인기 하의</h3>
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    return (
+        <div id={styles.bestBottom}
+        style={{
+        }}
+        >
+            <div id={styles.bestBottom_topText}>
+                <h3 id={styles.bestBottom_text}>이번 주 인기 짐웨어</h3>
             </div>
             <div id={styles.bestBottom_itemContainer_flexNowrap}>
                 <div id={styles.bestBottom_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    {items.map((item, index) => (
+                    {getDatabase.map((object, index) => (
                         <span key={index} id={styles.bestBottom_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.bestBottom_item_imageSize}></span>
-                          <span id={styles.bestBottom_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.bestBottom_item_itemBrandName}><h4>{item.brandName}</h4></span>
-                            <span id={styles.bestBottom_item_itemName}><h5>{item.itemName}</h5></span>
-                            <span id={styles.bestBottom_item_itemPrice}><h5>{item.itemPrice}</h5></span>
-                          </span>
-                        </span>
-                    ))}
-                </div>
-                <div id={styles.bestBottom_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    {items.map((item, index) => (
-                        <span key={index} id={styles.bestBottom_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.bestBottom_item_imageSize}></span>
-                          <span id={styles.bestBottom_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.bestBottom_item_itemBrandName}><h4>{item.brandName}</h4></span>
-                            <span id={styles.bestBottom_item_itemName}><h5>{item.itemName}</h5></span>
-                            <span id={styles.bestBottom_item_itemPrice}><h5>{item.itemPrice}</h5></span>
-                          </span>
-                        </span>
-                    ))}
-                </div>
-                <div id={styles.bestBottom_itemContainer} className={`${styles.grid_2x2} ${styles.flex_scrollSet}`}>
-                    {items.map((item, index) => (
-                        <span key={index} id={styles.bestBottom_item_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
-                          <span id={styles.bestBottom_item_imageSize}></span>
-                          <span id={styles.bestBottom_item_textBoxSize} className={`${styles.flex_column}`}>
-                            <span id={styles.bestBottom_item_itemBrandName}><h4>{item.brandName}</h4></span>
-                            <span id={styles.bestBottom_item_itemName}><h5>{item.itemName}</h5></span>
-                            <span id={styles.bestBottom_item_itemPrice}><h5>{item.itemPrice}</h5></span>
-                          </span>
+                            <span id={styles.bestBottom_item_imageSize}>
+                                <Image
+                                    src={object.image}
+                                    alt='이미지 표시 불가'
+                                    layout='fill'
+                                    onClick={()=>{
+                                        router.push(`${object.url}`)
+                                    }}
+                                    />
+                            </span>
+                            <span id={styles.bestBottom_item_textBoxSize} className={`${styles.flex_column}`}>
+                                <span id={styles.bestBottom_item_itemBrandName} className={styles.text_set_center}><h4>{object.brandname}</h4></span>
+                                <span id={styles.bestBottom_item_itemName} className={styles.text_set_center}><h5>{object.productname}</h5></span>
+                                <span id={styles.bestBottom_item_itemPrice} className={styles.text_set_center}><h5>{convertWon(object.price)} ₩</h5></span>
+                            </span>
                         </span>
                     ))}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default BestBottom;

bdd2fd904cd93e4179f130ae7f439ae8781a048f
jiminKang <wlals1234560@gmail.com>
2023-09-07 17:57:30 +0900

    쿼리문이 잘못 되었었네, 그럼 전에도 클라이언트 사이드 에러 뜬거 이거 때문인거 같은데

--- gym/pages/api/search.ts
+++ gym/pages/api/search.ts
@@ -7,58 +7,58 @@ export default function search(req : NextApiRequest, res : NextApiResponse) {
     const searchstring = req.query.search;
     const pageNumber = req.query.page;
     
-    // if(pageNumber==='1'){
-    //     db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 0,20`,
-    //     function (err: any, result: any) {
-    //         if(err) {
-    //             res.status(500).json({ error: 'An error occurred in err' });
-    //         } else {
-    //             db.query(`SELECT COUNT(*) AS C FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-    //             function (counterr: any, countresult: any) {
-    //             if(counterr) {
-    //                res.status(500).json({ error: 'An error occurred in counterr' });
-    //             } else {
-    //                     res.json({result,countresult})
-    //             }
-    //             });                
-    //         }
-    //     });
-    // }
-    // else if(pageNumber==='2'){
-    //     db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 20,20`,
-    //     function (err: any, result: any) {
-    //         if(err) {
-    //             res.status(500).json({ error: 'An error occurred in err' });
-    //         } else {
-    //             db.query(`SELECT COUNT(*) AS C FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-    //             function (counterr: any, countresult: any) {
-    //             if(counterr) {
-    //                res.status(500).json({ error: 'An error occurred in counterr' });
-    //             } else {
-    //                     res.json({result,countresult})//
-    //             }
-    //             });                
-    //         }
-    //     });
-    // }
-    // else if(pageNumber==='3'){
-    //     db.query(`SELECT * FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 40,20`,
-    //     function (err: any, result: any) {
-    //         if(err) {
-    //             res.status(500).json({ error: 'An error occurred in err' });
-    //         } else {
-    //             db.query(`SELECT COUNT(*) AS C FROM Top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-    //             function (counterr: any, countresult: any) {
-    //             if(counterr) {
-    //                res.status(500).json({ error: 'An error occurred in counterr' });
-    //             } else {
-    //                     res.json({result,countresult})
-    //             }
-    //             });                
-    //         }
-    //     });
-    // }
-    // else{
-    //     console.log("오류 발생")
-    // }
+    if(pageNumber==='1'){
+        db.query(`SELECT * FROM gym.top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 0,20`,
+        function (err: any, result: any) {
+            if(err) {
+                res.status(500).json({ error: 'An error occurred in err' });
+            } else {
+                db.query(`SELECT COUNT(*) AS C FROM gym.top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+                function (counterr: any, countresult: any) {
+                if(counterr) {
+                   res.status(500).json({ error: 'An error occurred in counterr' });
+                } else {
+                        res.json({result,countresult})
+                }
+                });                
+            }
+        });
+    }
+    else if(pageNumber==='2'){
+        db.query(`SELECT * FROM gym.top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 20,20`,
+        function (err: any, result: any) {
+            if(err) {
+                res.status(500).json({ error: 'An error occurred in err' });
+            } else {
+                db.query(`SELECT COUNT(*) AS C FROM gym.top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+                function (counterr: any, countresult: any) {
+                if(counterr) {
+                   res.status(500).json({ error: 'An error occurred in counterr' });
+                } else {
+                        res.json({result,countresult})//
+                }
+                });                
+            }
+        });
+    }
+    else if(pageNumber==='3'){
+        db.query(`SELECT * FROM gym.top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 40,20`,
+        function (err: any, result: any) {
+            if(err) {
+                res.status(500).json({ error: 'An error occurred in err' });
+            } else {
+                db.query(`SELECT COUNT(*) AS C FROM gym.top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+                function (counterr: any, countresult: any) {
+                if(counterr) {
+                   res.status(500).json({ error: 'An error occurred in counterr' });
+                } else {
+                        res.json({result,countresult})
+                }
+                });                
+            }
+        });
+    }
+    else{
+        console.log("오류 발생")
+    }
 }

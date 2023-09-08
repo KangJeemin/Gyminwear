3be338f1d43bd05c30a849eb2dafaafd2784ef3f
jiminKang <wlals1234560@gmail.com>
2023-09-08 16:46:24 +0900

    Fc에는 할당할 수 없다는 오류 발생

--- gym/pages/searchresult/index.tsx
+++ gym/pages/searchresult/index.tsx
@@ -11,7 +11,11 @@ import axios from 'axios';
 import type { gymWearItem } from '../type/gymwear';
 import { GetServerSideProps,GetServerSidePropsContext } from 'next';
 
-const Index: React.FC = ({data}:gymWearItem) => { 
+interface gymwear{
+    data:gymWearItem;
+}
+
+const Index: React.FC = ({data}:gymwear) => { 
     
     const target = useRef<HTMLDivElement | null>(null);
     const target1 = useRef<HTMLDivElement | null>(null);

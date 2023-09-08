050e6b0a4e16430079716ef89ebdd16ed91fcf46
jiminKang <wlals1234560@gmail.com>
2023-09-08 14:29:48 +0900

    _app.tsx에 header margin 과, footer를 추가함

--- gym/pages/_app.tsx
+++ gym/pages/_app.tsx
@@ -3,13 +3,16 @@ import type { AppProps } from 'next/app'
 import { AuthProvider } from '../public/context/authcontext'
 import React from 'react'
 import Header from './src/component/header/header'
+import HeaderMargin from './src/component/header/headerMargin'
+import Footer from './src/component/footer/footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    
     <AuthProvider>
       <Header/>
+      <HeaderMargin/>
       <Component {...pageProps} />
+      <Footer/>
     </AuthProvider>
   )
 

98525ba88f33ee8f7244292a875e45e9e7e74e31
jiminKang <wlals1234560@gmail.com>
2023-09-13 12:44:27 +0900

    gymitem 에서 발생하던 렌더링 문제 해결. besttop이 내어주는 배열안의 첫번째 배열에 접근 했어야 했네  .

--- gym/pages/index.tsx
+++ gym/pages/index.tsx
@@ -4,6 +4,21 @@ import BestTop from './src/component/middle/bestTop/bestTop'
 import BestBottom from './src/component/middle/bestBottom/bestBottom'
 import Announcement from './src/component/header/announceMent/announcement'
 
+export const getStaticProps = async (context: any) => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/besttop`);
+        const res = await response.json();
+        await console.log(res) 
+        return { 
+            props: { 
+                gymitem:res.result[0]
+                 } 
+            };
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return { props: { res: [] } }; // 혹은 빈 배열 등의 기본값으로 처리
+    }
+};
 const index = ({gymitem}:any)=>{
     return(
         <div
@@ -17,21 +32,7 @@ const index = ({gymitem}:any)=>{
         </div>
     )
 }
-export const getStaticProps = async (context: any) => {
-    try {
-        const response = await fetch(`http://localhost:3000/api/besttop`);
-        const res = await response.json();
-        await console.log(res) 
-        return { 
-            props: { 
-                gymitem:res.result
-                 } 
-            };
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return { props: { res: [] } }; // 혹은 빈 배열 등의 기본값으로 처리
-    }
-};
+
 
 
 export default index;

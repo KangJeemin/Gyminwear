5ea82cb422ad23ec6851eb28b5ffe48dbc38520d
jiminKang <wlals1234560@gmail.com>
2023-09-13 17:41:25 +0900

    toppage Div에 orverscrollBehaviro 주면 footer가 안 보이네

--- gym/pages/topPage/index.tsx
+++ gym/pages/topPage/index.tsx
@@ -27,7 +27,7 @@ const Index: React.FC = () => {
   return (
     <div id={styles.topComponent}
     style={{
-        
+        overscrollBehavior:"contain"
     }}>
         <div id={styles.topComponent_topText}  className={styles.text_set_center}>
           <h3 id={styles.topComponent_text}>Top</h3>

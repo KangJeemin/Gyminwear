2ca6259eb6b468a649f35ccfd19e3862b05fdc14
jiminKang <wlals1234560@gmail.com>
2023-09-14 12:40:23 +0900

    getStaticProps 에서 api 요청 사용하지 말라고해서, 기존에 api 에 넣어 놓았던 코드를 수정하여 getStaticProps 안에 직접 작성해봄

--- gym/pages/api/bestall.ts
+++ gym/pages/api/bestall.ts
@@ -2,14 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import type { gymWearItem } from '@/src/type/gymwear';
 const db = require('../../src/db/db')
 
-export default function bestall(req : NextApiRequest, res : NextApiResponse) {
+export default function bestall() {
 
     db.query("SELECT * FROM gym.bottom UNION SELECT * FROM gym.top ORDER BY likecount DESC LIMIT 0,4",
     function (err: any, result: gymWearItem) {
         if(err) {
-            console.log(err)
+            return console.log(err)
         } else {
-            res.json(result);
+            return result;
         }
     });
 }
--- gym/pages/index.tsx
+++ gym/pages/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 import BestAll from './src/component/middle/bestAll/bestAll'
 import BestTop from './src/component/middle/bestTop/bestTop'
 import BestBottom from './src/component/middle/bestBottom/bestBottom'
 import Announcement from './src/component/header/announceMent/announcement'
+import bestall from '../pages/api/bestall'
 import getmaindata from '@/pages/src/module/fetchingMainData'
 import { gymWearItem } from '@/src/type/gymwear'
 
@@ -17,13 +20,12 @@ interface mainPageItemPropsType {
     bestBottomItem:[]
 }
 export const getStaticProps = async () => {
-    const getfetchingMainData:any = getmaindata()
-    console.log(getfetchingMainData)
+   const bestAllItem = bestall()
     return { 
         props: { 
-            bestTopItem:getfetchingMainData[1],
-            bestAllItem:getfetchingMainData[0],
-            bestBottomItem:getfetchingMainData[2]
+            
+            bestAllItem,
+            
              } 
         };
     

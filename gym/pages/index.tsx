908195fad162fd6ec8190f941b7a40403bcdaf7c
jiminKang <wlals1234560@gmail.com>
2023-09-14 11:28:20 +0900

    내가 원하는대로 정상 작동 되고 있지 않는 사앹

--- gym/pages/index.tsx
+++ gym/pages/index.tsx
@@ -3,32 +3,30 @@ import BestAll from './src/component/middle/bestAll/bestAll'
 import BestTop from './src/component/middle/bestTop/bestTop'
 import BestBottom from './src/component/middle/bestBottom/bestBottom'
 import Announcement from './src/component/header/announceMent/announcement'
+import getmaindata from './src/module/fetchingMainData'
 import { gymWearItem } from '@/src/type/gymwear'
 
+interface getmaindataFetching{
+    resBestAll:[]
+    resBestTop:[]
+    resBestBottom:[]
+}
 interface mainPageItemPropsType {
     bestTopItem:[]
     bestAllItem:[]
     bestBottomItem:[]
 }
 export const getStaticProps = async () => {
-    try {
-        const response1 = await fetch("http://localhost:3000/api/besttop");
-        const resBestTop = await response1.json();
-        const response2 = await fetch("http://localhost:3000/api/bestall");
-        const resBestAll = await response2.json();
-        const response3 = await fetch("http://localhost:3000/api/bestbottom");
-        const resBestBottom = await response3.json();
-        return { 
-            props: { 
-                bestTopItem:resBestTop,
-                bestAllItem:resBestAll,
-                bestBottomItem:resBestBottom,
-                 } 
-            };
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return { props: { res: [] } }; // 혹은 빈 배열 등의 기본값으로 처리
-    }
+    const getfetchingMainData:any = getmaindata()
+
+    return { 
+        props: { 
+            bestTopItem:getfetchingMainData.resBestTop,
+            bestAllItem:getfetchingMainData.resBestAll,
+            bestBottomItem:getfetchingMainData.resBestBottom,
+             } 
+        };
+    
 };
 const Index = ({bestTopItem,bestAllItem,bestBottomItem}:mainPageItemPropsType)=>{
     return(
--- gym/pages/src/module/fetchingMainData.ts
+++ gym/pages/src/module/fetchingMainData.ts
@@ -6,13 +6,7 @@ async function getmaindata () {
         const resBestAll = await response2.json();
         const response3 = await fetch("http://localhost:3000/api/bestbottom");
         const resBestBottom = await response3.json();
-        return { 
-            props: { 
-                bestTopItem:resBestTop,
-                bestAllItem:resBestAll,
-                bestBottomItem:resBestBottom,
-                 } 
-            };
+        return{resBestAll,resBestTop,resBestBottom}
     } catch (error) {
         console.error("Error fetching data:", error);
         return { props: { res: [] } }; // 혹은 빈 배열 등의 기본값으로 처리

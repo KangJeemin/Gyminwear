477966cfa8b02d53660f6547d718b476e240e33d
jiminKang <wlals1234560@gmail.com>
2023-09-14 12:42:37 +0900

    fetchingmaindata 에서 경로 문제 발생해서 삭제. 안 쓸듯

--- gym/pages/index.tsx
+++ gym/pages/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import type { NextApiRequest, NextApiResponse } from 'next';
-
 import BestAll from './src/component/middle/bestAll/bestAll'
 import BestTop from './src/component/middle/bestTop/bestTop'
 import BestBottom from './src/component/middle/bestBottom/bestBottom'
 import Announcement from './src/component/header/announceMent/announcement'
 import bestall from '../pages/api/bestall'
-import getmaindata from '@/pages/src/module/fetchingMainData'
 import { gymWearItem } from '@/src/type/gymwear'
 
 interface getmaindataFetching{
@@ -23,9 +20,7 @@ export const getStaticProps = async () => {
    const bestAllItem = bestall()
     return { 
         props: { 
-            
             bestAllItem,
-            
              } 
         };
     

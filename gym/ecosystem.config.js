a3b3f728f482bc693310f8052194695bec279571
jiminKang <wlals1234560@gmail.com>
2023-09-19 16:29:13 +0900

    파일 내 내용을 pm2 문서대로 수정

--- gym/ecosystem.config.js
+++ gym/ecosystem.config.js
@@ -1,22 +1,20 @@
 module.exports = {
-  apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+  apps: [
+    {
+      name: 'NextAppName',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      env_local: {
+        APP_ENV: 'local' // APP_ENV=local
+      },
+      env_development: {
+        APP_ENV: 'dev' // APP_ENV=dev
+      },
+      env_production: {
+        APP_ENV: 'prod' // APP_ENV=prod
+      }
     }
-  }
-};
+  ]
+}

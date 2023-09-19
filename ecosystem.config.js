d61c3d9bc2102691fa1286d700cfd7eca367b19d
jiminKang <wlals1234560@gmail.com>
2023-09-19 16:26:41 +0900

    ecosystem.config의 위치가 gym 안에 있어야 하네

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'gym',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
-      script: 'node_modules/next/dist/bin/next',
+      script: 'gym/node_modules/next/dist/bin/next',
       args: 'start',
       env_local: {
         APP_ENV: 'local' // APP_ENV=local

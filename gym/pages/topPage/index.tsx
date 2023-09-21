0b1e069bd3b0d5f0bbe67b0ebabe75403bd79ff6
jiminKang <wlals1234560@gmail.com>
2023-09-21 18:22:53 +0900

    pc-toppage 아이템들 잘 나오고, 컨포넌트들의 크기도 수정 했음.

--- gym/pages/topPage/index.tsx
+++ gym/pages/topPage/index.tsx
@@ -20,13 +20,13 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   const Index = ({gymitem}:GymItemProps) => {
         const router = useRouter();
     return(
-        <div id={styles.pc_topContainer}>
+        <div id={styles.pc_topContainer} className={`${styles.flex_row}`}>
             <div id={styles.pc_topLeftBox}></div>
             <div id={styles.pc_topCenterBox}>
                 <div id={styles.pc_topCenterMarginBox}></div>
                 <div id={styles.pc_topContentBox} className={`${styles.grid_5x4} ${styles.flex_scrollSet}`}>
                    {gymitem.map((object:gymWearItem, index:number) => (
-                        <span key={index} id={styles.pc_topItem_itemComponent} className={`${styles.padding_1} ${styles.flex_column}`}>
+                        <span key={index} id={styles.pc_topItem_itemComponent} className={`${styles.padding_3} ${styles.flex_column}`}>
                             <span id={styles.pc_topItem_imageSize}>
                                 <Image
                                     src={object.image}

58aba39c2e3d6ac3753533aa2a3760e0376cb823
jiminKang <wlals1234560@gmail.com>
2023-09-21 15:48:56 +0900

    피시 헤더 크기지정

--- gym/pages/src/component/header/pc/pcHeader.tsx
+++ gym/pages/src/component/header/pc/pcHeader.tsx
@@ -1,24 +1,25 @@
 import { motion } from 'framer-motion';
 import * as React from 'react';
-import styles from './header.module.css'
+import styles from './pcHeader.module.css'
 import { useRef } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Image from 'next/image';
 import Logo from '../../../../../public/image/gyminwearLogo.png'
+import { style } from '@mui/system';
 
 
 const PcHeader = () => {
-    <div id='pc-headerContainer'>
-        <div id='pc-headerLeftBox'></div>
-        <div id='pc-headerCenterBox'>
-            <div id='pc-headerLogoBox'></div>
-            <div id='pc-headerTopBox'></div>
-            <div id='pc-headerBottomBox'></div>
-            <div id='pc-headerxxxBox'></div>
-            <div id='pc-headerSearchBox'></div>
+    <div id={styles.pc_headerContainer}>
+        <div id={styles.pc_headerLeftBox}></div>
+        <div id={styles.pc_headerCenterBox}>
+            <div id={styles.pc_headerLogoBox}></div>
+            <div id={styles.pc_headerTopBox}></div>
+            <div id={styles.pc_headerBottomBox}></div>
+            <div id={styles.pc_headerxxxBox}></div>
+            <div id={styles.pc_headerSearchBox}></div>
         </div>
-        <div id='pc-headerRightBox'></div>
+        <div id={styles.pc_headerRightBox}></div>
     </div>
 
 }

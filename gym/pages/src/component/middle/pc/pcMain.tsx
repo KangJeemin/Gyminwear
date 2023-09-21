abca7cfc4e056bef9455dcd0a8244b8c6085138d
jiminKang <wlals1234560@gmail.com>
2023-09-21 16:58:22 +0900

    Pc main 페이지 레이아웃 제작 완료 .이제 css 제작하기

--- gym/pages/src/component/middle/pc/pcMain.tsx
+++ gym/pages/src/component/middle/pc/pcMain.tsx
@@ -6,7 +6,15 @@ import Image from 'next/image';
 const PcMain = () => {
     return(
         <div id='pc_mainContainer'>
-            <div id='pc_mainImage'></div>
+            <div id='pc_mainImageContainer'>
+                <div id='pc_mainImageContainerLeftBox'></div>
+                <div id='pc_mainImageContainerImageBox'>
+                    <div id='pc_mainImageContainerImageBoxLeftButton'></div>
+                    <div id='pc_mainImageContainerImageBoxImage'></div>
+                    <div id='pc_mainImageContainerImageBoxRightButton'></div>
+                </div>
+                <div id='pc_mainImageContainerRightBox'></div>
+            </div>
             <div id='pc_mainImagemargin'></div>
             <div id='pc_mainImage2'></div>
             <div id='pc_mainImagemargin'></div>

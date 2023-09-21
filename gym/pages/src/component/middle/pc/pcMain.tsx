923a3f1b558adab46fb74b59e8051db087da7238
jiminKang <wlals1234560@gmail.com>
2023-09-21 16:53:47 +0900

    메인 화면 메인 컨테이너 제작 완료

---
+++ gym/pages/src/component/middle/pc/pcMain.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import styles from './pcHeader.module.css'
+import Image from 'next/image';
+
+
+const PcMain = () => {
+    return(
+        <div id='pc_mainContainer'>
+            <div id='pc_mainImage'></div>
+            <div id='pc_mainImagemargin'></div>
+            <div id='pc_mainImage2'></div>
+            <div id='pc_mainImagemargin'></div>
+            <div id='pc_mainImage3'></div>
+        </div>
+    )
+}
+
+export default PcMain

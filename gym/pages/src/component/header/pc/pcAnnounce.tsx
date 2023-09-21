457be818b27db780d9ae026864271a02c75c879b
jiminKang <wlals1234560@gmail.com>
2023-09-21 22:59:27 +0900

    공지사항 컨포넌트 만들기 시작

---
+++ gym/pages/src/component/header/pc/pcAnnounce.tsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion';
+import * as React from 'react';
+import styles from './pcHeader.module.css'
+import { useRef } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import Image from 'next/image';
+import Logo from '../../../../../public/image/gyminwearLogo.png'
+
+
+const PcAnnounce = () => {
+    return(
+    
+    )
+}
+
+export default PcAnnounce

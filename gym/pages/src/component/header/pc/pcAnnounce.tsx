9c0e31551dc062d491228a45458b534b503a273b
jiminKang <wlals1234560@gmail.com>
2023-09-21 23:40:43 +0900

    CenterBox랑 Right 박스 안 나오고 있네

--- gym/pages/src/component/header/pc/pcAnnounce.tsx
+++ gym/pages/src/component/header/pc/pcAnnounce.tsx
@@ -12,9 +12,9 @@ import Logo from '../../../../../public/image/gyminwearLogo.png'
 const PcAnnounce = () => {
     
     return(
-        <div id={styles.pc_announceContainer}>
+        <div id={styles.pc_announceContainer} className={`${styles.flex_row}`}>
             <div id={styles.pc_announceLeftBox}></div>
-            <div id={styles.pc_announceCenterBox}>
+            <div id={styles.pc_announceCenterBox} className={`${styles.flex_row}`}>
                 <div id={styles.pc_announceLeftButton}>&#60;</div>
                 <div id={styles.pc_announceText}>덤브스트럭 23 F/W 출시</div>
                 <div id={styles.pc_announceRightButton}>&#62;</div>

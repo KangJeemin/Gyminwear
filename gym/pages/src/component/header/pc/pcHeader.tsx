f47087a6ca70eb7c15a975ab730dceea61b89230
jiminKang <wlals1234560@gmail.com>
2023-09-22 17:36:04 +0900

    topPage로 이동하게 하는 기능

--- gym/pages/src/component/header/pc/pcHeader.tsx
+++ gym/pages/src/component/header/pc/pcHeader.tsx
@@ -6,9 +6,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import Image from 'next/image';
 import Logo from '../../../../../public/image/gyminwearLogo.png'
+import { useRouter } from 'next/router';
 
 
 const PcHeader = () => {
+    const router=useRouter();
     return(
     <div id={styles.pc_headerContainer} className={`${styles.flexRow}`}>
         <div id={styles.pc_headerLeftBox}></div>
@@ -20,7 +22,9 @@ const PcHeader = () => {
                     layout='fill'                        
                 />
             </div>
-            <div id={styles.pc_headerTopBox} className={`${styles.setTextCenter}`}>Top</div>
+            <div id={styles.pc_headerTopBox} className={`${styles.setTextCenter}`} onClick={()=>{
+                router.push(`/topPage?page=1`)
+            }}>Top</div>
             <div id={styles.pc_headerBottomBox} className={`${styles.setTextCenter}`}>Bottom</div>
             <div id={styles.pc_headerxxxBox}>d</div>
             <div id={styles.pc_headerSearchBox}>

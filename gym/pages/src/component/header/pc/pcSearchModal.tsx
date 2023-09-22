8cb07a0cd981b028c1539f0582417ee3d9263426
jiminKang <wlals1234560@gmail.com>
2023-09-22 19:42:20 +0900

    검색input  및 아이콘들 생성

--- gym/pages/src/component/header/pc/pcSearchModal.tsx
+++ gym/pages/src/component/header/pc/pcSearchModal.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
 import styles from './pcSearchModal.module.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass,faCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 const PcSearchModal = () => {
     return (
@@ -7,7 +9,11 @@ const PcSearchModal = () => {
             <div id={styles.pc_searchContainerLeftBox}></div>
             <div id={styles.pc_searchContainerCenterBox} className={`${styles.flexColumn}`}>
                 <div id={styles.pc_searchContainerContentTop}></div>
-                <div id={styles.pc_searchContainerContentMiddle}></div>
+                <div id={styles.pc_searchContainerContentMiddle}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} style={{fontSize:"2em",color:"gray"}}/>
+                    <input type="text" />
+                    <FontAwesomeIcon icon={faCircleXmark} style={{fontSize:"2em",color:"gray"}}/>
+                </div>
                 <div id={styles.pc_searchContainerContentBottom}></div>
             </div>
             <div id={styles.pc_searchContainerRightBox}></div>

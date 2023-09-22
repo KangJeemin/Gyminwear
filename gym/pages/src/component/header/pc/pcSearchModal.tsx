f97953b78610c51e4e258f289be0957462033e0d
jiminKang <wlals1234560@gmail.com>
2023-09-22 19:28:24 +0900

    flex 적용

--- gym/pages/src/component/header/pc/pcSearchModal.tsx
+++ gym/pages/src/component/header/pc/pcSearchModal.tsx
@@ -3,9 +3,9 @@ import styles from './pcSearchModal.module.css'
 
 const PcSearchModal = () => {
     return (
-        <div id={styles.pc_searchContainer}>
+        <div id={styles.pc_searchContainer} className={`${styles.flexRow}`}>
             <div id={styles.pc_searchContainerLeftBox}></div>
-            <div id={styles.pc_searchContainerCenterBox}>
+            <div id={styles.pc_searchContainerCenterBox} className={`${styles.flexColumn}`}>
                 <div id={styles.pc_searchContainerContentTop}></div>
                 <div id={styles.pc_searchContainerContentMiddle}></div>
                 <div id={styles.pc_searchContainerContentBottom}></div>

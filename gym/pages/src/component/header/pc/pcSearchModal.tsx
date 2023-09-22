451c1d679658c0053fc78e557e703b628e6dc5a8
jiminKang <wlals1234560@gmail.com>
2023-09-22 21:19:24 +0900

    마우스가 헤더로 이동할때도 검색 모달이 사라지질래, 검색 모달을 헤더를 덮게 했다. 그랬더니 현재 헤더가 검색모달에 가려서 안 보이는 상태 .

--- gym/pages/src/component/header/pc/pcSearchModal.tsx
+++ gym/pages/src/component/header/pc/pcSearchModal.tsx
@@ -15,7 +15,7 @@ const PcSearchModal = (props:pcSearchState) => {
         <motion.div id={styles.pc_searchContainer} className={`${styles.flexRow}`}
         animate={{
             opacity:props.state===0 ? [0,0] : props.state=== 1 ? [0,1] : [1,0],
-            height: props.state===0 ? ['0rem','20rem'] : props.state=== 1 ? ['0rem','20rem'] : ['20rem','0rem']
+            height: props.state===0 ? ['0rem','23rem'] : props.state=== 1 ? ['0rem','23rem'] : ['23rem','0rem']
           }}
           transition={{
             duration: props.state===0 ? 0 : props.state===1 ? 0.5 : 0.5, 

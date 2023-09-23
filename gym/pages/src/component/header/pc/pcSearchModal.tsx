85aca25b9cd6db333e17181524d9aee354679578
jiminKang <wlals1234560@gmail.com>
2023-09-23 18:21:02 +0900

    검색어 입력하고 엔터 누르면 req 요청하는 기능 및 , x 누르면 검색어 사라지는 기능 구 현 완료

--- gym/pages/src/component/header/pc/pcSearchModal.tsx
+++ gym/pages/src/component/header/pc/pcSearchModal.tsx
@@ -3,6 +3,9 @@ import { motion } from 'framer-motion';
 import styles from './pcSearchModal.module.css'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass,faCircleXmark } from "@fortawesome/free-solid-svg-icons";
+import { useContext, useState, ChangeEvent,} from 'react';
+import { AuthContext } from '@/public/context/authcontext';
+import { useRouter } from 'next/router';
 
 interface pcSearchState {
     pcSearchModalState: number;
@@ -10,6 +13,20 @@ interface pcSearchState {
 }
 
 const PcSearchModal = (props:pcSearchState) => {
+    const {searchWord, setSearchWord} = useContext(AuthContext)
+    const router = useRouter()
+    const pcSetInputText = (e:ChangeEvent<HTMLInputElement>) => {
+        setSearchWord(e.target.value)
+      }
+      const keydown = (e:React.KeyboardEvent<HTMLInputElement>)=>{
+        if(e.keyCode===13){
+          router.push(`/searchresult?search=${searchWord}&page=1`)
+        }
+      }
+      const pcInitializeSearchText = () => {
+        setSearchWord("")
+      }
+
 
     return (
         <motion.div id={styles.pc_searchContainer} className={`${styles.flexRow}`}
@@ -28,8 +45,8 @@ const PcSearchModal = (props:pcSearchState) => {
                 <div id={styles.pc_searchContainerContentTop}></div>
                 <div id={styles.pc_searchContainerContentMiddle} className={`${styles.flexRow}`}>
                     <FontAwesomeIcon icon={faMagnifyingGlass} style={{fontSize:"2rem",color:"gray",marginLeft: "1rem"}}/>
-                    <input id={styles.pc_searchInput} type="text" placeholder='검색어를 입력하세요.'/>
-                    <FontAwesomeIcon icon={faCircleXmark} style={{fontSize:"2rem",color:"gray"}}/>
+                    <input id={styles.pc_searchInput} onChange={pcSetInputText} onKeyDown={keydown} value={searchWord} type="text" placeholder='검색어를 입력하세요.'/>
+                    <FontAwesomeIcon icon={faCircleXmark} style={{fontSize:"2rem",color:"gray",visibility:searchWord===""? "hidden":"visible"}} onClick={pcInitializeSearchText}/>
                 </div>
                 <div id={styles.pc_searchContainerContentBottom}></div>
             </div>

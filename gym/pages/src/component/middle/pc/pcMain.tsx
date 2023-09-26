1591e57372c0d6d005cc0396ed25315d3213062d
jiminKang <wlals1234560@gmail.com>
2023-09-26 15:08:47 +0900

    대문글 제작 완성

--- gym/pages/src/component/middle/pc/pcMain.tsx
+++ gym/pages/src/component/middle/pc/pcMain.tsx
@@ -10,7 +10,15 @@ const PcMain = () => {
                 <div id={styles.pc_mainImageContainerLeftBox}></div>
                 <div id={styles.pc_mainImageContainerImageBox} className={`${styles.flexRow}`}>
                     <div id={styles.pc_mainImageContainerImageBoxLeftButton}></div>
-                    <div id={styles.pc_mainImageContainerImageBoxImage}></div>
+                    <div id={styles.pc_mainImageContainerImageBoxImage}>
+                        <div style={{width:"100%",height:"20%"}}></div>
+                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 유행하는 짐웨어,</div>
+                        <div style={{width:"100%"}}></div>
+                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 오버핏 짐웨어,</div>
+                        <div style={{width:"100%"}}></div>
+                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 잠인웨어에서 확인해 보세요.</div>
+                        <div style={{width:"100%",height:"20%"}}></div>
+                    </div>
                     <div id={styles.pc_mainImageContainerImageBoxRightButton}></div>
                 </div>
                 <div id={styles.pc_mainImageContainerRightBox}></div>

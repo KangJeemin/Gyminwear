3e633f18962701b59b3872d7ab7a38e5daadf0f9
jiminKang <wlals1234560@gmail.com>
2023-10-03 14:38:53 +0900

    슬라이드 양 옆 눌렀을 때 useState 만들어서 상태 변화하게 설정

--- gym/src/component/middle/pc/pcSlide/pc.tsx
+++ gym/src/component/middle/pc/pcSlide/pc.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
 import styles from './index.module.css'
 import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 
 const PcSlide = () => {
+    const [slideState,setSlideState] = React.useState<number>(0)
     return(
             <div id={styles.pc_slideContainerImageBox} className={`${styles.flexRow}`}>
-                <div id={styles.pc_slideContainerImageBoxLeftButton} className={`${styles.setTextCenter}`}>&#60;</div>
+                <div id={styles.pc_slideContainerImageBoxLeftButton} className={`${styles.setTextCenter}`} onClick={()=>{
+                    setSlideState(slideState-1)
+                }}>&#60;</div>
                 <div id={styles.pc_slideContainerImageBoxImage} className={`${styles.flexRow}`}>
-                    <div id={styles.pc_slideComponent}>
+                    <motion.div id={styles.pc_slideComponent}/>
                         <div id={styles.pc_slide1} >
                             <div style={{width:"100%",height:"20%"}}></div>
                             <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> 유행하는 짐웨어,</div>
@@ -27,9 +31,10 @@ const PcSlide = () => {
                             <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> ghi.</div>
                             <div style={{width:"100%",height:"20%"}}></div>
                         </div>
-                    </div>
                 </div>
-                <div id={styles.pc_slideContainerImageBoxRightButton} className={`${styles.setTextCenter}`}>&#62;</div>
+                <div id={styles.pc_slideContainerImageBoxRightButton} className={`${styles.setTextCenter}`} onClick={()=>{
+                    setSlideState(slideState+1)
+                }}>&#62;</div>
             </div>
     )
 }

46002f462dd37fad0001801fbf23adfb2909dc65
jiminKang <wlals1234560@gmail.com>
2023-10-03 16:01:41 +0900

    flex-wrap 속성으로 넘ㅊ는 것도 안보이게 설정

--- gym/src/component/middle/pc/pcSlide/pc.tsx
+++ gym/src/component/middle/pc/pcSlide/pc.tsx
@@ -11,16 +11,7 @@ const PcSlide = () => {
                 <div id={styles.pc_slideContainerImageBoxLeftButton} className={`${styles.setTextCenter}`} onClick={()=>{
                     setSlideState(slideState-1)
                 }}>&#60;</div>
-                    <motion.div id={styles.pc_slideContainerImageBoxImage} className={`${styles.flexRow}`}
-                        animate={{
-                            clipPath: slideState === 0 ? "circle(40%)" : "inset(0px)"
-                        }}
-                        transition={{
-                            duration: slideState ===0 ? 0 : slideState===1 ? 0.5 : 0.5,
-                            ease: "linear",
-                            times: slideState ===1? [0, 0.5] : [0,0.5],
-                          }}
-                    >
+                    <motion.div id={styles.pc_slideContainerImageBoxImage}>
                         <div id={styles.pc_slide1} >
                             <div style={{width:"100%",height:"20%"}}></div>
                             <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> 유행하는 짐웨어,</div>

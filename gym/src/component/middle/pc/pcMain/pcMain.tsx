28643577358bb593003f9f49956452059c7dfb1d
jiminKang <wlals1234560@gmail.com>
2023-10-03 12:36:12 +0900

    슬라이드 컨포넌트를 pc 메인에 이식하려고 하는데 무언가 잘 못 되었다 .

--- gym/src/component/middle/pc/pcMain/pcMain.tsx
+++ gym/src/component/middle/pc/pcMain/pcMain.tsx
@@ -2,25 +2,14 @@ import * as React from 'react';
 import styles from './pcMain.module.css'
 import Image from 'next/image';
 import gbroLogo from '../../../../../public/image/gbro/gbroLogo.jpg'
+import PcSlide from '../pcSlide/pc';
 
 const PcMain = () => {
     return(
         <div id={styles.pc_mainContainer} className={`${styles.flexColumn}`}>
             <div id={styles.pc_mainImageContainer} className={`${styles.flexRow}`}>
                 <div id={styles.pc_mainImageContainerLeftBox}></div>
-                <div id={styles.pc_mainImageContainerImageBox} className={`${styles.flexRow}`}>
-                    <div id={styles.pc_mainImageContainerImageBoxLeftButton} className={`${styles.setTextCenter}`}>&#60;</div>
-                    <div id={styles.pc_mainImageContainerImageBoxImage}>
-                        <div style={{width:"100%",height:"20%"}}></div>
-                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 유행하는 짐웨어,</div>
-                        <div style={{width:"100%"}}></div>
-                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 오버핏 짐웨어,</div>
-                        <div style={{width:"100%"}}></div>
-                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 짐인웨어에서 확인해 보세요.</div>
-                        <div style={{width:"100%",height:"20%"}}></div>
-                    </div>
-                    <div id={styles.pc_mainImageContainerImageBoxRightButton} className={`${styles.setTextCenter}`}>&#62;</div>
-                </div>
+                <PcSlide/>  
                 <div id={styles.pc_mainImageContainerRightBox}></div>
             </div>
             <div id={styles.pc_mainImagemargin}></div>
--- gym/src/component/middle/pc/pcSlide/pc.tsx
+++ gym/src/component/middle/pc/pcSlide/pc.tsx
@@ -5,37 +5,22 @@ import Image from 'next/image';
 
 const PcSlide = () => {
     return(
-        <div id={styles.pc_mainContainer} className={`${styles.flexColumn}`}>
-            <div id={styles.pc_mainImageContainer} className={`${styles.flexRow}`}>
-                <div id={styles.pc_mainImageContainerLeftBox}></div>
-                <div id={styles.pc_mainImageContainerImageBox} className={`${styles.flexRow}`}>
-                    <div id={styles.pc_mainImageContainerImageBoxLeftButton} className={`${styles.setTextCenter}`}>&#60;</div>
-                    <div id={styles.pc_mainImageContainerImageBoxImage}>
-                        <div style={{width:"100%",height:"20%"}}></div>
-                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 유행하는 짐웨어,</div>
-                        <div style={{width:"100%"}}></div>
-                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 오버핏 짐웨어,</div>
-                        <div style={{width:"100%"}}></div>
-                        <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 짐인웨어에서 확인해 보세요.</div>
-                        <div style={{width:"100%",height:"20%"}}></div>
-                    </div>
-                    <div id={styles.pc_mainImageContainerImageBoxRightButton} className={`${styles.setTextCenter}`}>&#62;</div>
+        <div>
+            <div id={styles.pc_mainImageContainerImageBox} className={`${styles.flexRow}`}>
+                <div id={styles.pc_mainImageContainerImageBoxLeftButton} className={`${styles.setTextCenter}`}>&#60;</div>
+                <div id={styles.pc_mainImageContainerImageBoxImage}>
+                    <div style={{width:"100%",height:"20%"}}></div>
+                    <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 유행하는 짐웨어,</div>
+                    <div style={{width:"100%"}}></div>
+                    <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 오버핏 짐웨어,</div>
+                    <div style={{width:"100%"}}></div>
+                    <div id={styles.pc_mainImageText}  className={`${styles.setTextCenter}`}> 짐인웨어에서 확인해 보세요.</div>
+                    <div style={{width:"100%",height:"20%"}}></div>
                 </div>
-                <div id={styles.pc_mainImageContainerRightBox}></div>
+                <div id={styles.pc_mainImageContainerImageBoxRightButton} className={`${styles.setTextCenter}`}>&#62;</div>
             </div>
-            <div id={styles.pc_mainImagemargin}></div>
-            <div id={styles.pc_mainImage2}>
-                <Image
-                    src={gbroLogo}
-                    alt='이미지 표시 불가'
-                    layout='fixed'
-                    width={1280}
-                    height={480}
-                />
-            </div>
-            <div id={styles.pc_mainImagemargin}></div>
-            <div id={styles.pc_mainImage3}></div>
         </div>
+                
     )
 }
 

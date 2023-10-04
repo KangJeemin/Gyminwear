418b0f5896f387cbfe159f4971bb37f7e6602d65
jiminKang <wlals1234560@gmail.com>
2023-10-04 10:32:06 +0900

    일단 슬라이드 네비개이션부터 만들어보자

--- gym/src/component/middle/pc/pcSlide/pc.tsx
+++ gym/src/component/middle/pc/pcSlide/pc.tsx
@@ -11,32 +11,37 @@ const PcSlide = () => {
                 <div id={styles.pc_slideContainerImageBoxLeftButton} className={`${styles.setTextCenter}`} onClick={()=>{
                     setSlideState(slideState-1)
                 }}>&#60;</div>
-                    <motion.div id={styles.pc_slideContainerImageBoxImage} style={{
+                <motion.div id={styles.pc_slideContainerImageBoxImage} style={{
                         top:'0px',
                         left:'-500px'
-                    }}>
-                        <div id={styles.pc_slide1} >
-                            <div style={{width:"100%",height:"20%"}}></div>
-                            <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> 유행하는 짐웨어,</div>
-                            <div style={{width:"100%"}}></div>
-                            <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> 오버핏 짐웨어,</div>
-                            <div style={{width:"100%"}}></div>
-                            <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> 짐인웨어에서 확인해 보세요.</div>
-                            <div style={{width:"100%",height:"20%"}}></div>
-                        </div>
-                        <div id={styles.pc_slide2} >
-                            <div style={{width:"100%",height:"20%"}}></div>
-                            <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> abc,</div>
-                            <div style={{width:"100%"}}></div>
-                            <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> def,</div>
-                            <div style={{width:"100%"}}></div>
-                            <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> ghi.</div>
-                            <div style={{width:"100%",height:"20%"}}></div>
-                        </div>
-                    </motion.div>
+                }}>
+                    <div id={styles.pc_slide1} >
+                        <div style={{width:"100%",height:"20%"}}></div>
+                        <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> 유행하는 짐웨어,</div>
+                        <div style={{width:"100%"}}></div>
+                        <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> 오버핏 짐웨어,</div>
+                        <div style={{width:"100%"}}></div>
+                        <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> 짐인웨어에서 확인해 보세요.</div>
+                        <div style={{width:"100%",height:"20%"}}></div>
+                    </div>
+                    <div id={styles.pc_slide2} >
+                        <div style={{width:"100%",height:"20%"}}></div>
+                        <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> abc,</div>
+                        <div style={{width:"100%"}}></div>
+                        <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> def,</div>
+                        <div style={{width:"100%"}}></div>
+                        <div id={styles.pc_slideText}  className={`${styles.setTextCenter}`}> ghi.</div>
+                        <div style={{width:"100%",height:"20%"}}></div>
+                    </div>
+                    <div id={styles.pc_slideNavigation}>
+                        
+                    </div>
+                </motion.div>
+                
                 <div id={styles.pc_slideContainerImageBoxRightButton} className={`${styles.setTextCenter}`} onClick={()=>{
                     setSlideState(slideState+1)
-                }}>&#62;</div>
+                }}>&#62;</div>\
+                
             </div>
     )
 }

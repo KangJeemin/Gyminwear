3ea595c22afca461c5e905a1cddfcc6bf3e1e7a4
jiminKang <wlals1234560@gmail.com>
2023-10-09 20:09:14 +0900

    setInterval 함수가 동작 안 한ㄴ 듯

--- gym/src/component/middle/pc/pcSlide/pc.tsx
+++ gym/src/component/middle/pc/pcSlide/pc.tsx
@@ -59,7 +59,9 @@ const PcSlide = () => {
     },[autoSlide])
     React.useEffect(()=>{
         setSlideAnimate(5) //처음 로딩 시 애니메이션 컴포넌트의 위치 초기화.
-        autoSlideFunction()
+        setInterval(()=>{
+            setAutoSlide(autoSlide+1)
+        },1000)
     },[])
     
     return(

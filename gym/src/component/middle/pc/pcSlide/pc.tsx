3f49fb0f62c7b5a9164ddc0a118fda27b141c374
jiminKang <wlals1234560@gmail.com>
2023-10-10 09:44:11 +0900

    1초마다 콘솔값이 찍히는 것으로 interval 함수 실행하는것 확인했ㅇ,ㅁ

--- gym/src/component/middle/pc/pcSlide/pc.tsx
+++ gym/src/component/middle/pc/pcSlide/pc.tsx
@@ -60,7 +60,8 @@ const PcSlide = () => {
     React.useEffect(()=>{
         setSlideAnimate(5) //처음 로딩 시 애니메이션 컴포넌트의 위치 초기화.
         setInterval(()=>{
-            setAutoSlide(autoSlide+1)
+            // setAutoSlide(autoSlide+1)
+            console.log("hey")
         },1000)
     },[])
     

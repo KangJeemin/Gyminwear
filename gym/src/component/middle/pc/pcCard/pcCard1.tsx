1f5ba5b922d0387da807f7a7408211080e310bc9
jiminKang <wlals1234560@gmail.com>
2023-10-12 17:08:27 +0900

    card1 text 설정

--- gym/src/component/middle/pc/pcCard/pcCard1.tsx
+++ gym/src/component/middle/pc/pcCard/pcCard1.tsx
@@ -7,7 +7,7 @@ const PcCard1 = () => {
     
     return(
         <div id={styles.pc_card1Component}>
-            <div></div>
+            <div id={styles.pc_card1ComponentText}>이번주 인기 상품들</div>
             <div>
                 <div></div>
                 <div></div>

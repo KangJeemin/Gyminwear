09a7abc6a216273b280cad233835b1d0b4f09509
jiminKang <wlals1234560@gmail.com>
2023-10-12 17:10:07 +0900

    card1 컨테이너 생성

--- gym/src/component/middle/pc/pcCard/pcCard1.tsx
+++ gym/src/component/middle/pc/pcCard/pcCard1.tsx
@@ -8,7 +8,7 @@ const PcCard1 = () => {
     return(
         <div id={styles.pc_card1Component}>
             <div id={styles.pc_card1ComponentText}>이번주 인기 상품들</div>
-            <div>
+            <div id={styles.pc_card1Container}>
                 <div></div>
                 <div></div>
                 <div></div>

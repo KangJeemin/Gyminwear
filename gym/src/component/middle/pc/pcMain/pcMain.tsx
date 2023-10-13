0a684d2fa1e55e95d7f3e196bb334f7ca91b1eca
jiminKang <wlals1234560@gmail.com>
2023-10-13 16:22:22 +0900

    pc메인 화면에 이미지 넣고 꾸며봤음 .

--- gym/src/component/middle/pc/pcMain/pcMain.tsx
+++ gym/src/component/middle/pc/pcMain/pcMain.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import gbroLogo from '../../../../../public/image/gbro/gbroLogo.jpg'
 import PcSlide from '../pcSlide/pc';
 import PcCard1 from '../pcCard/pcCard1';
+import Image1 from '@/public/image/mainImage/Image1.jpg'
 import type { GymItemProps,gymWearItem } from '@/src/type/gymwear';
 const PcMain = (props:GymItemProps) => {
 
@@ -16,6 +17,17 @@ const PcMain = (props:GymItemProps) => {
                 <div id={styles.pc_mainImageContainerRightBox}></div>
             </div>
             <div id={styles.pc_mainImagemargin}></div>
+            <div id={styles.pc_mainImage1Container} className={`${styles.flexRow}`}>
+                <div id={styles.pc_mainImage1Box}></div>
+                <div id={styles.pc_mainImage1}>
+                    <Image
+                        src={Image1}
+                        alt="이미지를 표시 할 수 없음"
+                        layout='fill'
+                    />
+                </div>
+                <div id={styles.pc_mainImage1Box}></div>
+            </div>
             <div id={styles.pc_mainImage2}>
                 <PcCard1 gymitem={props.gymitem}/>
             </div>

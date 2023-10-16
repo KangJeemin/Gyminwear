bd741730764fa7ede4a292472f98e15180aac77e
jiminKang <wlals1234560@gmail.com>
2023-10-16 17:51:10 +0900

    app.tsx에 seo 설정하고 시도

---
+++ gym/next-seo.config.js
@@ -0,0 +1,36 @@
+export default {
+    openGraph: {
+      type: 'website',
+      locale: 'kr_ko',
+      url: 'https://www.gyminwear.com/',
+      siteName: '짐인웨어',
+    },
+    twitter: {
+      handle: '@handle',
+      site: '@site',
+      cardType: 'summary_large_image',
+    },
+  };
+//   title:"짐인웨어",
+//   description:"유행하는 짐웨어, 오버핏 짐웨어를 만나보세요!",
+//   canonical:"https://www.gyminwear.com/"
+//   openGraph={{
+//     url: 'https://www.gyminwear.com',
+//     title: '짐인웨어',
+//     description: '최신 유행하는 짐웨어를 만나보세요!',
+//     images: [
+//       {
+//         url: 'https://www.example.ie/og-image-01.jpg',
+//         width: 800,
+//         height: 600,
+//         alt: 'Og Image Alt',
+//         type: 'image/jpeg',
+//       },
+//     ],
+//     siteName: 'Gyminwear',
+//   }}
+//  twitter={{
+//    handle: '@handle',
+//    site: '@site',
+//    cardType: 'summary_large_image',
+//  }}
--- gym/pages/_app.tsx
+++ gym/pages/_app.tsx
@@ -9,10 +9,38 @@ import PcHeader from '../src/component/header/pc/pcHeader/pcHeader'
 import PcHeaderMargin from '../src/component/header/pc/pcHeaderMargin'
 import PcFooter from '@/src/component/footer/pc/pcFooter'
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+import { DefaultSeo } from 'next-seo';
+import { NextSeo } from 'next-seo'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    
     <AuthProvider>
+      <NextSeo
+       title="짐인웨어"
+       description="유행하는 짐웨어, 오버핏 짐웨어를 만나보세요!"
+       canonical="https://www.gyminwear.com/"
+       openGraph={{
+         url: 'https://www.gyminwear.com',
+         title: '짐인웨어',
+         description: '최신 유행하는 짐웨어를 만나보세요!',
+         images: [
+           {
+             url: 'https://www.example.ie/og-image-01.jpg',
+             width: 800,
+             height: 600,
+             alt: 'Og Image Alt',
+             type: 'image/jpeg',
+           },
+         ],
+         siteName: 'Gyminwear',
+       }}
+      twitter={{
+        handle: '@handle',
+        site: '@site',
+        cardType: 'summary_large_image',
+      }}
+      />
       <BrowserView>
         <PcHeader/>
         <PcHeaderMargin/>

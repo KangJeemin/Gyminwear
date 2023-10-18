d498bbbddb96d92d4b5c9e0f262badba3ca0c735
jiminKang <wlals1234560@gmail.com>
2023-10-18 15:28:31 +0900

    pcmain에서 2개의 props를 가져와서 각각 card1, card2에 뿌려주기 위해, type를 약간 수정했고, card2도 card1이랑 컨텐츠 내용만 다를뿐 아에 똑같기 때문에, 걍 복붙함, 근데 문제가 발생, 그냥 복붙하다보니, 마우스 hover가 되었을때 왼쪽, 오른쪽 버튼이 나오지 않고 있음 .

--- gym/pages/index.tsx
+++ gym/pages/index.tsx
@@ -7,6 +7,7 @@ import bestall from './api/bestall'
 import besttop from './api/besttop'
 import bestbottom from './api/bestbottom'
 import pcbestall from './api/pcbestall'
+import pcrecent from './api/pcrecent'
 import { gymWearItem } from '@/src/type/gymwear'
 import PcMain from '../src/component/middle/pc/pcMain/pcMain'
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
@@ -17,8 +18,9 @@ interface mainPageItemPropsType {
     bestAllItem:[]
     bestBottomItem:[]
     bestPcAllItem:[]
-    
+    bestPcRecentItem:[]
 }
+
 export const getStaticProps = async () => {
    const getAllItem= await bestall()
    const convertJSONAll:gymWearItem = JSON.parse(getAllItem)
@@ -28,24 +30,28 @@ export const getStaticProps = async () => {
    const convertJSONBottom:gymWearItem = JSON.parse(getBottomItem)
    const getPcAllItem= await pcbestall()
    const convertJSONPcAll:gymWearItem =JSON.parse(getPcAllItem)
+   const getPcRecentItem= await pcrecent()
+   const convertJSONPcRecent:gymWearItem =JSON.parse(getPcRecentItem)
     return { 
+
         props: { 
             bestAllItem:convertJSONAll?convertJSONAll:null,
             bestTopItem:convertJSONTop?convertJSONTop:null,
             bestBottomItem:convertJSONBottom?convertJSONBottom:null,
             bestPcAllItem:convertJSONPcAll?convertJSONPcAll:null,
+            bestPcRecentItem:convertJSONPcRecent?convertJSONPcRecent:null,
              } 
         };
     
     
 };
-const Index = ({bestTopItem,bestAllItem,bestBottomItem,bestPcAllItem}:mainPageItemPropsType)=>{
+const Index = ({bestTopItem,bestAllItem,bestBottomItem,bestPcAllItem,bestPcRecentItem}:mainPageItemPropsType)=>{
     return(
         <div
         style={{
             overscrollBehavior:"none"
         }}>
-        {isBrowser && (<PcMain gymitem={bestPcAllItem}/>)}
+        {isBrowser && (<PcMain gymitem={bestPcAllItem} gymitem2={bestPcRecentItem}/>)}
         {isMobile && (
             <div>
               <Announcement/>

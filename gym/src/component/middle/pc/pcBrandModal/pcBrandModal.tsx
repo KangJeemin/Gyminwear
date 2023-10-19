62ae1f0f30ba6c60e5fa289b7c2ed9d506f27c40
jiminKang <wlals1234560@gmail.com>
2023-10-19 15:53:26 +0900

    brandModal 생성,

--- gym/src/component/middle/pc/pcBrandModal/pcBrandModal.tsx
+++ gym/src/component/middle/pc/pcBrandModal/pcBrandModal.tsx
@@ -1,2 +1,13 @@
 import * as React from 'react'
 import styles from './index.module.css'
+
+const PcBrandModal= () => {
+    
+    
+    return(
+        <div></div>
+    )
+}
+
+
+export default PcBrandModal;

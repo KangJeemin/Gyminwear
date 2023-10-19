ad5da0bbcaa041487b137e0c1e0783d369b09f9e
jiminKang <wlals1234560@gmail.com>
2023-10-19 16:49:40 +0900

    브랜드 네임들까지 레이아웃 구성 완료, 브랜드가 여러개 생성시, 자동으로 스크롤 및 레이아웃에 맞게 스타일 지정해주자, 근데 갑자기 brandModal 왜 안나옴?

--- gym/src/component/header/pc/pcBrandModal/pcBrandModal.tsx
+++ gym/src/component/header/pc/pcBrandModal/pcBrandModal.tsx
@@ -9,16 +9,25 @@ const PcBrandModal= () => {
     const {brandModalState} = React.useContext(AuthContext)
     
     return(
-        <motion.div id={styles.pc_brandNameModalContainer} 
+        <motion.div id={styles.pc_brandNameModalComponent} 
         animate={{
             height:brandModalState===true ? "20rem" : "0rem",
         }}
         transition={{
-            duration:1,
+            duration:0.5,
         }}
         >
             <div id={styles.pc_brandNameModalMarginBox}></div>
-            <motion.div id={styles.pc_brandNameModalCenterBox}></motion.div>
+            <motion.div id={styles.pc_brandNameModalCenterBox}>
+                <div id={styles.pc_brandNameModalMarginBox2}></div>
+                <div id={styles.pc_brandNameModalBrandBox}>
+                    <ul className={styles.ulStyle}>
+                        <li className={styles.liStyle}>피지컬크라운</li>
+                        <li className={styles.liStyle}>짐브로</li>
+                    </ul>
+                </div>
+                <div id={styles.pc_brandNameModalMarginBox2}></div>
+            </motion.div>
             <div id={styles.pc_brandNameModalMarginBox}></div>
         </motion.div>
     )

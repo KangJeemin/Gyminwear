850b90c39d027261a25831f2a6e5ec39f86d8ae1
jiminKang <wlals1234560@gmail.com>
2023-10-19 17:23:33 +0900

    지금 brandModal 나옴, li부분이 문제네

--- gym/src/component/header/pc/pcBrandModal/pcBrandModal.tsx
+++ gym/src/component/header/pc/pcBrandModal/pcBrandModal.tsx
@@ -20,12 +20,12 @@ const PcBrandModal= () => {
             <div id={styles.pc_brandNameModalMarginBox}></div>
             <motion.div id={styles.pc_brandNameModalCenterBox}>
                 <div id={styles.pc_brandNameModalMarginBox2}></div>
-                {/* <div id={styles.pc_brandNameModalBrandBox}>
+                <div id={styles.pc_brandNameModalBrandBox}>
                     <ul className={styles.ulStyle}>
-                        <li className={styles.liStyle}>피지컬크라운</li>
-                        <li className={styles.liStyle}>짐브로</li>
+                        {/* <li className={styles.liStyle}>피지컬크라운</li> */}
+                        {/* <li className={styles.liStyle}>짐브로</li> */}
                     </ul>
-                </div> */}
+                </div>
                 <div id={styles.pc_brandNameModalMarginBox2}></div>
             </motion.div>
             <div id={styles.pc_brandNameModalMarginBox}></div>

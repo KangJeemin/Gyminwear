381d7ae04ceceda4635b25c8c9e9666cfa01b9f7
jiminKang <wlals1234560@gmail.com>
2023-10-20 19:15:34 +0900

    api 요청에 개수 추가하려고 시도 .

--- gym/pages/api/brandpage.ts
+++ gym/pages/api/brandpage.ts
@@ -18,13 +18,41 @@ export default function brandpage(req : NextApiRequest, res : NextApiResponse) {
         });
     }
     else{
+        
         if(pageNumber==='1'){
+// 
+// db.query(`SELECT * FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 0,20`,
+//         function (err: any, result: gymWearItem) {
+//             if(err) {
+//                 res.status(500).json({ error: 'An error occurred in err' });
+//             } else {
+//                 db.query(`SELECT COUNT(*) AS C FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+//                 function (counterr: any, countresult: searchResultCount) {
+//                 if(counterr) {
+//                    res.status(500).json({ error: 'An error occurred in counterr' });
+//                 } else {
+//                         res.json({result,countresult})
+//                 }
+//                 });                
+//             }
+        // });
+// 
+
+
             db.query(`SELECT * FROM gym.top WHERE brandname LIKE '%${brandname}%' AND sort LIKE '%${sort}%' UNION SELECT * FROM gym.bottom WHERE brandname LIKE '%${brandname}%' AND sort LIKE '%${sort}%' LIMIT 0,20`,
             function (err: any, result: gymWearItem) {
             if(err) {
-                console.log(err)
+                res.status(500).json({ error: 'An error occurred in err' });
             } else {
-                res.json(result);
+                db.query(`SELECT COUNT(*) AS C FROM (SELECT * FROM gym.top WHERE brandname LIKE '%${brandname}%' AND sort LIKE '%${sort}%' UNION ALL SELECT * FROM gym.bottom WHERE brandname LIKE '%${brandname}%' AND sort LIKE '%${sort}%') AS combine_results`,
+                function (counterr: any, countresult: searchResultCount) {
+                if(counterr) {
+                    res.status(500).json({ error: 'An error occurred in counterr' });
+                } else {
+                    res.json({result,countresult})
+                }
+                                });                
+                // res.json(result);
             }
             });
         }

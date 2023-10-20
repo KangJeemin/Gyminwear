8fcac4e53cf1154553ed446e374f0ccd999491f4
jiminKang <wlals1234560@gmail.com>
2023-10-20 10:18:12 +0900

    브랜드 페이지 api 생성(toppage API 복붙 )

---
+++ gym/pages/api/brandpage.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { gymWearItem } from '@/src/type/gymwear';
+const db = require('../../src/db/db')
+
+export default function brandpage(req : NextApiRequest, res : NextApiResponse) {
+    const pageNumber= req.query.page
+    if(pageNumber==='1'){
+        db.query("SELECT * FROM gym.top ORDER BY date LIMIT 0,20",
+        function (err: any, result: gymWearItem) {
+        if(err) {
+            console.log(err)
+        } else {
+            res.json(result);
+        }
+        });
+    }
+    else if(pageNumber==='2'){
+        db.query("SELECT * FROM gym.top ORDER BY date LIMIT 20,20",
+        function (err: any, result: gymWearItem) {
+        if(err) {
+            console.log(err)
+        } else {
+            res.json(result);
+        }
+        });
+    }
+    else{
+        console.log("페이지 1의 아이템 정보 요청 중 에러 발생")
+    }
+    
+}

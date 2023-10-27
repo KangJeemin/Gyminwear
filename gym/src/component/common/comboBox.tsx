1b1e7a490a87f4940e9c513326570681149f7e75
jiminKang <wlals1234560@gmail.com>
2023-10-27 17:01:50 +0900

    comboBox로 표현될 배열 타입 지정

--- gym/src/component/common/comboBox.tsx
+++ gym/src/component/common/comboBox.tsx
@@ -1,18 +1,33 @@
 import React, { useEffect, useState,useContext } from 'react';
 import { AuthContext } from '@/public/context/authcontext';
-import { ShortText } from '@mui/icons-material';
+import { PropaneSharp, ShortText } from '@mui/icons-material';
 type comboBoxProps = {
     handle:Function
 }
 const ComboBox= (props:comboBoxProps) => {
     const {comboBoxState, setComboBoxState} = useContext(AuthContext)
-    const sort5Array =[
-        {sortvalue:"All", sortText:"All"},
-        {sortvalue:"긴팔", sortText:"Long Sleeve"},
-        {sortvalue:"반팔", sortText:"T-Shirt"},
-        {sortvalue:"반바지", sortText:"Short Pants"},
-        {sortvalue:"긴바지", sortText:"Long Pants"},
-    ]
+    const [sortArray, setSortArray]=useState<{ sortvalue: string, sortText: string }[]>([])
+
+    const checkSortNum =(text:string)=>{
+        const resultArray:{ sortvalue: string, sortText: string }[]
+        const sort5Array:{ sortvalue: string, sortText: string }[]=[
+            {sortvalue:"All", sortText:"All"},
+            {sortvalue:"긴팔", sortText:"Long Sleeve"},
+            {sortvalue:"반팔", sortText:"T-Shirt"},
+            {sortvalue:"반바지", sortText:"Short Pants"},
+            {sortvalue:"긴바지", sortText:"Long Pants"},
+        ]
+        // brandpage
+        if(text==="brand"){
+            setSortArray(sort5Array)
+        }
+        // toppage
+        else if(){
+            resultArray = sort5Array.slice(0, 1).concat(sort5Array.slice(3));
+        }
+        // bottompage
+         else 
+    }    
     
     const selectValue = (e:React.ChangeEvent<HTMLSelectElement>) => {
         props.handle(e.target.value)
@@ -24,7 +39,7 @@ const ComboBox= (props:comboBoxProps) => {
             color:"black",
             fontSize:"inherit"
         }} onChange={selectValue}>
-            {sort5Array.map((object,index)=>(
+            {sortArray.map((object,index)=>(
             <option key={index} value={object.sortvalue} >{object.sortText}</option>    
             ))}
 		</select>

b3ebab2e403ec24159e61da94ab0f3d956da0d17
jiminKang <wlals1234560@gmail.com>
2023-10-27 16:39:48 +0900

    콤보박스를 brand, top, bottom 에서 공용으로 사용할 수 있게 코드 수정 중

--- gym/src/component/common/comboBox.tsx
+++ gym/src/component/common/comboBox.tsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState,useContext } from 'react';
 import { AuthContext } from '@/public/context/authcontext';
+import { ShortText } from '@mui/icons-material';
 type comboBoxProps = {
     handle:Function
 }
 const ComboBox= (props:comboBoxProps) => {
     const {comboBoxState, setComboBoxState} = useContext(AuthContext)
+    const sort5Array =[
+        {sortvalue:"All", sortText:"All"},
+        {sortvalue:"긴팔", sortText:"Long Sleeve"},
+        {sortvalue:"반팔", sortText:"T-Shirt"},
+        {sortvalue:"반바지", sortText:"Short Pants"},
+        {sortvalue:"긴바지", sortText:"Long Pants"},
+    ]
+    
     const selectValue = (e:React.ChangeEvent<HTMLSelectElement>) => {
         props.handle(e.target.value)
         setComboBoxState(e.target.value);
@@ -14,13 +23,10 @@ const ComboBox= (props:comboBoxProps) => {
             backgroundColor:"white",
             color:"black",
             fontSize:"inherit"
-            
         }} onChange={selectValue}>
-			<option value="all" selected>All</option>
-            <option value="긴팔">Long Sleeve</option>
-            <option value="반팔">T-Shirt</option>
-            <option value="반바지">Short Pants</option>
-            <option value="긴바지">Long Pants</option>
+            {sort5Array.map((object,index)=>(
+            <option key={index} value={object.sortvalue} >{object.sortText}</option>    
+            ))}
 		</select>
     )
 }

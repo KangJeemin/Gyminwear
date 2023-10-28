d148cb6d15d78bfe3bcab778d6bd1f62bd3d07ff
jiminKang <wlals1234560@gmail.com>
2023-10-28 14:14:41 +0900

    toppage, searchpage 완료

--- gym/pages/api/search.ts
+++ gym/pages/api/search.ts
@@ -1,66 +1,29 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { gymWearItem } from '@/src/type/gymwear';
 import type { searchResultCount } from '@/src/type/gymwear';
+import createMysqlLimit from '@/src/module/createMysqlLimit';
 const db = require('../../src/db/db')
 
 
-export default function search(req : NextApiRequest, res : NextApiResponse) {
+export default async function search(req : NextApiRequest, res : NextApiResponse) {
     console.log(req.query)
     const searchstring = req.query.search;
-    const pageNumber = req.query.page;
+    const pageNumber:string= req.query.page as string
+    let limit:string = await createMysqlLimit(pageNumber)
     
-    if(pageNumber==='1'){
-        db.query(`SELECT * FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 0,20`,
-        function (err: any, result: gymWearItem) {
-            if(err) {
-                res.status(500).json({ error: 'An error occurred in err' });
+    db.query(`SELECT * FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT ${limit}`,
+    function (err: any, result: gymWearItem) {
+        if(err) {
+            res.status(500).json({ error: 'An error occurred in err' });
+        } else {
+            db.query(`SELECT COUNT(*) AS C FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
+            function (counterr: any, countresult: searchResultCount) {
+            if(counterr) {
+                res.status(500).json({ error: 'An error occurred in counterr' });
             } else {
-                db.query(`SELECT COUNT(*) AS C FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-                function (counterr: any, countresult: searchResultCount) {
-                if(counterr) {
-                   res.status(500).json({ error: 'An error occurred in counterr' });
-                } else {
-                        res.json({result,countresult})
-                }
-                });                
+                    res.json({result,countresult})
             }
-        });
-    }
-    else if(pageNumber==='2'){
-        db.query(`SELECT * FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 20,20`,
-        function (err: any, result: gymWearItem) {
-            if(err) {
-                res.status(500).json({ error: 'An error occurred in err' });
-            } else {
-                db.query(`SELECT COUNT(*) AS C FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-                function (counterr: any, countresult: searchResultCount) {
-                if(counterr) {
-                   res.status(500).json({ error: 'An error occurred in counterr' });
-                } else {
-                        res.json({result,countresult})//
-                }
-                });                
-            }
-        });
-    }
-    else if(pageNumber==='3'){
-        db.query(`SELECT * FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT 40,20`,
-        function (err: any, result: gymWearItem) {
-            if(err) {
-                res.status(500).json({ error: 'An error occurred in err' });
-            } else {
-                db.query(`SELECT COUNT(*) AS C FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%'`,
-                function (counterr: any, countresult: searchResultCount) {
-                if(counterr) {
-                   res.status(500).json({ error: 'An error occurred in counterr' });
-                } else {
-                        res.json({result,countresult})
-                }
-                });                
-            }
-        });
-    }
-    else{
-        console.log("오류 발생")
-    }
+            });                
+        }
+    });
 }

8f38514d9b1de099f3337612c33e0f25e08c36f2
jiminKang <wlals1234560@gmail.com>
2023-11-02 20:53:06 +0900

    모바일 검색 결과 파일 위치 옮김

--- gym/pages/searchresult/index.tsx
+++ gym/pages/searchresult/index.tsx
@@ -1,35 +1,23 @@
 
-import { useRouter } from 'next/router';
 import React, { useState, useRef, useEffect, useContext, ChangeEvent } from 'react';
-import styles from './index.module.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass,faCircleXmark } from "@fortawesome/free-solid-svg-icons";
-import { AuthContext } from '@/public/context/authcontext';
-import Image from 'next/image'
-import convertWon from '@/pages/src/module/convertWon';
-import axios from 'axios';
 import type { gymWearItem ,SearchResultProps} from '../../src/type/gymwear';
 import { GetServerSideProps,GetServerSidePropsContext } from 'next';
 import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
-import NumberNavigate from '../../src/component/middle/mobile/numberNavigate/numberNavigate';
+
 import Pc from '../../src/component/middle/pc/pcSearchresult/pc';
-import Mobile from '../../src/component/middle/pc/pcSearchresult/mobile'
+import Mobile from '../../src/component/middle/mobile/search/mobile'
 
 interface gymwear{
     data:gymWearItem;
 }
-
 const Index = ({item,count}:SearchResultProps) => { 
 return(
     <div>
         {isBrowser && <Pc item={item} count={count}/>}
         {isMobile && <Mobile item={item} count={count}/>}
     </div>
-    
-
 )
     
-
 };
 export async function getServerSideProps(context: GetServerSidePropsContext) {
     const { search, page } = context.query;

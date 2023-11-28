03357ee830103c09238388b719af116f6f671aa7
jiminKang <wlals1234560@gmail.com>
2023-11-28 18:25:17 +0900

    pageNav 만듬

--- gym/pages/index.tsx
+++ gym/pages/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
-import Header from '../components/header'
+import Header from '@/components/header'
+import PageNavigate from '@/components/pageNavigate';
 
 export default function index() {
     return(
-        <Header></Header>
+        <>
+        <Header/>
+        <PageNavigate/>
+        </>
     )
-  
 }

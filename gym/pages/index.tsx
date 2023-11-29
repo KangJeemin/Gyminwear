c2c60480c25f39f797d6ffa29ad08217d7859660
jiminKang <wlals1234560@gmail.com>
2023-11-29 16:36:16 +0900

    seo 최적화 시도

--- gym/pages/index.tsx
+++ gym/pages/index.tsx
@@ -1,13 +1,14 @@
-import * as React from 'react';
+import React from 'react';
+import Head from 'next/head';
 import Button from '@mui/material/Button';
-import Header from '@/components/header'
+import Header from '@/components/header';
 import PageNavigate from '@/components/PageNavigate';
 
 export default function index() {
-    return(
-        <>
-        <Header/>
-        <PageNavigate/>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <PageNavigate />
+    </>
+  );
 }

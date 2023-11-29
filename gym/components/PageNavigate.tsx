3073220ba721758ee0e8fbf8dcb0f727705509cc
jiminKang <wlals1234560@gmail.com>
2023-11-29 13:22:17 +0900

    각 브랜드마다 페이지 링크 걸기 완성, 손 개 아프네

--- gym/components/PageNavigate.tsx
+++ gym/components/PageNavigate.tsx
@@ -32,93 +32,92 @@ export default function AutoGrid() {
                  <Item><Link href='https://borntowin.kr/'>본투윈</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item><Link href='https://borntowin.kr/'>덤브스트럭</Link></Item>
+                 <Item><Link href='https://dumbstruck.kr/'>덤브스트럭</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>피지컬크라운</Item>
+                 <Item><Link href='https://smartstore.naver.com/physicalcrown'>피지컬크라운</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>Freedom</Item>
+                 <Item><Link href='https://www.freedomdom.kr/'>프리덤에슬레틱</Link></Item>
                 </Box>
                 <Box gridColumn="span 2">
-                 <Item>압도</Item>
+                 <Item><Link href='https://overwhelm.co.kr/'>압도</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>퍼스트웨어</Item>
+                 <Item><Link href='https://firstwear.kr/'>퍼스트웨어</Link></Item>
                 </Box>
                 <Box gridColumn="span 2">
-                 <Item>RBW</Item>
+                 <Item><Link href='https://www.rbwapparel.com/'>RBW</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>짐브로</Item>
+                 <Item><Link href='https://gymbro.kr/'>짐브로</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>키네틱피플</Item>
+                 <Item><Link href='http://www.kineticpeople.co.kr/'>키네틱피플</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>B_FoB</Item>
+                 <Item><Link href='https://www.befirstorbest.com/'>B_FoB</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>머슬암드</Item>
+                 <Item><Link href='https://musclearmed.com/'>머슬암드</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>랩스온랩스</Item>
+                 <Item><Link href='https://smartstore.naver.com/rsrs'>랩스온랩스</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>Hdex</Item>
+                 <Item><Link href='https://hdex.co.kr/'>Hdex</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>잭시미스</Item>
+                 <Item><Link href='https://xexymix.com/'>잭시미스</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>RSPT</Item>
+                 <Item><Link href='https://smartstore.naver.com/rspt'>RSPT</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>피지코어패럴</Item>
+                 <Item><Link href='https://smartstore.naver.com/fizico'>피지코어패럴</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>아던트소울</Item>
+                 <Item><Link href='https://ardentsoul.co.kr/'>아던트소울</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>멧블랙</Item>
+                 <Item><Link href='https://smartstore.naver.com/matblack/'>멧블랙</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>피지컬가먼츠</Item>
+                 <Item><Link href='https://physicalgarments.com/'>피지컬가먼츠</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>액츄얼라이즈</Item>
+                 <Item><Link href='https://smartstore.naver.com/actualize'>액츄얼라이즈</Link></Item>
                 </Box>
                 <Box gridColumn="span 3">
-                 <Item>RANK</Item>
+                 <Item><Link href='https://the-rank.co.kr/'>RANK</Link></Item>
                 </Box>
                 <Box gridColumn="span 2">
-                 <Item>고룩</Item>
+                 <Item><Link href='https://smartstore.naver.com/go_look'>고룩</Link></Item>
                 </Box>
                 <Box gridColumn="span 3">
-                 <Item>UNF</Item>
+                 <Item><Link href='https://smartstore.naver.com/universal_fit'>UNF</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>골드앤머신</Item>
+                 <Item><Link href='https://goldennmachine.kr/'>골드앤머신</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>언브로큰</Item>
+                 <Item><Link href='https://www.unbroken.co.kr/'>언브로큰</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>베르만어패럴</Item>
+                 <Item><Link href='https://smartstore.naver.com/vermann'>베르만어패럴</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>머스콜로</Item>
+                 <Item><Link href='https://muscoloadm.cafe24.com/'>머스콜로</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>VENAFIT</Item>
+                 <Item><Link href='https://smartstore.naver.com/venafit'>VENAFIT</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>BRUNK</Item>
+                 <Item><Link href='https://brunk.kr/'>BRUNK</Link></Item>
                 </Box>
                 <Box gridColumn="span 4">
-                 <Item>하드지머</Item>
+                 <Item><Link href='https://smartstore.naver.com/addtobag'>하드지머</Link></Item>
                 </Box>
-
             </Box>
         </Box>
         <Box className={styles.main_Margin}></Box>

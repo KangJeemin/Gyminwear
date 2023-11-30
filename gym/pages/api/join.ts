1015c8944b3f2ba0539a48ad7b2f400ab3d44fd1
jiminKang <wlals1234560@gmail.com>
2023-11-30 18:31:08 +0900

    유저정보 데이터 베이스 삽입 성공,

--- gym/pages/api/join.ts
+++ gym/pages/api/join.ts
@@ -12,39 +12,23 @@ type userInfo = {
 export default async function Join(req : NextApiRequest, res : NextApiResponse){
     if (req.method === 'POST') {
         const requestData:userInfo = req.body;
-
+        
         // 오늘 날짜의 밀리초와 랜덤 값을 곱하여 반올림하여 정수를 만듬, 그 후 문자열로 변환
-        let inputPassword = await requestData.password;
-        let salt = await Math.round((new Date().valueOf() * Math.random())) + "";
-        let hashPassword = await crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
+        const inputPassword = requestData.password;
+        const salt =  Math.round((new Date().valueOf() * Math.random())) + "";
+        const hashPassword =  crypto.createHash("sha512").update(inputPassword + salt).digest("hex");
 
           try{
             db.query(
-                `INSERT INTO user (email,name,password,nickname,salt) VALUES (${requestData.email},${requestData.name},${hashPassword},${requestData.nickname},${salt})`
+                `INSERT INTO user (email,name,password,nickname,salt) VALUES ('${requestData.email}','${requestData.name}','${hashPassword}','${requestData.nickname}','${salt}');`
             ,(error:any,result:any)=>{
                 if(error){
                     console.error("회원가입 중 유저 정보를 삽입 하는 과정에서 오류 발생")
                     return false
                 } else{
-                    console.log('result=',result)
+                    res.status(200).json({ result: true });  
                 }
             })
-
-
-            res.status(200).json({ result: '성공' });  
-            // return new Promise((resolve, reject) => {
-            //     db.query(
-            //       "SELECT * FROM gym.bottom UNION SELECT * FROM gym.top ORDER BY likecount DESC LIMIT 0,4",
-            //       (err: any, result: boolean) => {
-            //         if (err) {
-            //           console.error(err);
-            //           reject(err);
-            //         } else {
-            //             resolve(result)
-            //         }
-            //       }
-            //     );
-            //   });
           }
           catch (error) {
             console.error("데이터 베이스에 유저 정보 저장 중 에러 발생")

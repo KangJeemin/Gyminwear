f502aec8c18389c3ea89993389ec5b5604566b6e
jiminKang <wlals1234560@gmail.com>
2023-11-30 19:17:28 +0900

    salt 값도 조회

--- gym/pages/api/login.ts
+++ gym/pages/api/login.ts
@@ -20,14 +20,15 @@ export default async function Join(req : NextApiRequest, res : NextApiResponse){
         // db.query(`SELECT * FROM top WHERE brandname LIKE '%${searchstring}%' OR productname LIKE '%${searchstring}%' LIMIT ${limit}`,
           try{
             db.query(
-                `SELECT password FROM user WHERE email = '${inputEmail}'; `
-            ,(error:any,result:any)=>{
+                `SELECT password,salt FROM user WHERE email = '${inputEmail}'; `
+            ,async (error:any,result:any)=>{
                 if(error){
                     console.error("로그인하기위한 데이터베이스에 정보 조회중 오류 발생")
                     return false
                 } else{
-                    console.log(result)
-                    // res.status(200).json({ result: true });  
+                    
+                    DbPassword=await result.password;
+                    salt=await result.salt;
                     // 모든 쿼리가 완료되면 데이터베이스 연결 종료
                     db.end();
                 }
@@ -37,6 +38,8 @@ export default async function Join(req : NextApiRequest, res : NextApiResponse){
             console.error("데이터 베이스에 유저 정보 저장 중 에러 발생")
             res.status(500).json({ error: '서버 오류' });  
           }
+
+        
       } else {
         res.status(405).json({ error: '허용되지 않는 메서드' });
       }  

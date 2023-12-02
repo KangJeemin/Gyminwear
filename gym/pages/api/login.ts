f4bf65b864afd05db7617035c2fa1958b266ec72
jiminKang <wlals1234560@gmail.com>
2023-12-02 21:39:13 +0900

    전역에 설정한 변수는 없어도 될 것 같아서 삭제함

--- gym/pages/api/login.ts
+++ gym/pages/api/login.ts
@@ -1,8 +1,7 @@
 import type {User} from '@/interface/user'
 import { withIronSessionApiRoute } from 'iron-session/next'
 import { NextApiRequest, NextApiResponse } from 'next';
-
-
+import { sessionOptions } from '@/lib/config/iron-config'
 const db = require('@/lib/connectMysql');
 const crypto = require('crypto');
 
@@ -13,6 +12,49 @@ type LoginInfo = {
 }
 async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
   const { username } = await req.body
+  if (req.method === 'POST') {
+    const requestData:LoginInfo = req.body;
+    const {email,password}:LoginInfo = req.body;
+    
+    // 현재 비밀번호와 데이터베이스에 저장되어 있는 salt 값으로 비밀번호 조회하기
+    const inputEmail = await requestData.email;
+    const inputPassword = await requestData.password;
+    
+       
+      try{
+        db.query(
+            `SELECT password,salt FROM user WHERE email='${inputEmail}'; `
+        ,async(error:any,result:any)=>{
+            if(error){
+                console.error("로그인하기위한 데이터베이스에 정보 조회중 오류 발생")
+                res.status(200).json({ result:false }); 
+                return false
+            } else{
+                let DbPassword= await result[0].password;
+                let salt= await result[0].salt;
+                let hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");     
+                
+                //데이터 베이스에 있던 해쉬암호와 새로 받은 해쉬 암호와 비교하여 똑같으면 프론트에 true를 줌 (로그인 성공)
+                if(DbPassword==hashPassword){
+                  res.status(200).json({ result:true }); 
+                  hashPassword=''; 
+                }
+                //데이터 베이스에 있던 해쉬암호와 새로 받은 해쉬 암호와 비교하여 다르면 프론트에 false를 줌 (로그인 실패)
+                else{
+                  console.error("비밀번호 불일치")
+                  res.status(200).json({result:false}); 
+                  hashPassword=''; 
+                }
+            }
+        })
+      }
+      catch (error) {
+        console.error("데이터 베이스에 유저 정보 저장 중 에러 발생")
+        res.status(500).json({ error: '서버 오류' });  
+      }
+  } else {
+    res.status(405).json({ error: '허용되지 않는 메서드' });
+  }  
 
   try {
     const user = { isLoggedIn: true, email, name } as User
@@ -25,50 +67,5 @@ async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
 }
 
 export default withIronSessionApiRoute(loginRoute, sessionOptions)
-export default async function Join(req : NextApiRequest, res : NextApiResponse){
-    if (req.method === 'POST') {
-        const requestData:LoginInfo = req.body;
-        
-        // 현재 비밀번호와 데이터베이스에 저장되어 있는 salt 값으로 비밀번호 조회하기
-        const inputEmail = await requestData.email;
-        const inputPassword = await requestData.password;
-        let salt:string = "";
-        let DbPassword: string = "";
-        let hashPassword :string = "";
-           
-          try{
-            db.query(
-                `SELECT password,salt FROM user WHERE email='${inputEmail}'; `
-            ,async(error:any,result:any)=>{
-                if(error){
-                    console.error("로그인하기위한 데이터베이스에 정보 조회중 오류 발생")
-                    res.status(200).json({ result:false }); 
-                    return false
-                } else{
-                    DbPassword= await result[0].password;
-                    salt= await result[0].salt;
-                    hashPassword = crypto.createHash("sha512").update(inputPassword + salt).digest("hex");     
-                    
-                    //데이터 베이스에 있던 해쉬암호와 새로 받은 해쉬 암호와 비교하여 똑같으면 프론트에 true를 줌 (로그인 성공)
-                    if(DbPassword==hashPassword){
-                      res.status(200).json({ result:true }); 
-                      hashPassword=''; 
-                    }
-                    //데이터 베이스에 있던 해쉬암호와 새로 받은 해쉬 암호와 비교하여 다르면 프론트에 false를 줌 (로그인 실패)
-                    else{
-                      console.error("비밀번호 불일치")
-                      res.status(200).json({result:false}); 
-                      hashPassword=''; 
-                    }
-                }
-            })
-          }
-          catch (error) {
-            console.error("데이터 베이스에 유저 정보 저장 중 에러 발생")
-            res.status(500).json({ error: '서버 오류' });  
-          }
-      } else {
-        res.status(405).json({ error: '허용되지 않는 메서드' });
-      }  
+    
   
-}

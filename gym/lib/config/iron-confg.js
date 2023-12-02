da475652ee5fbd2a9176f9b796d2c4a6b44e0723
jiminKang <wlals1234560@gmail.com>
2023-12-02 20:51:18 +0900

    iron-session config 파일 타입을 ts 로 바꿈

--- gym/lib/config/iron-confg.js
+++
@@ -1,8 +0,0 @@
-export const ironOptions = {
-    cookieName: "Next-iron-session-cookie",
-    password: "yPo4T7apfbdvctV1Bso1oAndQH9qwC94",
-    // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-//     cookieOptions: {
-//         secure: process.env.NODE_ENV === "production" ? true: false,
-//   },
-}
---
+++ gym/lib/config/iron-confg.ts
@@ -0,0 +1,18 @@
+// this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
+import type { IronSessionOptions } from 'iron-session'
+
+export const sessionOptions: IronSessionOptions = {
+  password: process.env.NEXT_PUBLIC_IRON_SESSION_PASSWORD as string,
+  cookieName: 'iron-session/examples/next.js',
+  // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+}
+
+// This is where we specify the typings of req.session.*
+declare module 'iron-session' {
+  interface IronSessionData {
+    user?: User
+  }
+}

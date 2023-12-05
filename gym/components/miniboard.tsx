5eb85f4b7d7e5e4615e5bd6b4d80eca8435a3478
jiminKang <wlals1234560@gmail.com>
2023-12-05 19:08:51 +0900

    box 자식 설정하는 법 알아봐야ㅕ할듯

--- gym/components/miniboard.tsx
+++ gym/components/miniboard.tsx
@@ -1,34 +1,27 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import CommentIcon from '@mui/icons-material/Comment';
-import IconButton from '@mui/material/IconButton';
-import ImageListItem from '@mui/material/ImageListItem';
 import styles from '@/styles/styles.module.css';
 
-
 export default function MiniBoard() {
-
-    return(
-        <Container>
-            <Container >
-                <Box className={styles.Text1}>미니보드 바로가기</Box>
-                    <Box>
-                        <Box id={styles.miniboardTextContainer}>미니보드 바로가기
-                            <Box id={styles.miniboardHeaderBox}>미니보드 바로가기</Box>
-                            <Box id={styles.miniboardBodyBox}>미니보드 바로가기
-                                {/* <Box id={styles.miniboardNickName}>미니보드 바로가기</Box> */}
-                                {/* <Box id={styles.miniboardHeart}>미니보드 바로가기</Box> */}
-                                {/* <Box id={styles.miniboardComment}>미니보드 바로가기</Box> */}
-                            </Box>
-                        </Box>
-                        <Box id={styles.miniboardImageBox}>미니보드 바로가기</Box>
-                </Box>
-            </Container>
-        </Container>
-    
-    )
+  return (
+    <Container>
+      <Container>
+        <Box className={styles.Text1}>미니보드 바로가기</Box>
+        <Box id={styles.miniboardMainContainer}>
+          <Box id={styles.miniboardTextContainer}>
+            미니보드 바로가기
+            <Box id={styles.miniboardHeaderBox}>미니보드 바로가기</Box>
+            <Box id={styles.miniboardBodyBox}>
+              미니보드 바로가기
+              {/* <Box id={styles.miniboardNickName}>미니보드 바로가기</Box> */}
+              {/* <Box id={styles.miniboardHeart}>미니보드 바로가기</Box> */}
+              {/* <Box id={styles.miniboardComment}>미니보드 바로가기</Box> */}
+            </Box>
+          </Box>
+          <Box id={styles.miniboardImageBox}>미니보드 바로가기</Box>
+        </Box>
+      </Container>
+    </Container>
+  );
 }

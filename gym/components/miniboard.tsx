c6ac775d8a883ef98c5d41e43e25dacea2249b1a
KangJeemin <wlals1234560@gmail.com>
2023-12-05 23:22:32 +0900

    기본 틀 완성

--- gym/components/miniboard.tsx
+++ gym/components/miniboard.tsx
@@ -6,20 +6,40 @@ import styles from '@/styles/styles.module.css';
 export default function MiniBoard() {
   return (
     <Container>
-      <Container>
-        <Box className={styles.Text1}>미니보드 바로가기</Box>
-        <Box id={styles.miniboardMainContainer}>
-          <Box id={styles.miniboardTextContainer}>
-            미니보드 바로가기
-            <Box id={styles.miniboardHeaderBox}>미니보드 바로가기</Box>
-            <Box id={styles.miniboardBodyBox}>
-              미니보드 바로가기
+      <Container maxWidth="xl">
+        <Box maxWidth="xl"sx={{
+          color:1,
+          padding:1,
+          fontWeight:"bold"
+        }}>미니보드 바로가기</Box>
+        <Box maxWidth="xl" sx={{
+          backgroundColor:"green",
+          display:"flex"
+        }}>
+          <Box sx={{
+            width:"80%",
+            backgroundColor:"yellow",
+            display:"flex",
+            flexDirection:"column"
+          }}>
+            <Box sx={{
+              width:"100%",
+              height:"75px",
+              backgroundColor:"red"
+            }}>HeaderBox</Box>
+            <Box sx={{
+              width:"100%",
+              height:"75px",
+              backgroundColor:"blue"
+            }}>BodyBox
               {/* <Box id={styles.miniboardNickName}>미니보드 바로가기</Box> */}
               {/* <Box id={styles.miniboardHeart}>미니보드 바로가기</Box> */}
               {/* <Box id={styles.miniboardComment}>미니보드 바로가기</Box> */}
             </Box>
           </Box>
-          <Box id={styles.miniboardImageBox}>미니보드 바로가기</Box>
+          <Box sx={{
+            width:"20%"
+          }}>ImageBox</Box>
         </Box>
       </Container>
     </Container>

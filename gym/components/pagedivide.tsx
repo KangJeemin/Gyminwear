4e249310f0ed6c151b23bded8fc35a2c6378de0c
jiminKang <wlals1234560@gmail.com>
2023-12-06 17:00:22 +0900

    divider 페이지 생성

---
+++ gym/components/pagedivide.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+export default function pagedivide() {
+  return (
+    <Box sx={{
+        width:'100%',
+        height:{xs:"10px",xl:"20px"},
+        backgroundColor:"#D9D9D9"
+    }}></Box>
+  )
+}

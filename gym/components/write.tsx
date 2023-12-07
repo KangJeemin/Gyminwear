e4cca40a4e2845e9c0068b1664a3830f90efa569
jiminKang <wlals1234560@gmail.com>
2023-12-07 21:44:33 +0900

    upload 버튼 생성

--- gym/components/write.tsx
+++ gym/components/write.tsx
@@ -1,7 +1,21 @@
 import * as React from "react";
 import DoubleContainer from "./doubleContainer";
 import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
+import Button from "@mui/material/Button";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 export default function write() {
   return (
     <DoubleContainer>
@@ -18,6 +32,14 @@ export default function write() {
       >
         글쓰기
       </Box>
+      <Button
+        component="label"
+        variant="contained"
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload file
+        <VisuallyHiddenInput type="file" />
+      </Button>
     </DoubleContainer>
   );
 }

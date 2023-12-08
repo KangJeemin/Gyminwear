2f6781f57115834f85f6e489f0013bc26a4aacb9
jiminKang <wlals1234560@gmail.com>
2023-12-08 17:53:03 +0900

    댓글 레이아웃 일단 완성

--- gym/components/read.tsx
+++ gym/components/read.tsx
@@ -2,6 +2,9 @@ import * as React from "react";
 import DoubleContainer from "./doubleContainer";
 import Box from "@mui/material/Box";
 import { Container } from "@mui/system";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Comment from "./comment";
 
 export default function Read() {
   return (
@@ -53,6 +56,61 @@ export default function Read() {
         }}
       >
         댓글(0)
+      </Box>
+      <Comment />
+      <Box
+        sx={{
+          width: "100%",
+          height: "300px",
+          border: 1,
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <TextField
+          id="CommentNickname"
+          label="닉네임"
+          variant="standard"
+          sx={{ width: "300px" }}
+        />
+        <Box
+          sx={{
+            width: "100%",
+            height: "200px",
+          }}
+        >
+          <input
+            style={{
+              width: "100%",
+              height: "100%",
+              backgroundColor: "white",
+              border: "none",
+              color: "black",
+              outline: "none", // 기본 포커스 효과 제거
+            }}
+            placeholder="내용을 입력하세요"
+            name="comment"
+          />
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            height: "20px",
+            display: "flex",
+            justifyContent: "flex-end",
+          }}
+        >
+          <Button
+            component="label"
+            variant="contained"
+            sx={{
+              width: "70px",
+              height: "40px",
+            }}
+          >
+            등록
+          </Button>
+        </Box>
       </Box>
     </DoubleContainer>
   );

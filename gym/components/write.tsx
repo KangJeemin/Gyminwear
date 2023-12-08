d3c7ef067538290a94adcb3f79991a206c6b2d2b
jiminKang <wlals1234560@gmail.com>
2023-12-08 15:37:04 +0900

    취소, 확인 버튼을 눌러서 모달 제어하려고 하는데, 버튼을 클릭해도 모달이 안 닫히네 ?

--- gym/components/write.tsx
+++ gym/components/write.tsx
@@ -4,15 +4,13 @@ import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import ReactQuill from 'react-quill'
-import 'react-quill/dist/quill.snow.css'; 
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 import QuillWrapper from "./quillWrapper";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 import Modal from "./modal";
-import Container from '@mui/material/Container';
-
-
-
+import Container from "@mui/material/Container";
+import { useRouter } from "next/router";
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -25,8 +23,9 @@ const VisuallyHiddenInput = styled("input")({
   whiteSpace: "nowrap",
   width: 1,
 });
-export default function write() {
-  const [isModalOpen, setModalOpen] = React.useState(true);
+export default function Write() {
+  const [isModalOpen, setModalOpen] = React.useState(false);
+  const router = useRouter();
 
   const openModal = () => {
     setModalOpen(true);
@@ -35,7 +34,6 @@ export default function write() {
   const closeModal = () => {
     setModalOpen(false);
   };
-
 
   return (
     <DoubleContainer>
@@ -48,7 +46,6 @@ export default function write() {
           fontWeight: "bold",
           borderBottom: 1,
           marginBottom: "20px",
-          
         }}
       >
         글쓰기
@@ -57,17 +54,26 @@ export default function write() {
         <Container>
           <h3>글 작성을 취소하시겠습니까?</h3>
           <h5>(취소하면 작성 중인 내용은 저장되지 않습니다.)</h5>
-          <Box sx={{
-            width:"100%",
-            display:"flex",
-            justifyContent:"space-around",
-            paddingTop:"20px"}}>
-          <Button variant="contained" size="small">
-          예
-        </Button>
-        <Button variant="contained" size="small">
-          아니오
-        </Button>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-around",
+              paddingTop: "20px",
+            }}
+          >
+            <Button
+              variant="contained"
+              size="small"
+              onClick={() => {
+                router.push("/");
+              }}
+            >
+              예
+            </Button>
+            <Button variant="contained" size="small" onClick={closeModal}>
+              아니오
+            </Button>
           </Box>
         </Container>
       </Modal>
@@ -76,34 +82,38 @@ export default function write() {
           width: "100%",
           height: "50px",
           color: "black",
-          display:"flex",
+          display: "flex",
           marginTop: "20px",
           fontWeight: "bold",
           marginBottom: "20px",
         }}
       >
-       <TextField id="title" label="제목" variant="standard" sx={{width:"70%"}} />
-       <Box sx={{width:"10%"}}></Box>
-       <TextField id="nickname" label="작성자" variant="standard" sx={{width:"20%"}} value="강지민"/>
+        <TextField
+          id="title"
+          label="제목"
+          variant="standard"
+          sx={{ width: "70%" }}
+        />
+        <Box sx={{ width: "10%" }}></Box>
+        <TextField
+          id="nickname"
+          label="작성자"
+          variant="standard"
+          sx={{ width: "20%" }}
+          value="강지민"
+        />
       </Box>
-      <QuillWrapper/>
-      <Box sx={{paddingTop:"100px",display:"flex"}}>
-      <Box sx={{width:{xl:"90%"}}}></Box>
-      <Button
-        component="label"
-        variant="contained"
-      >
-        취소
-      </Button>
-      <Box sx={{width:{xs:"90%",xl:"5%"}}}></Box>
-      <Button
-        component="label"
-        variant="contained"
-      >
-        등록
-      </Button>
+      <QuillWrapper />
+      <Box sx={{ paddingTop: "100px", display: "flex" }}>
+        <Box sx={{ width: { xl: "90%" } }}></Box>
+        <Button component="label" variant="contained" onClick={openModal}>
+          취소
+        </Button>
+        <Box sx={{ width: { xs: "90%", xl: "5%" } }}></Box>
+        <Button component="label" variant="contained">
+          등록
+        </Button>
       </Box>
-      
     </DoubleContainer>
   );
 }

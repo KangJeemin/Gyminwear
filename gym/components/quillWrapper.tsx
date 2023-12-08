9fe90231ab17fb61671dbeffdd20805a54e238dc
jiminKang <wlals1234560@gmail.com>
2023-12-08 18:21:25 +0900

    모바일, 피시버전 Quill 툴바 적용 중인데, QuillWrapper가 JSX 구성 요소로 사용할 수 없다는 에러 발생

--- gym/components/quillWrapper.tsx
+++ gym/components/quillWrapper.tsx
@@ -1,56 +1,83 @@
-import dynamic from 'next/dynamic'
+import * as React from "react";
+import dynamic from "next/dynamic";
 
-  
-  const modules = {
+/*
+ * Quill editor formats
+ * See https://quilljs.com/docs/formats/
+ */
+const formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "video",
+];
+
+const QuillWrapper = dynamic(() => import("react-quill"), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
+
+export default function Quillwrapper() {
+  const [modules, setModules] = React.useState({
     toolbar: [
-      [{ header: '1' }, { header: '2' }, { font: [] }],
+      [{ header: "1" }, { header: "2" }, { font: [] }],
       [{ size: [] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      ["bold", "italic", "underline", "strike", "blockquote"],
       [
-        { list: 'ordered' },
-        { list: 'bullet' },
-        { indent: '-1' },
-        { indent: '+1' },
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
       ],
-      ['link', 'image', 'video'],
-      ['clean'],
+      ["link", "image", "video"],
+      ["clean"],
     ],
     clipboard: {
-      // toggle to add extra line breaks when pasting HTML:
       matchVisual: false,
     },
-  }
-  /*
-   * Quill editor formats
-   * See https://quilljs.com/docs/formats/
-   */
-  const formats = [
-    'header',
-    'font',
-    'size',
-    'bold',
-    'italic',
-    'underline',
-    'strike',
-    'blockquote',
-    'list',
-    'bullet',
-    'indent',
-    'link',
-    'image',
-    'video',
-  ]
-  
-const QuillWrapper = dynamic(() => import('react-quill'), {
-    ssr: false,
-    loading: () => <p>Loading ...</p>,
-  })
+  });
 
-export default function Quillwrapper(){
-    return(
-        <QuillWrapper  modules={modules} formats={formats} theme="snow"  placeholder="내용을 입력하세요." style={{
-            width:"100%",
-            height:"300px"
-        }}/>
-    )
+  React.useEffect(() => {
+    // 화면 너비를 기반으로 모바일 장치 여부 확인
+    const isMobile = window.innerWidth <= 768;
+
+    if (isMobile) {
+      // 모바일 버전에 대한 툴바 설정
+      setModules({
+        toolbar: [
+          [{ header: "1" }, { header: "2" }],
+          ["bold", "italic", "underline"],
+          ["list", "bullet"],
+          ["link", "image"],
+          ["clean"],
+        ],
+        clipboard: {
+          matchVisual: false,
+        },
+      });
+    }
+  }, []);
+
+  return (
+    <QuillWrapper
+      modules={modules}
+      formats={formats}
+      theme="snow"
+      placeholder="내용을 입력하세요."
+      style={{
+        width: "100%",
+        height: "300px",
+      }}
+    />
+  );
 }

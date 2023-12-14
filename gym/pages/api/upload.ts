a683904f0e41c5cf54b0675fcf2c02ca7aa66edb
jiminKang <wlals1234560@gmail.com>
2023-12-14 10:36:06 +0900

    file이 null일수도 있다는 오류 발생

---
+++ gym/pages/api/upload.ts
@@ -0,0 +1,30 @@
+import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
+import { S3Client } from '@aws-sdk/client-s3';
+import { v4 as uuidv4 } from 'uuid';
+import { NextApiRequest, NextApiResponse } from "next";
+
+
+export async function POST(request: Request,response:NextApiResponse) {
+  const { filename, contentType } = await request.json()
+
+  try {
+    const client = new S3Client({ region: process.env.NEXT_PUBLIC_AWS_REGION })
+    const { url, fields } = await createPresignedPost(client, {
+      Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME ? process.env.NEXT_PUBLIC_AWS_BUCKET_NAME: '',
+      Key: uuidv4(),
+      Conditions: [
+        ['content-length-range', 0, 10485760], // up to 10 MB
+        ['starts-with', '$Content-Type', contentType],
+      ],
+      Fields: {
+        acl: 'public-read',
+        'Content-Type': contentType,
+      },
+      Expires: 600, // Seconds before the presigned post expires. 3600 by default.
+    })
+
+    return response.json({ url, fields })
+  } catch (error) {
+    return response.json({ error: onmessage })
+  }
+}

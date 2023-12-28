33e826acdbedec60ba6f9859cd8a364fca236d6f
jiminKang <wlals1234560@gmail.com>
2023-12-28 10:58:39 +0900

    extractFirstImageUrl 모듈화

--- gym/components/board.tsx
+++ gym/components/board.tsx
@@ -11,7 +11,7 @@ import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import { useRouter } from "next/router";
 import DateTimeFormatter from "@/lib/dateTimeFomatter";
-// import extractFristImageUrl from "@/lib/extractFirstImageUrl";
+import extractFirstImageUrl from "@/lib/extractFirstImageUrl";
 type boardInfo = {
   postid: number;
   title: string;
@@ -23,18 +23,6 @@ type boardInfo = {
 };
 export default function Board(props: any) {
   const router = useRouter();
-
-  const extractFirstImageUrl = (html: string) => {
-    if (typeof window === "undefined") {
-      // 코드가 서버에서 실행 중인 경우, 기본 값을 반환하거나 해당하는 방식으로 처리합니다. (DOMParser is not defined 에러 해결 )
-      return null;
-    }
-    const doc = new DOMParser().parseFromString(html, "text/html");
-    const imageElements = doc.querySelectorAll("img");
-    const firstImageUrl =
-      imageElements && imageElements.length > 0 ? imageElements[0].src : null;
-    return firstImageUrl;
-  };
 
   // console.log(props.data);
   return (
--- gym/lib/extractFirstImageUrl.ts
+++ gym/lib/extractFirstImageUrl.ts
@@ -1,4 +1,8 @@
 const extractFirstImageUrl = (html: string) => {
+  if (typeof window === "undefined") {
+    // 코드가 서버에서 실행 중인 경우, 기본 값을 반환하거나 해당하는 방식으로 처리합니다. (DOMParser is not defined 에러 해결 )
+    return null;
+  }
   const doc = new DOMParser().parseFromString(html, "text/html");
   const imageElements = doc.querySelectorAll("img");
   const firstImageUrl =

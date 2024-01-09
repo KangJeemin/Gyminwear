43aed6596ff225fc6ecfc9cade95b2fc44e3b463
jiminKang <wlals1234560@gmail.com>
2024-01-09 23:05:13 +0900

    ecosystem.config 파일에 프로세스 실패시 재시작 하도록 설정 추가

--- gym/ecosystem.config.js
+++ gym/ecosystem.config.js
@@ -2,10 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'NextAppName',
-      exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
-      script: 'node_modules/next/dist/bin/next',
+      exec_mode: 'cluster',  //fork, cluster 둘중 하나 선택.
+      instances: 'max', // cluster 사용시 가동할 인스턴스 수
+      script: 'node_modules/next/dist/bin/next',  // 앱 실행 스크립트
       args: 'start',
+      autorestart: true, // 프로세스 실패 시 자동으로 재시작할지 선택
+      // watch: false, // 파일이 변경되었을 때 재시작 할지 선택
       env_local: {
         APP_ENV: 'local' // APP_ENV=local
       },

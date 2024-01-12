87a960656eb447d32406c6e1315cd2007ead9190
jiminKang <wlals1234560@gmail.com>
2024-01-12 11:59:25 +0900

    app.tsx 파일에 title 태그를 작성해봄

--- gym/pages/_app.tsx
+++ gym/pages/_app.tsx
@@ -3,11 +3,16 @@ import type { AppProps } from "next/app";
 import React from "react";
 import ResponsiveAppBar from "@/components/appbar";
 import { DefaultSeo } from "next-seo";
+import Head from "next/head";
 import defaultSeoProps from "@/next-seo.config";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta />
+        <title>짐인웨어</title>
+      </Head>
       <DefaultSeo {...defaultSeoProps} />
       <ResponsiveAppBar />
       <Component {...pageProps} />

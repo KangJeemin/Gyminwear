8d6c74ed57e2fa46b52f2019ae51053cdf1b78f6
jiminKang <wlals1234560@gmail.com>
2024-02-03 20:00:32 +0900

    google OAUTH의 값 받아오기 완료. 현재  구글에서 수락 했을때 redirect 를 허지 못하는 상황인ㄷ ㅔ해결하기

--- gym/pages/api/oauth/google.ts
+++ gym/pages/api/oauth/google.ts
@@ -4,11 +4,12 @@ import axios from 'axios'
 
 
 const GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token';
+const GOOGLE_USERINFO_URL = 'https://www.googleapis.com/oauth2/v2/userinfo'
 const GOOGLE_SIGNUP_REDIRECT_URI = 'http://localhost:3000/api/oauth/google'
 export default async function google(request: NextApiRequest, response: NextApiResponse) {
     
     const {code}=request.query;
-    const resp = await axios.post(GOOGLE_TOKEN_URL, {
+    const res = await axios.post(GOOGLE_TOKEN_URL, {
         // x-www-form-urlencoded(body)
         code,
         client_id: process.env.NEXT_PUBLIC_Google_OAuth_Client_Id,
@@ -16,8 +17,13 @@ export default async function google(request: NextApiRequest, response: NextApiR
         redirect_uri: GOOGLE_SIGNUP_REDIRECT_URI,
         grant_type: 'authorization_code',
     });
-    console.log(resp.data)
-    // response.json(resp.data);
+    
+    const res2 = await axios.get(GOOGLE_USERINFO_URL,{
+        headers:{
+            Authorization:`Bearer ${res.data.access_token}`
+        }
+    })
+    console.log('res2=',res2.data)
     
 
 }

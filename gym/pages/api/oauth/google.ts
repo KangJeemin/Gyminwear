bf809c38ca5ef2f82bcb6a734fe8e957912f698d
jiminKang <wlals1234560@gmail.com>
2024-02-03 20:13:16 +0900

    redirect 경로 이렇게 작성해봤는데 400오류 발생

--- gym/pages/api/oauth/google.ts
+++ gym/pages/api/oauth/google.ts
@@ -6,6 +6,8 @@ import axios from 'axios'
 const GOOGLE_TOKEN_URL = 'https://oauth2.googleapis.com/token';
 const GOOGLE_USERINFO_URL = 'https://www.googleapis.com/oauth2/v2/userinfo'
 const GOOGLE_SIGNUP_REDIRECT_URI = 'http://localhost:3000/api/oauth/google'
+const GOOGLE_SIGNUP_REDIRECT_URI2 = 'http://localhost:3000/login/oauth/google'
+
 export default async function google(request: NextApiRequest, response: NextApiResponse) {
     
     const {code}=request.query;
@@ -18,12 +20,13 @@ export default async function google(request: NextApiRequest, response: NextApiR
         grant_type: 'authorization_code',
     });
     
-    const res2 = await axios.get(GOOGLE_USERINFO_URL,{
+    const res2 = await axios.post(GOOGLE_USERINFO_URL,{
         headers:{
-            Authorization:`Bearer ${res.data.access_token}`
+            Authorization:`Bearer ${res.data.access_token}`,
+        },
+        body:{
+            redirect_uri: GOOGLE_SIGNUP_REDIRECT_URI2,
         }
     })
     console.log('res2=',res2.data)
-    
-
 }

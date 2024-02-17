817074278a88eadc0bb417fcdbe580668c7decc3
jiminKang <wlals1234560@gmail.com>
2024-02-17 17:33:22 +0900

    jest 사용법 미숙으로 node 파일을 생성하고 console을 찍어보니 ranking 코드가 예상과 달리 동작 했었음

--- gym/lib/__test__/ranking.test.ts
+++ gym/lib/__test__/ranking.test.ts
@@ -5,7 +5,7 @@ const arrayA = [1,2,3,4,5,6,7,8,9,10]
 const arrayB = [2,3,4,5,6,7,8,9,10,11]
 const expactResultArray=[1,1,1,1,1,1,1,1,1,"new"]
 describe('ranking module',()=>{
-    test('두 배열간의 원소값 차이 테스트 ',()=>{
+    test('전날 등수와 오늘 등수의 차이를 반환 ',()=>{
         expect(ranking(arrayA,arrayB)).toEqual(expactResultArray);
     })
 })

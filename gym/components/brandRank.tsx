4340c104cd8431f14626cf427e73644a5a13a076
jiminKang <wlals1234560@gmail.com>
2024-02-20 00:34:03 +0900

    슬라이드 기능 되긴 되는데, height를 15로 설정 했는데, 왜 7.5씩만 움직여야 div들이 안 짤리고 슬라이드가 구현되는 걸까?

--- gym/components/brandRank.tsx
+++ gym/components/brandRank.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import Skeleton from "@mui/material/Skeleton";
 import { motion } from "framer-motion";
 
-function brandRank({ rank, brand }: any) {
+function brandRank() {
   //   if (!(rank || brand)) {
   //     return (
   //       <Box sx={{ width: 300 }}>
@@ -18,7 +18,7 @@ function brandRank({ rank, brand }: any) {
   const [slideState, setSlideState] = React.useState(0);
   function test() {
     setTimeout(() => {
-      setSlideState((state) => state - 25);
+      setSlideState((state) => state - 7.5);
       console.log(slideState);
     }, 3000);
   }
@@ -28,53 +28,89 @@ function brandRank({ rank, brand }: any) {
 
   return (
     <Container sx={{ marginTop: 1 }}>
-      <motion.div
-        style={{ height: 25, overflowY: "hidden" }}
-        animate={{ top: `${slideState}` }}
-        transition={{
-          duration: 0.5,
-          ease: "linear",
+      <Box
+        sx={{
+          width: "100%",
+          color: "black",
+          fontWeight: "bold",
+          overflowY: "hidden",
+          fontSize: 14,
         }}
       >
-        <Box
-          sx={{
+        <motion.div
+          style={{
             width: "100%",
-            display: "flex",
-            color: "black",
-            fontWeight: "bold",
-            justifyContent: "flex-end",
-            fontSize: 14,
-            height: 25,
+            height: 15,
           }}
         >
-          <span>
-            <p style={{ fontWeight: "bold" }}>1등 :</p>
-          </span>
-          <Box sx={{ width: { xs: "30%", md: "10%" } }}>프리덤애슬레틱</Box>
-          <span style={{ color: "red" }}>&#9650;</span>
-          <span>3</span>
-          {/* <span style={{ color: "blue" }}>&#9660;</span> */}
-        </Box>
-        <Box
-          sx={{
-            width: "100%",
-            display: "flex",
-            color: "black",
-            fontWeight: "bold",
-            justifyContent: "flex-end",
-            fontSize: 14,
-            height: 25,
-          }}
-        >
-          <span>
-            <p style={{ fontWeight: "bold" }}>2등 :</p>
-          </span>
-          <Box sx={{ width: { xs: "30%", md: "10%" } }}>본투윈</Box>
-          <span style={{ color: "red" }}>&#9650;</span>
-          <span>3</span>
-          {/* <span style={{ color: "blue" }}>&#9660;</span> */}
-        </Box>
-      </motion.div>
+          <motion.div
+            style={{
+              height: 15,
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "relative",
+              top: 0,
+            }}
+            animate={{ top: slideState }}
+            transition={{
+              duration: 0.5,
+              ease: "linear",
+            }}
+          >
+            <span>
+              <p style={{ fontWeight: "bold" }}>1등 :</p>
+            </span>
+            <Box sx={{ width: { xs: "30%", md: "10%" } }}>프리덤애슬레틱</Box>
+            <span style={{ color: "red" }}>&#9650;</span>
+            <span>3</span>
+            {/* <span style={{ color: "blue" }}>&#9660;</span> */}
+          </motion.div>
+          <motion.div
+            style={{
+              height: 15,
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "relative",
+              top: 0,
+            }}
+            animate={{ top: slideState }}
+            transition={{
+              duration: 0.5,
+              ease: "linear",
+            }}
+          >
+            <span>
+              <p style={{ fontWeight: "bold" }}>2등 :</p>
+            </span>
+            <Box sx={{ width: { xs: "30%", md: "10%" } }}>본투윈</Box>
+            <span style={{ color: "red" }}>&#9650;</span>
+            <span>3</span>
+            {/* <span style={{ color: "blue" }}>&#9660;</span> */}
+          </motion.div>
+          <motion.div
+            style={{
+              height: 15,
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "relative",
+              top: 0,
+            }}
+            animate={{ top: slideState }}
+            transition={{
+              duration: 0.5,
+              ease: "linear",
+            }}
+          >
+            <span>
+              <p style={{ fontWeight: "bold" }}>2등 :</p>
+            </span>
+            <Box sx={{ width: { xs: "30%", md: "10%" } }}>본투윈</Box>
+            <span style={{ color: "red" }}>&#9650;</span>
+            <span>3</span>
+            {/* <span style={{ color: "blue" }}>&#9660;</span> */}
+          </motion.div>
+        </motion.div>
+      </Box>
     </Container>
   );
   //   }

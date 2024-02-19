92046bbf97689ab11ee59255cc65c44bed566177
jiminKang <wlals1234560@gmail.com>
2024-02-19 21:19:50 +0900

    애니메이트 적용 안 되고 있는데 이유가 뭐지

--- gym/components/brandRank.tsx
+++ gym/components/brandRank.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import * as React from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Link from "next/link";
 import Skeleton from "@mui/material/Skeleton";
+import { motion } from "framer-motion";
 
 function brandRank({ rank, brand }: any) {
   //   if (!(rank || brand)) {
@@ -14,30 +15,66 @@ function brandRank({ rank, brand }: any) {
   //       </Box>
   //     );
   //   } else {
+  const [slideState, setSlideState] = React.useState(0);
+  function test() {
+    setTimeout(() => {
+      setSlideState((state) => state - 25);
+      console.log(slideState);
+    }, 3000);
+  }
+  React.useEffect(() => {
+    test();
+  });
+
   return (
     <Container sx={{ marginTop: 1 }}>
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          color: "black",
-          fontWeight: "bold",
-          justifyContent: "flex-end",
-          fontSize: 14,
-          height: 25,
+      <motion.div
+        style={{ height: 25, overflowY: "hidden" }}
+        animate={{ top: `${slideState}` }}
+        transition={{
+          duration: 0.5,
+          ease: "linear",
         }}
       >
-        {/* <h2 style={{ fontSize: 15, color: "gray", marginRight: 10 }}>
-          오늘의 브랜드 클릭 순위
-        </h2> */}
-        <span>
-          <p style={{ fontWeight: "bold" }}>1등 :</p>
-        </span>
-        <Box sx={{ width: { xs: "30%", md: "10%" } }}>프리덤애슬레틱</Box>
-        <span style={{ color: "red" }}>&#9650;</span>
-        <span>3</span>
-        {/* <span style={{ color: "blue" }}>&#9660;</span> */}
-      </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            color: "black",
+            fontWeight: "bold",
+            justifyContent: "flex-end",
+            fontSize: 14,
+            height: 25,
+          }}
+        >
+          <span>
+            <p style={{ fontWeight: "bold" }}>1등 :</p>
+          </span>
+          <Box sx={{ width: { xs: "30%", md: "10%" } }}>프리덤애슬레틱</Box>
+          <span style={{ color: "red" }}>&#9650;</span>
+          <span>3</span>
+          {/* <span style={{ color: "blue" }}>&#9660;</span> */}
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            color: "black",
+            fontWeight: "bold",
+            justifyContent: "flex-end",
+            fontSize: 14,
+            height: 25,
+          }}
+        >
+          <span>
+            <p style={{ fontWeight: "bold" }}>2등 :</p>
+          </span>
+          <Box sx={{ width: { xs: "30%", md: "10%" } }}>본투윈</Box>
+          <span style={{ color: "red" }}>&#9650;</span>
+          <span>3</span>
+          {/* <span style={{ color: "blue" }}>&#9660;</span> */}
+        </Box>
+      </motion.div>
     </Container>
   );
   //   }

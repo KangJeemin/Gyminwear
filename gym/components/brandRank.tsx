880ac78eeeeab4c9fc0a6c237c5c3e36a50b7043
jiminKang <wlals1234560@gmail.com>
2024-02-20 10:42:35 +0900

    slide roop 로직 구현 중인데, 슬라이드 1칸 넘어가고 안 넘어가네

--- gym/components/brandRank.tsx
+++ gym/components/brandRank.tsx
@@ -6,114 +6,143 @@ import Skeleton from "@mui/material/Skeleton";
 import { motion } from "framer-motion";
 
 function brandRank() {
-  //   if (!(rank || brand)) {
-  //     return (
-  //       <Box sx={{ width: 300 }}>
-  //         <Skeleton />
-  //         <Skeleton animation="wave" />
-  //         <Skeleton animation={false} />
-  //       </Box>
-  //     );
-  //   } else {
   const [slideState, setSlideState] = React.useState(0);
-  function test() {
-    setTimeout(() => {
-      setSlideState((state) => state - 7.5);
-      console.log(slideState);
-    }, 3000);
+  const [onSlide, setOnslide] = React.useState(true);
+  const [animateime, setAnimatetime] = React.useState(0.5);
+  const rank = [
+    {
+      brandname: "본투윈",
+      rank: +3,
+    },
+    {
+      brandname: "프리덤",
+      rank: 0,
+    },
+    {
+      brandname: "UNF",
+      rank: -1,
+    },
+    {
+      brandname: "피지컬크라운",
+      rank: "new",
+    },
+    {
+      brandname: "키카그리도",
+      rank: +2,
+    },
+    {
+      brandname: "덤브스트럭",
+      rank: 0,
+    },
+    {
+      brandname: "허그본",
+      rank: +1,
+    },
+    {
+      brandname: "피지코어패럴",
+      rank: -1,
+    },
+    {
+      brandname: "머슬암드",
+      rank: -1,
+    },
+    {
+      brandname: "골드앤머신",
+      rank: +4,
+    },
+  ];
+  function slideroolpe() {
+    if (slideState === -150) {
+      setAnimatetime(0);
+      setSlideState(0);
+      setOnslide(false);
+      setTimeout(() => {
+        setOnslide(true);
+      }, 3000);
+    }
+
+    if (onSlide)
+      setTimeout(() => {
+        setSlideState((state) => state - 15);
+        console.log(slideState);
+      }, 3000);
   }
   React.useEffect(() => {
-    test();
-  });
-
-  return (
-    <Container sx={{ marginTop: 1 }}>
-      <Box
-        sx={{
-          width: "100%",
-          color: "black",
-          fontWeight: "bold",
-          overflowY: "hidden",
-          fontSize: 14,
-        }}
-      >
-        <motion.div
-          style={{
+    slideroolpe();
+  }, []);
+  if (!rank) {
+    return (
+      <Box sx={{ width: 300 }}>
+        <Skeleton />
+        <Skeleton animation="wave" />
+        <Skeleton animation={false} />
+      </Box>
+    );
+  } else {
+    return (
+      <Container sx={{ height: 15 }}>
+        <Box
+          sx={{
             width: "100%",
-            height: 15,
+            color: "black",
+            fontWeight: "bold",
+            overflowY: "hidden",
+            fontSize: 14,
           }}
         >
-          <motion.div
+          <div
             style={{
+              width: "100%",
               height: 15,
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "relative",
-              top: 0,
-            }}
-            animate={{ top: slideState }}
-            transition={{
-              duration: 0.5,
-              ease: "linear",
             }}
           >
-            <span>
-              <p style={{ fontWeight: "bold" }}>1등 :</p>
-            </span>
-            <Box sx={{ width: { xs: "30%", md: "10%" } }}>프리덤애슬레틱</Box>
-            <span style={{ color: "red" }}>&#9650;</span>
-            <span>3</span>
-            {/* <span style={{ color: "blue" }}>&#9660;</span> */}
-          </motion.div>
-          <motion.div
-            style={{
-              height: 15,
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "relative",
-              top: 0,
-            }}
-            animate={{ top: slideState }}
-            transition={{
-              duration: 0.5,
-              ease: "linear",
-            }}
-          >
-            <span>
-              <p style={{ fontWeight: "bold" }}>2등 :</p>
-            </span>
-            <Box sx={{ width: { xs: "30%", md: "10%" } }}>본투윈</Box>
-            <span style={{ color: "red" }}>&#9650;</span>
-            <span>3</span>
-            {/* <span style={{ color: "blue" }}>&#9660;</span> */}
-          </motion.div>
-          <motion.div
-            style={{
-              height: 15,
-              display: "flex",
-              justifyContent: "flex-end",
-              position: "relative",
-              top: 0,
-            }}
-            animate={{ top: slideState }}
-            transition={{
-              duration: 0.5,
-              ease: "linear",
-            }}
-          >
-            <span>
-              <p style={{ fontWeight: "bold" }}>2등 :</p>
-            </span>
-            <Box sx={{ width: { xs: "30%", md: "10%" } }}>본투윈</Box>
-            <span style={{ color: "red" }}>&#9650;</span>
-            <span>3</span>
-            {/* <span style={{ color: "blue" }}>&#9660;</span> */}
-          </motion.div>
-        </motion.div>
-      </Box>
-    </Container>
-  );
-  //   }
+            {rank.map((object, index) => (
+              <motion.div
+                style={{
+                  height: 15,
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  position: "relative",
+                  top: 0,
+                }}
+                animate={{ top: slideState }}
+                transition={{
+                  duration: animateime,
+                  ease: "linear",
+                }}
+              >
+                <span>
+                  <p style={{ fontWeight: "bold" }}>{index + 1}등 :</p>
+                </span>
+                <Box sx={{ width: { xs: "30%", md: "10%" } }}>
+                  {object.brandname}
+                </Box>
+                {object.rank > 0 ? (
+                  <>
+                    <span style={{ color: "red" }}>&#9650;</span>
+                    <span>{object.rank}</span>
+                  </>
+                ) : object.rank < 0 ? (
+                  <>
+                    <span style={{ color: "blue" }}>&#9660;</span>
+                    <span>{Math.abs(object.rank)}</span>
+                  </>
+                ) : object.rank === 0 ? (
+                  <>
+                    <span>&#10134;</span>
+                  </>
+                ) : (
+                  <>
+                    <span>"new"</span>
+                  </>
+                )}
+              </motion.div>
+            ))}
+          </div>
+        </Box>
+      </Container>
+    );
+  }
 }
 
 export default brandRank;

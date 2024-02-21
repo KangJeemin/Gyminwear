f40e695dfbafa798bc7ec89758bf13b477340fce
jiminKang <wlals1234560@gmail.com>
2024-02-22 00:54:10 +0900

    로직 완성된 것 같은데 왜 값이 제대로 안 나오지? 심지어 trueArray의 콘솔은 [ 3 ] 으로 찍힘

--- gym/lib/__test__/ranking.test.ts
+++ gym/lib/__test__/ranking.test.ts
@@ -2,7 +2,7 @@ import {describe, expect, test} from '@jest/globals';
 import ranking from '../ranking'
 
 const arrayA = [3,1,2,4,5,7,11,23,35,13]
-const arrayB = [1,4,2,6,5,8,13,24,3,14,23]
+const arrayB = [1,4,2,6,5,8,13,24,3,14]
 const expactResultArray=[1,2,0,"new",0,"new",3,"new",-8,"new"]
 describe('ranking module',()=>{
     test('전날 등수와 오늘 등수의 차이를 반환 ',()=>{
--- gym/lib/__test__/ranking2.test.ts
+++ gym/lib/__test__/ranking2.test.ts
@@ -2,11 +2,12 @@ import {describe, expect, test} from '@jest/globals';
 import ranking2 from '../ranking2'
 
 const arrayA = [3,1,2,4,5,7,11,23,35,13]
-const arrayB = [1,4,2,6,5,8,13,24,3,14,23]
+const arrayB = [1,4,2,6,5,8,13,24,3,14]
+const expactResultArray=[1,2,0,"new",0,"new",3,"new",-8,"new"]
 
 
 describe('ranking module2',()=>{
     test('전날 등수와 오늘 등수의 차이를 반환 ',()=>{
-        expect(ranking2(arrayA,arrayB));
+        expect(ranking2(arrayA,arrayB)).toEqual(expactResultArray);
     })
 })

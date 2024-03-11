56fb86aed2aa95f32eeb695c123fe36ac78852ba
jiminKang <wlals1234560@gmail.com>
2024-03-11 17:58:19 +0900

    remove type at FucntionalRanking.ts

--- gym/lib/FunctionalRanking.ts
+++ gym/lib/FunctionalRanking.ts
@@ -1,29 +1,25 @@
-type CalulateRankingArray = number;
-type RankingArray = number | string;
 
-function generateTrueArray(todayArray: CalulateRankingArray[], yesterdayArray: CalulateRankingArray[]): (CalulateRankingArray | boolean)[][] {
 
-    const trueArray: (CalulateRankingArray | boolean)[][] = todayArray.map((item) => {
+function generateTrueArray(todayArray: number[], yesterdayArray: number[]): (number | boolean)[][] {
+
+    const trueArray: (number | boolean)[][] = todayArray.map((item) => {
         const index = yesterdayArray.indexOf(item);
         return index !== -1 ? [item, true] : [item, false];
     });
     return trueArray;
 }
 
-function calculateRankDifference(todayArray: CalulateRankingArray[], trueArray: (CalulateRankingArray | boolean)[][],yesterdayArray: CalulateRankingArray[]): (number | string)[] {
-
+function calculateRankDifference(trueArray: (number | boolean)[][],yesterdayArray: number[]): (number | string)[] {
 
     return trueArray.map(([item, isTrue],key) => {
         if (!isTrue) return "new";
-        
-        // const index = todayArray.indexOf(item);
         const yesterdayIndex = yesterdayArray.findIndex((value) => value === item);
         return yesterdayIndex-key;
     });
 }
 
-export default function ranking2(yesterdayArray: CalulateRankingArray[], todayArray: CalulateRankingArray[]): (number | string)[] {
+export default function ranking2(yesterdayArray: number[], todayArray: number[]): (number | string)[] {
     const trueArray = generateTrueArray(todayArray, yesterdayArray);
-    const rankDifference = calculateRankDifference(todayArray, trueArray,yesterdayArray);
+    const rankDifference = calculateRankDifference(trueArray,yesterdayArray);
     return rankDifference;
 }

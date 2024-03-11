dccd68aa04a2da724345287d317aaaf165a26f5b
jiminKang <wlals1234560@gmail.com>
2024-03-11 17:55:48 +0900

    make rankingLogic Functional

---
+++ gym/lib/__test__/functionalRank.test.ts
@@ -0,0 +1,11 @@
+import {describe, expect, test} from '@jest/globals';
+import ranking from '../FunctionalRanking'
+
+const arrayA = [3,1,2,4,5,7,11,23,35,13]
+const arrayB = [1,4,2,6,5,8,13,24,3,14]
+const expactResultArray=[1,2,0,"new",0,"new",3,"new",-8,"new"]
+describe('ranking module',()=>{
+    test('전날 등수와 오늘 등수의 차이를 반환 ',()=>{
+        expect(ranking(arrayA,arrayB)).toEqual(expactResultArray);
+    })
+})
